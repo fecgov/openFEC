4ea409ef7a8b46feaf50272a3333782ed2cf8848
Catherine Devlin <catherine.devlin@gsa.gov>
2014-11-28 15:15:11 -0500

    fixed form_3

--- data/synch/fill_cmte_dates.sql
+++ data/synch/fill_cmte_dates.sql
@@ -1,5 +1,4 @@
 -- run on cfdm on the bridge machine
-
 UPDATE public.dimcand p
 SET    load_date = o.receipt_dt
 FROM   frn.form_2 o
@@ -22,29 +21,24 @@ AND    p.form_tp IN ('F2', 'F2Z');
 
 UPDATE public.dimcmte p
 SET    load_date = o.receipt_dt
-FROM   frn.form_2 o
-WHERE  p.form_sk = o.form_2_sk
+FROM   frn.form_1 o
+WHERE  p.form_sk = o.form_1_sk
 AND    p.form_tp IN ('F1', 'F1Z');
 
 UPDATE public.dimcmteproperties p
 SET    load_date = o.receipt_dt
-FROM   frn.form_2 o
-WHERE  p.form_sk = o.form_2_sk
-AND    p.form_tp IN ('F1', 'F1Z');
-
-UPDATE public.dimcmtetpdsgn p
-SET    load_date = o.receipt_dt
-FROM   frn.form_2 o
-WHERE  p.form_sk = o.form_2_sk
+FROM   frn.form_1 o
+WHERE  p.form_sk = o.form_1_sk
 AND    p.form_tp IN ('F1', 'F1Z');
 
 -- no form data for dimlinkages
+-- dimcmtetpdsgn (but that has a receipt_date)
 -- dimoffice
 -- dimparty
 
 
 UPDATE public.facthousesenate_f3 p
-SET    load_date = o.receipt_dt
+SET    load_date = o.load_date
 FROM   frn.form_3 o
 WHERE  p.form_3_sk = o.form_3_sk;
 
@@ -58,7 +52,8 @@ SET    load_date = o.receipt_dt
 FROM   frn.form_3p o
 WHERE  p.form_3p_sk = o.form_3p_sk;
 
-
+-- this runs but does not actually update anything -
+-- cand_ids are unique in dimcand
 WITH s AS (
   SELECT cand_sk,
          lead(load_date) over
@@ -102,36 +97,42 @@ FROM   s
 WHERE  p.candproperties_sk = s.candproperties_sk;
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-UPDATE public.dimcmteproperties p
-SET    load_date = f.receipt_dt
-FROM   frn.form_1 f
-WHERE  p.form_sk = f.form_1_sk
-AND    p.form_tp IN ('F1', 'F1Z');
+WITH s AS (
+  SELECT cmte_sk,
+         lead(load_date) over
+           (partition by cmte_id
+            order by load_date, cmte_sk) end_date,
+         load_date
+  FROM   public.dimcmte)
+UPDATE public.dimcmte p
+SET    -- effective = tsrange(s.beg, s.expire_date, '[)'),
+       expire_date = s.end_date
+FROM   s
+WHERE  p.cmte_sk = s.cmte_sk;
 
 WITH s AS (
   SELECT cmte_sk,
          cmteproperties_sk,
          lead(load_date) over
-           (partition by cand_sk
+           (partition by cmte_sk
             order by load_date, cmteproperties_sk) end_date,
          load_date
   FROM   public.dimcmteproperties)
 UPDATE public.dimcmteproperties p
-SET    effective = tsrange(s.beg, s.expire_date, '[)'),
+SET    -- effective = tsrange(s.beg, s.expire_date, '[)'),
        expire_date = s.end_date
 FROM   s
 WHERE  p.cmteproperties_sk = s.cmteproperties_sk;
+
+
+
+
+
+
+
+
+
+
+
+
+
