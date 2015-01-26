f58e4d32119684ecc80e930d419ff7b0db600203
CM Lubinski <christopher.lubinski@gsa.gov>
2015-01-26 16:34:16 +0000

    Flat module

--- webservices/rest.py
+++ webservices/rest.py
@@ -57,7 +57,7 @@ import copy
 from datetime import datetime
 from psycopg2._range import DateTimeRange
 
-from .candidate.models import Candidate
+from models import Candidate
 
 speedlogger = logging.getLogger('speed')
 speedlogger.setLevel(logging.CRITICAL)
