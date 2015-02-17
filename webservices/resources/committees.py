bbbb88981f97b687bd81ac06f8d9bba7722f3eb1
LindsayYoung <lindsayn.young@gsa.gov>
2015-02-17 10:48:44 -0500

    working on associations

--- webservices/resources/committees.py
+++ webservices/resources/committees.py
@@ -20,6 +20,7 @@ committee_fields = {
     'committee_type': fields.String,
     'expire_date': fields.String,
     'original_registration_date': fields.String,
+    # want to add ids
     'candidate_ids': fields.String,
 }
 pagination_fields = {
@@ -38,81 +39,50 @@ committee_list_fields = {
 class CommitteeList(Resource):
     parser = reqparse.RequestParser()
     parser.add_argument(
-        'q',
-        type=str,
-        help='Text to search all fields for'
+        'q', type=str, help='Text to search all fields for'
     )
     parser.add_argument(
-        'committee_id',
-        type=str,
-        help="Committee's FEC ID"
+        'committee_id', type=str, help="Committee's FEC ID"
     )
     parser.add_argument(
-        'state',
-        type=str,
-        help='Two digit U.S. State committee is registered in'
+        'state', type=str, help='Two digit U.S. State committee is registered in'
     )
     parser.add_argument(
-        'name',
-        type=str,
-        help="Committee's name (full or partial)"
+        'name', type=str, help="Committee's name (full or partial)"
     )
     parser.add_argument(
-        'page',
-        type=int,
-        default=1,
-        help='For paginating through results, starting at page 1'
+        'page', type=int, default=1, help='For paginating through results, starting at page 1'
     )
     parser.add_argument(
-        'per_page',
-        type=int,
-        default=20,
-        help='The number of results returned per page. Defaults to 20.'
+        'per_page', type=int, default=20, help='The number of results returned per page. Defaults to 20.'
     )
     parser.add_argument(
-        'committee_type',
-        type=str,
-        help='The one-letter type code of the organization'
+        'committee_type', type=str, help='The one-letter type code of the organization'
     )
     parser.add_argument(
-        'designation',
-        type=str,
-        help='The one-letter designation code of the organization'
+        'designation', type=str, help='The one-letter designation code of the organization'
     )
     parser.add_argument(
-        'designation_short',
-        type=str,
-        help='The one-letter designation code of the organization'
+        'designation_short', type=str, help='The one-letter designation code of the organization'
     )
     parser.add_argument(
-        'organization_type',
-        type=str,
-        help='The one-letter code for the kind for organization'
+        'organization_type', type=str, help='The one-letter code for the kind for organization'
     )
     parser.add_argument(
-        'organization_type_short',
-        type=str,
-        help='The one-letter code for the kind for organization'
+        'organization_type_short', type=str, help='The one-letter code for the kind for organization'
     )
     parser.add_argument(
-        'party',
-        type=str,
-        help='Three letter code for party'
+        'party', type=str, help='Three letter code for party'
     )
     parser.add_argument(
-        'expire_date',
-        type=str,
-        help='Date the committee registration expires'
+        'expire_date', type=str, help='Date the committee registration expires'
     )
     parser.add_argument(
-        'original_registration_date',
-        type=str,
-        help='Date of the committees first registered'
+        'original_registration_date', type=str, help='Date of the committees first registered'
     )
     parser.add_argument(
-        'candidate_ids',
-        type=str,
-        help='FEC IDs of candidates that committees have filed about. (See designation for the nature of the relationship.)')
+        'candidate_ids', type=str,help='FEC IDs of candidates that committees have mentioned in filings. (See designation for the nature of the relationship.)'
+    )
 
     @marshal_with(committee_list_fields)
     def get(self, **kwargs):
@@ -166,9 +136,15 @@ class CommitteeList(Resource):
         # I want to add a proper year filter here
 
         count = committees.count()
+
+        # trying to access the candidate_ids
+        for candidate in Association.child:
+            print linkages.cand_id
+
         print str(committees)
 
         return count, committees.order_by(Committee.name).paginate(page_num, per_page, False).items
+
 
 class Committee(db.Model):
     committee_key = db.Column(db.Integer, primary_key=True)
@@ -186,7 +162,6 @@ class Committee(db.Model):
     party = db.Column(db.String(50))
     original_registration_date = db.Column(db.DateTime())
     name = db.Column(db.String(100))
-    candidate_ids = db.Column(db.String(100))
 
     __tablename__ = 'ofec_committees_vw'
 
@@ -197,19 +172,18 @@ class CommitteeFulltext(db.Model):
 
     __tablename__ = 'dimcmte_fulltext'
 
+
+class Linkages(db.Model):
+    cmte_sk = db.Column(db.Integer, primary_key=True)
+    cand_id = db.Column(db.String(9))
+
+    __tablename__ = 'linkages'
+
+
 # trying to link candidate ids so that committees will be search-able by candidate ids
 class Association(Committee):
     __tablename__ = 'association'
-    left_id = db.Column(db.Integer, db.ForeignKey('dimcmte.committee_key'), primary_key=True)
-    right_id = db.Column(db.Integer, db.ForeignKey('linkages.cmte_sk'), primary_key=True)
-    child = db.relationship("Child", backref="parent_assocs")
-
-class Parent(Committee):
-    __tablename__ = 'dimcmte'
-    committee_key = db.Column(db.Integer, primary_key=True)
-    children = relationship("Association", backref="parent")
-
-class Child(Committee):
-    __tablename__ = 'linkages'
-    cmte_sk = db.Column(db.Integer, primary_key=True)
+    dimcmte_id = db.Column(db.Integer, db.ForeignKey('ofec_committees_vw.committee_key'), primary_key=True, )
+    linkages_id = db.Column(db.Integer, db.ForeignKey('linkages.cmte_sk'), primary_key=True)
+    child = db.relationship("Linkages", backref="parent_assocs")
 
