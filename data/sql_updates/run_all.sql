62668adf91ab6244154e0409f1af1d439cb6d565
arowla <alison.rowland@gsa.gov>
2015-03-06 00:52:29 -0500

    Made file to run all migrations.

---
+++ data/sql_updates/run_all.sql
@@ -0,0 +1,8 @@
+\i create_candidates_vw.sql
+\i create_candidate_detail_vw.sql
+\i create_committees_vw.sql
+\i create_totals_pacs_parties_view.sql
+\i create_totals_presidential_view.sql
+\i create_totals_house_senate_view.sql
+\i create_committee_detail_vw.sql
+\i create_linkage_names_vw.sql
