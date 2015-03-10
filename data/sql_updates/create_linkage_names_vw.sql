09e720db1777c49ca7831f47ad99728a05c9f6e1
arowla <alison.rowland@gsa.gov>
2015-03-10 15:19:11 -0400

    Fixed linkage view SQL
    - Made a subselect for the active_through year
    - Removed group by
    - Removed order by (should be done at query time)

--- data/sql_updates/create_linkage_names_vw.sql
+++ data/sql_updates/create_linkage_names_vw.sql
@@ -1,11 +1,12 @@
 drop view if exists name_linkage_vw;
-create view name_linkage_vw as
+drop view if exists ofec_name_linkage_vw;
+create view ofec_name_linkage_vw as
 select
     dimlinkages.linkages_sk as linkage_key,
     dimlinkages.cand_sk as candidate_key,
     dimlinkages.cmte_sk as committee_key,
     dimlinkages.cand_id as candidate_id,
-    max(dimlinkages.cand_election_yr) as active_through,
+    (select max(cand_election_yr) from dimlinkages where cand_id = cand_id) as active_through,
     dimlinkages.cand_election_yr as election_year,
     dimlinkages.cmte_id as committee_id,
     dimlinkages.cmte_tp as committee_type,
@@ -41,9 +42,4 @@ select
     dimlinkages.expire_date as expire_date,
     (select cand_nm from dimcandproperties where dimcandproperties.cand_sk = dimlinkages.cand_sk order by candproperties_sk desc limit 1) as candidate_name,
     (select cmte_nm from dimcmteproperties where dimcmteproperties.cmte_sk = dimlinkages.cmte_sk order by cmteproperties_sk desc limit 1) as committee_name
-from dimlinkages
-    group by
-        dimlinkages.linkages_sk
-    order by
-        dimlinkages.cmte_dsgn,
-        dimlinkages.cand_election_yr desc;
+from dimlinkages;
