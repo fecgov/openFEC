406e14d7f1ea078435946aa299162a148405a3a1
Joshua Carp <jm.carp@gmail.com>
2015-04-13 09:05:24 -0400

    Run migration in subprocess on cf push.

--- run_api.py
+++ run_api.py
@@ -1,14 +1,21 @@
-import doctest
 import os
 import sys
+import doctest
+import subprocess
 
 
 from webservices.rest import app
 
 
 if __name__ == '__main__':
-    
+
     port = os.getenv('VCAP_APP_PORT', '5000')
+    instance_id = os.getenv('CF_INSTANCE_INDEX')
+
+    # Run database migration in separate process to avoid Cloud Foundry timeout
+    # Note: Only run if first application process
+    if instance_id == '0':
+        subprocess.Popen(['python manage.py', 'refresh_db'])
 
     if len(sys.argv) > 1 and sys.argv[1].lower().startswith('test'):
         doctest.testmod()
