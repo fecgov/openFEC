3c7f82a31654534cf340fe13569a2b45bfb28177
LindsayYoung <lindsayn.young@gsa.gov>
2015-04-21 13:11:27 -0400

    Updated name in candidate history sql.

--- data/sql_updates/create_candidate_history_view.sql
+++ data/sql_updates/create_candidate_history_view.sql
@@ -1,31 +1,31 @@
 drop materialized view if exists ofec_candidate_history_mv;
 create materialized view ofec_candidate_history_mv as
 select
-    dcp_recent.candproperties_sk as properties_key,
-    dcp_recent.cand_sk as candidate_key,
-    dcp_recent.cand_id as candidate_id,
-    dcp_recent.cand_nm as name,
-    dcp_recent.record_expire_date as expire_date,
-    dcp_recent.record_load_date as load_date,
-    dcp_recent.record_form_tp as form_type,
-    dcp_recent.two_year_period as two_year_period,
-    dcp_recent.cand_st as address_state,
-    dcp_recent.cand_city as address_city,
-    dcp_recent.cand_st1 as address_street_1,
-    dcp_recent.cand_st2 as address_street_2,
-    dcp_recent.cand_st2 as address_street,
-    dcp_recent.cand_zip as address_zip,
-    dcp_recent.cand_ici_desc as incumbent_challenge_full,
-    dcp_recent.cand_ici_cd as incumbent_challenge,
-    dcp_recent.cand_status_cd as candidate_status,
-    dcp_recent.cand_status_desc as candidate_status_full,
-    dcp_recent.cand_inactive_flg as candidate_inactive,
-    dcp_recent.office_tp as office,
-    dcp_recent.office_tp_desc as office_full,
-    dcp_recent.office_state as state,
-    dcp_recent.office_district as district,
-    dcp_recent.party_affiliation as party,
-    dcp_recent.party_affiliation_desc as party_full
+    dcp_by_period.candproperties_sk as properties_key,
+    dcp_by_period.cand_sk as candidate_key,
+    dcp_by_period.cand_id as candidate_id,
+    dcp_by_period.cand_nm as name,
+    dcp_by_period.record_expire_date as expire_date,
+    dcp_by_period.record_load_date as load_date,
+    dcp_by_period.record_form_tp as form_type,
+    dcp_by_period.two_year_period as two_year_period,
+    dcp_by_period.cand_st as address_state,
+    dcp_by_period.cand_city as address_city,
+    dcp_by_period.cand_st1 as address_street_1,
+    dcp_by_period.cand_st2 as address_street_2,
+    dcp_by_period.cand_st2 as address_street,
+    dcp_by_period.cand_zip as address_zip,
+    dcp_by_period.cand_ici_desc as incumbent_challenge_full,
+    dcp_by_period.cand_ici_cd as incumbent_challenge,
+    dcp_by_period.cand_status_cd as candidate_status,
+    dcp_by_period.cand_status_desc as candidate_status_full,
+    dcp_by_period.cand_inactive_flg as candidate_inactive,
+    dcp_by_period.office_tp as office,
+    dcp_by_period.office_tp_desc as office_full,
+    dcp_by_period.office_state as state,
+    dcp_by_period.office_district as district,
+    dcp_by_period.party_affiliation as party,
+    dcp_by_period.party_affiliation_desc as party_full
 from ofec_two_year_periods
     left join (
         select distinct on (two_year_period, cand_sk) ((CAST(EXTRACT(YEAR FROM dcp.load_date) AS INT) + CAST(EXTRACT(YEAR FROM dcp.load_date) AS INT) % 2)) as two_year_period, dcp.expire_date as record_expire_date, dcp.load_date as record_load_date, dcp.form_tp as record_form_tp, *
@@ -36,7 +36,7 @@ from ofec_two_year_periods
             inner join dimoffice using (office_sk)
             inner join dimparty using (party_sk)
         order by cand_sk, two_year_period, dcp.load_date desc
-    ) as dcp_recent on ofec_two_year_periods.year = two_year_period
+    ) as dcp_by_period on ofec_two_year_periods.year = two_year_period
 ;
 
 create index on ofec_candidate_history_mv(candidate_key);
