d79a9eff836e5a79edd20fcec2acf181027ba54c
Joshua Carp <jm.carp@gmail.com>
2015-04-23 17:05:25 -0400

    Optimize queries to build materialized views.
    A handful of materialized view queries used per-row SELECT statements,
    so that a separate SELECT was emitted for each row of the view. This
    patch replaces per-row SELECTs with JOINs, which makes queries
    noticeably faster on the production data.

--- data/sql_updates/create_committees_view.sql
+++ data/sql_updates/create_committees_view.sql
@@ -39,8 +39,7 @@ select distinct
     cp_most_recent.expire_date as expire_date,
     cp_most_recent.cand_pty_affiliation as party,
     p.party_affiliation_desc as party_full,
-    -- contrary to most recent, we'll need to pull just the oldest load_date here, since they don't give us registration dates yet
-    (select distinct on (cmte_sk) load_date from dimcmteproperties cp where cp.cmte_sk = dimcmte.cmte_sk order by cmte_sk, cmteproperties_sk) as original_registration_date,
+    dates.load_date as original_registration_date,
     cp_most_recent.cmte_nm as name,
     candidates.candidate_ids
 from dimcmte
@@ -51,6 +50,10 @@ from dimcmte
     ) cp_most_recent using (cmte_sk)
     left join dimparty p on cp_most_recent.cand_pty_affiliation = p.party_affiliation
     left join (select cmte_sk, array_agg(distinct cand_id)::text[] as candidate_ids from dimlinkages dl group by cmte_sk) candidates on candidates.cmte_sk = dimcmte.cmte_sk
+    left join (
+        select distinct on (cmte_sk) cmte_sk, load_date from dimcmteproperties
+            order by cmte_sk, cmteproperties_sk
+    ) dates on dimcmte.cmte_sk = dates.cmte_sk
     -- inner join dimlinkages dl using (cmte_sk)
 ;
 
