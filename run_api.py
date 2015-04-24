08b0b1b56308e1efce6a9774610a7c19cc65166b
Diego Lapiduz <diego.lapiduz@gsa.gov>
2015-04-23 21:44:44 -0500

    Change NR config

--- run_api.py
+++ run_api.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-import newrelic.agent
 import doctest
 import os
 import sys
@@ -10,7 +9,6 @@ from webservices.rest import app
 
 
 if __name__ == '__main__':
-    newrelic.agent.initialize()
     port = os.getenv('VCAP_APP_PORT', '5000')
     instance_id = os.getenv('CF_INSTANCE_INDEX')
 
