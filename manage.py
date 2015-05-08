135c762ca603b20c8764c153cd772bd7842e1223
Joshua Carp <jm.carp@gmail.com>
2015-05-08 15:53:00 -0400

    Filter views by election cycle.
    Round year filter down to the beginning of the election cycle.
    h/t @lindsayyoung @arowla

--- manage.py
+++ manage.py
@@ -15,8 +15,15 @@ from webservices.rest import app, db
 from webservices.common.util import get_full_path
 
 
+def get_cycle_start(year):
+    """Round year down to the first year of the two-year election cycle. Used
+    when filtering original data for election cycle.
+    """
+    return year if year % 2 == 1 else year - 1
+
+
 SQL_CONFIG = {
-    'START_YEAR': 1980,
+    'START_YEAR': get_cycle_start(1980),
 }
 
 manager = Manager(app)
