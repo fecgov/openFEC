084dbaae50f276d8692127caa985f6cc22a41b07
Joshua Carp <jm.carp@gmail.com>
2015-05-22 12:01:35 -0400

    Factor repeated logic to functions.

--- data/sql_updates/create_committee_history.sql
+++ data/sql_updates/create_committee_history.sql
@@ -8,50 +8,15 @@ select distinct on (cmte_sk, cycle)
     dcp.cmte_nm as name,
     dcp.cmte_treasurer_nm as treasurer_name,
     dcp.org_tp as organization_type,
-    -- Columns org_tp and org_tp_desc may be inconsistent; trust org_tp
-    case dcp.org_tp
-        when 'C' then 'Corporation'
-        when 'L' then 'Labor Organization'
-        when 'M' then 'Membership Organization'
-        when 'T' then 'Trade Association'
-        when 'V' then 'Cooperative'
-        when 'W' then 'Corporation w/o capital stock'
-        else null
-    end as organization_type_full,
+    expand_organization_type(dcp.org_tp) as organization_type_full,
     dcp.cand_pty_affiliation as party,
     dcp.cmte_st as state,
     dcp.expire_date as expire_date,
     dd.cmte_dsgn as designation,
-    case dd.cmte_dsgn
-        when 'A' then 'Authorized by a candidate'
-        when 'J' then 'Joint fundraising committee'
-        when 'P' then 'Principal campaign committee'
-        when 'U' then 'Unauthorized'
-        when 'B' then 'Lobbyist/Registrant PAC'
-        when 'D' then 'Leadership PAC'
-        else 'unknown'
-    end as designation_full,
+    expand_committee_designation(dd.cmte_dsgn) as designation_full,
     dd.cmte_tp as committee_type,
-    case dd.cmte_tp
-        when 'P' then 'Presidential'
-        when 'H' then 'House'
-        when 'S' then 'Senate'
-        when 'C' then 'Communication Cost'
-        when 'D' then 'Delegate Committee'
-        when 'E' then 'Electioneering Communication'
-        when 'I' then 'Independent Expenditor (Person or Group)'
-        when 'N' then 'PAC - Nonqualified'
-        when 'O' then 'Independent Expenditure-Only (Super PACs)'
-        when 'Q' then 'PAC - Qualified'
-        when 'U' then 'Single Candidate Independent Expenditure'
-        when 'V' then 'PAC with Non-Contribution Account - Nonqualified'
-        when 'W' then 'PAC with Non-Contribution Account - Qualified'
-        when 'X' then 'Party - Nonqualified'
-        when 'Y' then 'Party - Qualified'
-        when 'Z' then 'National Party Nonfederal Account'
-        else 'unknown'
-    end as committee_type_full,
-    regexp_replace(p.party_affiliation_desc, '\s*(Added|Removed|\(.*?)\)$', '') as party_full,
+    expand_committee_type(dd.cmte_tp) as committee_type_full,
+    clean_party(p.party_affiliation_desc) as party_full,
     agg.cycles as cycles
 from dimcmteproperties dcp
 inner join dimcmtetpdsgn dd using (cmte_sk)
