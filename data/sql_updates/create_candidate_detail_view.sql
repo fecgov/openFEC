49fc800eedae54331857aff9ca2f9f0be1562941
Joshua Carp <jm.carp@gmail.com>
2015-05-22 12:01:35 -0400

    Factor repeated logic to functions.

--- data/sql_updates/create_candidate_detail_view.sql
+++ data/sql_updates/create_candidate_detail_view.sql
@@ -5,12 +5,7 @@ select
     dimcand.cand_sk as candidate_key,
     dimcand.cand_id as candidate_id,
     max(csi_recent.cand_status) as candidate_status,
-    case max(csi_recent.cand_status)
-        when 'C' then 'Candidate'
-        when 'F' then 'Future candidate'
-        when 'N' then 'Not yet a candidate'
-        when 'P' then 'Prior candidate'
-        else 'Unknown' end as candidate_status_full,
+    expand_candidate_status(max(csi_recent.cand_status)) as candidate_status_full,
     max(co.cand_election_yr) as active_through,
     array_agg(distinct co.cand_election_yr)::int[] as election_years,
     array_agg(distinct(dcp.election_yr + dcp.election_yr % 2))::int[] as cycles,
@@ -18,10 +13,7 @@ select
     max(dimoffice.office_tp) as office,
     max(dimoffice.office_tp_desc) as office_full,
     max(dimparty.party_affiliation) as party,
-    -- Handle typos and notes in party description:
-    -- * "Commandments Party (Removed)" becomes "Commandments Party"
-    -- * "Green Party Added)" becomes "Green Party"
-    regexp_replace(max(dimparty.party_affiliation_desc), '\s*(Added|Removed|\(.*?)\)$', '') as party_full,
+    clean_party(max(dimparty.party_affiliation_desc)) as party_full,
     max(dimoffice.office_state) as state,
     max(dimoffice.office_district) as district,
     max(cand_p_most_recent.cand_nm) as name,
@@ -32,13 +24,7 @@ select
     max(cand_p_most_recent.cand_st2) as address_street_2,
     max(cand_p_most_recent.cand_zip) as address_zip,
     max(full_ici.cand_ici_cd) as incumbent_challenge,
-    -- Columns cand_ici_cd and cand_ici_desc may be inconsistent; trust cand_ici_cd
-    case max(full_ici.cand_ici_cd)
-        when 'I' then 'Incumbent'
-        when 'C' then 'Challenger'
-        when 'O' then 'Open seat'
-        else 'Unknown'
-    end as incumbent_challenge_full,
+    expand_candidate_incumbent(max(full_ici.cand_ici_cd)) as incumbent_challenge_full,
     -- I needed help keeping track of where the information is coming from when we have the information to get the forms linked we can link to the forms for each section.
     -- I would like to replace this information with just links to the form and expire dates
     max(dimcand.form_tp) as form_type,
--- data/sql_updates/create_committees_view.sql
+++ data/sql_updates/create_committees_view.sql
@@ -6,54 +6,18 @@ select distinct
     dimcmte.cmte_sk as committee_key,
     dimcmte.cmte_id as committee_id,
     dd.cmte_dsgn as designation,
-    case dd.cmte_dsgn
-        when 'A' then 'Authorized by a candidate'
-        when 'J' then 'Joint fundraising committee'
-        when 'P' then 'Principal campaign committee'
-        when 'U' then 'Unauthorized'
-        when 'B' then 'Lobbyist/Registrant PAC'
-        when 'D' then 'Leadership PAC'
-        else 'unknown' end as designation_full,
+    expand_committee_designation(dd.cmte_dsgn) as designation_full,
     dd.cmte_tp as committee_type,
-    case dd.cmte_tp
-        when 'P' then 'Presidential'
-        when 'H' then 'House'
-        when 'S' then 'Senate'
-        when 'C' then 'Communication Cost'
-        when 'D' then 'Delegate Committee'
-        when 'E' then 'Electioneering Communication'
-        when 'I' then 'Independent Expenditor (Person or Group)'
-        when 'N' then 'PAC - Nonqualified'
-        when 'O' then 'Independent Expenditure-Only (Super PACs)'
-        when 'Q' then 'PAC - Qualified'
-        when 'U' then 'Single Candidate Independent Expenditure'
-        when 'V' then 'PAC with Non-Contribution Account - Nonqualified'
-        when 'W' then 'PAC with Non-Contribution Account - Qualified'
-        when 'X' then 'Party - Nonqualified'
-        when 'Y' then 'Party - Qualified'
-        when 'Z' then 'National Party Nonfederal Account'
-        else 'unknown' end as committee_type_full,
+    expand_committee_type(dd.cmte_tp) as committee_type_full,
     cp_most_recent.cmte_treasurer_nm as treasurer_name,
     cp_most_recent.org_tp as organization_type,
-    -- Columns org_tp and org_tp_desc may be inconsistent; trust org_tp
-    case cp_most_recent.org_tp
-        when 'C' then 'Corporation'
-        when 'L' then 'Labor Organization'
-        when 'M' then 'Membership Organization'
-        when 'T' then 'Trade Association'
-        when 'V' then 'Cooperative'
-        when 'W' then 'Corporation w/o capital stock'
-        else null
-    end as organization_type_full,
+    expand_organization_type(cp_most_recent.org_tp) as organization_type_full,
     cp_most_recent.cmte_st as state,
     cp_most_recent.expire_date as expire_date,
     cp_most_recent.cand_pty_affiliation as party,
     cp_most_recent.receipt_dt as last_file_date,
     cp_original.receipt_dt as first_file_date,
-    -- Handle typos and notes in party description:
-    -- * "Commandments Party (Removed)" becomes "Commandments Party"
-    -- * "Green Party Added)" becomes "Green Party"
-    regexp_replace(p.party_affiliation_desc, '\s*(Added|Removed|\(.*?)\)$', '') as party_full,
+    clean_party(p.party_affiliation) as party_full,
     cp_most_recent.cmte_nm as name,
     candidates.candidate_ids,
     cp_agg.cycles
