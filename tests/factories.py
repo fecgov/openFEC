4c0836bbd03d847568affd5171135985009a83b7
Joshua Carp <jm.carp@gmail.com>
2015-05-29 16:28:42 -0400

    No need for paired factories.

--- tests/factories.py
+++ tests/factories.py
@@ -1,5 +1,4 @@
 import factory
-from factory.alchemy import SQLAlchemyOptions
 from factory.alchemy import SQLAlchemyModelFactory
 
 from webservices.rest import db
@@ -30,61 +29,30 @@ class CommitteeSearchFactory(BaseFactory):
     cmte_sk = factory.Sequence(lambda n: n)
 
 
-class PairedOptions(SQLAlchemyOptions):
-    def _build_default_options(self):
-        return super()._build_default_options() + [
-            factory.base.OptionDefault('paired_factory', None, inherit=True),
-        ]
-
-
-class PairedFactory(BaseFactory):
-
-    _options_class = PairedOptions
-
-    class Meta:
-        exclude = ('pair', )
-
-    pair = True
-
-    @classmethod
-    def _generate(cls, create, attrs):
-        ret = super()._generate(create, attrs)
-        if attrs.pop('pair', True) and create:
-            paired_factory = cls._meta.paired_factory()
-            ret._paired = paired_factory(pair=False, **{
-                key: value for key, value in attrs.items()
-                if key in set(paired_factory._meta.model.__table__.columns.keys())
-            })
-        return ret
-
-
 class BaseCandidateFactory(BaseFactory):
     candidate_key = factory.Sequence(lambda n: n)
     candidate_id = factory.Sequence(lambda n: 'id{0}'.format(n))
 
 
-class CandidateFactory(PairedFactory, BaseCandidateFactory):
+class CandidateFactory(BaseCandidateFactory):
     class Meta:
         model = models.Candidate
-        paired_factory = lambda: CandidateDetailFactory
 
     election_years = [2012, 2014]
 
 
-class CandidateDetailFactory(PairedFactory, BaseCandidateFactory):
+class CandidateDetailFactory(BaseCandidateFactory):
     class Meta:
         model = models.CandidateDetail
-        paired_factory = lambda: CandidateFactory
 
 
 class CandidateHistoryFactory(BaseCandidateFactory):
     class Meta:
         model = models.CandidateHistory
-    candidate_key = factory.Sequence(lambda n: n)
-    candidate_id = factory.Sequence(lambda n: 'id{0}'.format(n))
+    two_year_period = 2016
 
 
-class BaseCommitteeFactory(PairedFactory):
+class BaseCommitteeFactory(BaseFactory):
     committee_key = factory.Sequence(lambda n: n + 1)
     committee_id = factory.Sequence(lambda n: 'id{0}'.format(n))
 
@@ -92,20 +60,16 @@ class BaseCommitteeFactory(PairedFactory):
 class CommitteeFactory(BaseCommitteeFactory):
     class Meta:
         model = models.Committee
-        paired_factory = lambda: CommitteeDetailFactory
 
 
 class CommitteeDetailFactory(BaseCommitteeFactory):
     class Meta:
         model = models.CommitteeDetail
-        paired_factory = lambda: CommitteeFactory
 
 
-class CommitteeHistoryFactory(BaseFactory):
+class CommitteeHistoryFactory(BaseCommitteeFactory):
     class Meta:
         model = models.CommitteeHistory
-    committee_key = factory.Sequence(lambda n: n + 1)
-    committee_id = factory.Sequence(lambda n: 'id{0}'.format(n))
     cycle = 2016
 
 
