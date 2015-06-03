68f4646cac7406c62623162c0010884e83530a09
LindsayYoung <lindsayn.young@gsa.gov>
2015-06-03 15:03:26 -0400

    Fleshing out documentation details.

--- webservices/docs.py
+++ webservices/docs.py
@@ -19,6 +19,8 @@ using  `/candidates/search` will help you locate a particular candidate.
 Officially, a candidate is an individual seeking nomination for election to a federal
 office. Someone becomes a candidate when he or she (or agents working on his or her behalf)
 raises contributions or makes expenditures that exceed $5,000.
+
+The candidate endpoints primarily use data from FEC registration forms 1 and 2.
 '''
 
 NAME_SEARCH = '''
@@ -27,7 +29,6 @@ particular person or group, using a name to find the `candidate_id` or `committe
 this endpoint can be a helpful first step.
 '''
 
-# I am not sure this is being used
 CANDIDATE_LIST = '''
 You can fetch basic information about candidates and use parameters to filter for the
 candidates you are looking for.
@@ -37,9 +38,6 @@ particular office sought. So, if a candidate runs for the same office over time,
 will stay the same. If the same person runs for another office, for example, a House
 candidate runs for a Senate office, that candidate will get an additional id that will be
 unique to him or her for that office.
-
-The candidate endpoint uses data from FEC forms 1 and 2 with additional forms to provide
-context.
 '''
 
 CANDIDATE_HISTORY = '''
@@ -70,10 +68,20 @@ This endpoint is useful for finding detailed information about a particular cand
 
 '''
 
-COMMITTEE_LIST = '''
-Fetch basic information about committees.
+COMMITTEE_TAG = '''
+Committees are entities that spend and raise money in election. Their characteristics and
+relationships with candidates can change over time.
 
-Committees include the committees and organizations that file with the FEC. There are a
+You might want to use filters or the search endpoints to find a committee you are looking
+for, and then use the other committee endpoints to get more information about the committee
+you are interested.
+
+Financial information is organized by committee_id, so finding the committee you are interested
+will lead you to more granular financial information.
+
+The committee endpoints include all FEC filers, even if they are not registered as a committee.
+
+Officially, committees include the committees and organizations that file with the FEC. There are a
 number of different types of organizations who file financial reports with the FEC:
 
 * Campaign committees authorized by particular candidates to raise and spend funds in
@@ -84,16 +92,26 @@ unions, trade or membership groups, etc.
 * Groups and individuals making only independent expenditures
 * Corporations, unions and other organizations making internal communications
 
-The committee endpoint returns FEC forms 1 and 2 with additional forms to provide context.
+The committee endpoints primarily use data from FEC registration forms 1 and 2.
+'''
+
+COMMITTEE_LIST = '''
+Fetch basic information about committees and filers. Use parameters to filter for
+particular characteristics.
+
 '''
 
 COMMITTEE_DETAIL = '''
-Fetch detailed information about a committee.
-
-For further description, see `/v1/committees`.
+This endpoint is useful for finding detailed information about a particular committee or
+filer. Use the `committee_id` to find the most recent information about the committee.
 '''
 
-REPORTS = '''
+COMMITTEE_HISTORY = '''
+Find out a filer's characteristics over time. This can be particularly useful if the
+committees change treasurers, designation or committee_type over time.
+'''
+
+FINANCIAL_TAG = '''
 Fetch key information about a committee's form 3, 3x or 3p financial reports.
 
 As part of each financial report, most committees must provide a summary of their financial
@@ -104,14 +122,33 @@ season, therefore, the period covered by this file may be different for differen
 committees. These totals also incorporate any changes made by committees if any report
 covering the period is amended.
 
+Information is made available on the API as soon as it is processed. Keep in mind, complex
+paper filings take longer to process.
+
+The financial endpoints use data from FEC form 3, form 3X and form 3P.
+'''
+
+REPORTS='''
+Each report represents the summary information from FEC form 3, form 3X and form 3P.
+These reports have key statistics to see the financial status of a given committee.
+Things like cash on hand, debts owed by committee, total receipts and total disbursements
+are especially helpful for understanding a committee's financial dealings.
+
+If a report is amended, this endpoint just shows the final amended version.
+
 There are several different reporting structures, depending on what type of organization
 is submitting financial information. To see an example of these reporting requirements you
 can look at the summary and detailed summary pages of the FEC form 3, form 3X and form 3P.
 '''
 
 TOTALS = '''
-Fetch key information about a committee's form 3, 3x or 3p financial reports, aggregated by two-year
-election cycle period.
+This endpoint provides information about a committee's form 3, 3x or 3p financial reports,
+aggregated by two-year period. We are referring to two year periods as `cycle`.
 
-For further description, see `/v1/committee/{committee_id}/reports`.
+The cycle is named after the even numbered year and includes the previous year. So, if you
+wanted to see the totals from 2013 and 2014, you would use 2014. The current cycle will be
+the next year, if the current is an odd-numbered year.
+
+For presidential and senatorial candidates, there are multiple 2-year cycles in-between
+their elections.
 '''
