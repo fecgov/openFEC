677bb231f78dddbe56a6c053b3ed83ec0aeea1f7
LindsayYoung <lindsayn.young@gsa.gov>
2015-06-16 10:47:29 -0700

    First pass at reports not working yet

---
+++ data/sql_updates/create_reports_ie_only.sql
@@ -0,0 +1,48 @@
+drop view if exists ofec_totals_independent_vw;
+drop materialized view if exists ofec_totals_independent_mv_tmp;
+create materialized view ofec_totals_independent_mv_tmp as
+select
+    row_number() over () as idx,
+    cmte_id as committee_id,
+    factindpexpcontb_f5_sk as key,
+    form_5_sk as form_key,
+    rpt_tp as report_type,
+    two_yr_period_sk as cycle,
+    -- will this stay the same?
+    transaction_sk as transaction_id,
+    start_date.dw_date as coverage_start_date,
+    end_date.dw_date as coverage_end_date,
+    rpt_yr as report_year,
+    ttl_indt_contb as total_independent_contributions,
+    ttl_indt_exp as total_independent_expenditures,
+    filer_sign_dt as filer_sign_date,
+    notary_sign_dt as notary_sign_date,
+    notary_commission_exprtn_dt as notary_commission_experation_date,
+    begin_image_num as beginning_image_number,
+    end_image_num as end_image_number,
+    ief5.load_date as load_date,
+    ief5.expire_date as expire_date,
+    rpt_tp as report_type,
+    electiontp as election_type,
+    rpt_tp_desc as report_type_full
+from
+    dimcmte c
+    inner join factindpexpcontb_f5 ief5 on indv_org_sk = c.cmte_sk
+    left join dimreporttype rt using (reporttype_sk)
+    left join dimelectiontp et using (electiontp_sk)
+    left join dimdates start_date on cvg_start_dt_sk = start_date.date_sk and cvg_start_dt_sk != 1
+    left join dimdates end_date on cvg_end_dt_sk = end_date.date_sk and cvg_end_dt_sk != 1
+where
+    two_yr_period_sk >= 1999--:START_YEAR
+    and ief5.expire_date is null
+;
+
+create unique index on ofec_totals_independent_mv_tmp(idx);
+
+create index on ofec_totals_independent_mv_tmp(cycle);
+create index on ofec_totals_independent_mv_tmp(report_type);
+create index on ofec_totals_independent_mv_tmp(report_year);
+create index on ofec_totals_independent_mv_tmp(committee_id);
+create index on ofec_totals_independent_mv_tmp(coverage_end_date);
+create index on ofec_totals_independent_mv_tmp(coverage_start_date);
+create index on ofec_totals_independent_mv_tmp(beginning_image_number);
