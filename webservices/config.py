d4b8041186f334c4536815949509125ec6349e04
Joshua Carp <jm.carp@gmail.com>
2015-06-22 18:03:04 -0400

    Check in missing files.

---
+++ webservices/config.py
@@ -0,0 +1,11 @@
+def get_cycle_start(year):
+    """Round year down to the first year of the two-year election cycle. Used
+    when filtering original data for election cycle.
+    """
+    return year if year % 2 == 1 else year - 1
+
+
+SQL_CONFIG = {
+    'START_YEAR': get_cycle_start(1980),
+    'START_YEAR_ITEMIZED': 2012,
+}
