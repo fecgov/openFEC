32fd1864327e853fb082649332913c48080d3bd9
LindsayYoung <lindsayn.young@gsa.gov>
2015-06-24 17:54:06 -0400

    Adding indexes

---
+++ data/sql_updates/create_filings_indexes.sql
@@ -0,0 +1,4 @@
+
+create index on public.vw_filing_history(committee_id);
+create index on public.vw_filing_history(begin_image_number);
+create index on public.vw_filing_history(coverage_end_date);
