a865501b1e333a6db81c6c42e8e8407e7f04af1d
LindsayYoung <lindsayn.young@gsa.gov>
2015-06-25 11:13:28 -0400

    Receipt date will be better for all forms since RAFI's don't have a coverage date.

--- data/sql_updates/create_filings_indexes.sql
+++ data/sql_updates/create_filings_indexes.sql
@@ -1,4 +1,4 @@
 
 create index on public.vw_filing_history(committee_id);
 create index on public.vw_filing_history(begin_image_numeric);
-create index on public.vw_filing_history(coverage_end_date);
+create index on public.vw_filing_history(receipt_date);
