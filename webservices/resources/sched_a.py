c99ab11d2ecd83470c727729da0f58773915f523
Joshua Carp <jm.carp@gmail.com>
2015-06-30 12:20:01 -0400

    Join Schedule A receipts with relevant committees.

--- webservices/resources/sched_a.py
+++ webservices/resources/sched_a.py
@@ -1,3 +1,5 @@
+import sqlalchemy as sa
+
 from webservices import args
 from webservices import docs
 from webservices import spec
@@ -49,6 +51,12 @@ class ScheduleAView(ItemizedResource):
     @schemas.marshal_with(schemas.ScheduleAPageSchema())
     def get(self, **kwargs):
         return super(ScheduleAView, self).get(**kwargs)
+
+    def build_query(self, kwargs):
+        query = super(ScheduleAView, self).build_query(kwargs)
+        query = query.options(sa.orm.joinedload(models.ScheduleA.committee))
+        query = query.options(sa.orm.joinedload(models.ScheduleA.contributor))
+        return query
 
     def join_fulltext(self, query):
         return query.join(
