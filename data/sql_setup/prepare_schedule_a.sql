5f48ad8df9ffe2f79d20a8b76895bfc73d2a1279
Joshua Carp <jm.carp@gmail.com>
2015-07-07 12:03:34 -0400

    Speed up queries by committee.

--- data/sql_setup/prepare_schedule_a.sql
+++ data/sql_setup/prepare_schedule_a.sql
@@ -2,7 +2,6 @@
 create index on sched_a (rpt_yr) where rpt_yr >= :START_YEAR_ITEMIZED;
 create index on sched_a (image_num) where rpt_yr >= :START_YEAR_ITEMIZED;
 create index on sched_a (sched_a_sk) where rpt_yr >= :START_YEAR_ITEMIZED;
-create index on sched_a (cmte_id) where rpt_yr >= :START_YEAR_ITEMIZED;
 create index on sched_a (contbr_id) where rpt_yr >= :START_YEAR_ITEMIZED;
 create index on sched_a (contbr_st) where rpt_yr >= :START_YEAR_ITEMIZED;
 create index on sched_a (contbr_city) where rpt_yr >= :START_YEAR_ITEMIZED;
@@ -10,6 +9,12 @@ create index on sched_a (contbr_city) where rpt_yr >= :START_YEAR_ITEMIZED;
 -- Create composite indices on sortable columns
 create index on sched_a (contb_receipt_dt, sched_a_sk) where rpt_yr >= :START_YEAR_ITEMIZED;
 create index on sched_a (contb_receipt_amt, sched_a_sk) where rpt_yr >= :START_YEAR_ITEMIZED;
+
+-- Create composite indices on `cmte_id`; else filtering by committee can be very slow
+-- TODO(jmcarp) Find a better solution
+create index on sched_a (cmte_id, sched_a_sk) where rpt_yr >= :START_YEAR_ITEMIZED;
+create index on sched_a (cmte_id, contb_receipt_dt, sched_a_sk) where rpt_yr >= :START_YEAR_ITEMIZED;
+create index on sched_a (cmte_id, contb_receipt_amt, sched_a_sk) where rpt_yr >= :START_YEAR_ITEMIZED;
 
 -- Create Schedule A fulltext table
 drop table if exists ofec_sched_a_fulltext;
