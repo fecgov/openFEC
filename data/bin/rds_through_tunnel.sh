3a3b06690d44248cf104afba9a8a143bab5388ee
Joshua Carp <jm.carp@gmail.com>
2015-07-07 12:14:01 -0400

    Remove unused code.

--- data/bin/rds_through_tunnel.sh
+++
@@ -1,1 +0,0 @@
-psql $@ -h 127.0.0.1 -p 63336 cfdm openfec
--- data/ddl/psql_w_vars.sh
+++
@@ -1,6 +0,0 @@
-# Start a psql session with OS-level environment variables 
-# available within SQL as `:rds_host_loc`, etc.  
-# Run this on data bridge machine
-# for session that will connect to both FEC/Oracle and 18F/RDS.
-
-psql -v rds_password="'$RDS_PASSWORD'" -v rds_host_loc="'$RDS_HOST'" -v oracle_server_loc="'$ORACLE_SERVER_LOC'" -v oracle_password="'$ORACLE_PASSWORD'" $@
