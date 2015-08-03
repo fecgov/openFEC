cf44685b5e9bc5a5332cda5bad416c75683b856c
LindsayYoung <lindsayn.young@gsa.gov>
2015-08-03 14:55:46 -0400

    Working dates with upcoming=True option

--- tests/test_dates.py
+++ tests/test_dates.py
@@ -2,7 +2,7 @@
 
 import datetime
 
-from webservices.rest import db, api
+from webservices.rest import api
 from webservices.resources.dates import ReportingDatesView
 
 from tests import factories
@@ -13,18 +13,18 @@ class TestReportingDates(ApiBaseTest):
     def test_reporting_dates_filters(self):
         [
             factories.ReportingDatesFactory(due_date=datetime.datetime(2014, 1, 2)),
-            factories.ReportingDatesFactory(report_year=datetime.datetime(2014, 2, 2)),
+            factories.ReportingDatesFactory(report_year=2015),
             factories.ReportingDatesFactory(report_type='YE'),
             factories.ReportingDatesFactory(create_date=datetime.datetime(2014, 3, 2)),
             factories.ReportingDatesFactory(update_date=datetime.datetime(2014, 4, 2)),
         ]
 
         filter_fields = (
-            ('due_date', '01-02-2014'),
-            ('report_year', '01-03-2014'),
+            ('due_date', '2014-01-02'),
+            ('report_year', 2015),
             ('report_type', 'YE'),
-            ('create_date', '01-04-2014'),
-            ('update_date', '01-05-2014'),
+            ('create_date', '2014-03-02'),
+            ('update_date', '2014-04-02'),
         )
 
         # checking one example from each field
@@ -38,4 +38,14 @@ class TestReportingDates(ApiBaseTest):
             self.assertGreater(len(results), 0)
             # doesn't return all results
             response = self._response(page)
-    # def test_next(self):
+
+    def test_upcoming(self):
+        [
+            factories.ReportingDatesFactory(report_type='YE', due_date=datetime.datetime(2014, 1, 2)),
+            factories.ReportingDatesFactory(report_type='YE', due_date=datetime.datetime(2017, 1, 3)),
+        ]
+
+        page = api.url_for(ReportingDatesView, **{'upcoming': 'true'})
+        results = self._results(page)
+
+        self.assertEquals(len(results), 1)
