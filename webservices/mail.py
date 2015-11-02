86ff1145ddc887ca8f407b7b3f9c2d7daa0b8b22
Joshua Carp <jm.carp@gmail.com>
2015-11-02 10:08:16 -0500

    Add email unit tests.

--- webservices/mail.py
+++ webservices/mail.py
@@ -31,14 +31,20 @@ def send_mail(buffer):
     settings = json.loads(os.getenv('VCAP_APPLICATION', '{}'))
     message = {
         'text': buffer.getvalue(),
+        'subject': get_subject(settings),
         'from_email': os.getenv('FEC_EMAIL_SENDER'),
-        'subject': 'FEC Update: {space}|{app}'.format(
-            space=settings.get('space-name', 'unknown-space'),
-            app=settings.get('name', 'unknown-app'),
-        ),
-        'to': [
-            {'email': each}
-            for each in os.getenv('FEC_EMAIL_RECIPIENTS').split(',')
-        ],
+        'to': get_recipients(os.getenv('FEC_EMAIL_RECIPIENTS')),
     }
     client.messages.send(message=message, async=False)
+
+def get_subject(settings):
+    return 'FEC Update: {space} | {app}'.format(
+        space=settings.get('space-name', 'unknown-space'),
+        app=settings.get('name', 'unknown-app'),
+    )
+
+def get_recipients(recipients):
+    return [
+        {'email': each}
+        for each in recipients.split(',')
+    ]
