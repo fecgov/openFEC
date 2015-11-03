40a4aee3e45df4aca5155b5ef89cdac2631fdafd
Joshua Carp <jm.carp@gmail.com>
2015-11-03 17:22:28 -0500

    Use coalesce for more readable queries.

--- data/functions/individual.sql
+++ data/functions/individual.sql
@@ -10,7 +10,7 @@ $$ language plpgsql immutable;
 
 create or replace function is_coded_individual(receipt_type text) returns bool as $$
 begin
-    return receipt_type is not null and receipt_type in ('10', '15', '15E', '15J', '18J');
+    return coalesce(receipt_type, '') in ('10', '15', '15E', '15J', '18J');
 end
 $$ language plpgsql immutable;
 
@@ -18,8 +18,8 @@ $$ language plpgsql immutable;
 create or replace function is_inferred_individual(amount numeric, line_number text, memo_code text, memo_text text) returns bool as $$
 begin
     return (
-        amount is not null and amount < 200 and
-        line_number is not null and line_number in ('11AI', '12', '17', '17A', '18') and
+        amount < 200 and
+        coalesce(line_number, '') in ('11AI', '12', '17', '17A', '18') and
         not is_earmark(memo_code, memo_text)
     );
 end
@@ -29,8 +29,8 @@ $$ language plpgsql immutable;
 create or replace function is_earmark(memo_code text, memo_text text) returns bool as $$
 begin
   return (
-      memo_code is not null and memo_code = 'X' and
-      memo_text is not null and memo_text ~* 'earmark|earmk|ermk'
+      coalesce(memo_code, '') = 'X' and
+      coalesce(memo_text, '') ~* 'earmark|earmk|ermk'
   );
 end
 $$ language plpgsql immutable;
