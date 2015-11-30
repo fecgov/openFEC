3d22ff6edf98cc1bfd2046aa02dd7ad2a6176172
Joshua Carp <jm.carp@gmail.com>
2015-11-30 15:31:03 -0500

    Initialize new relic from application.

--- bin/run.sh
+++ bin/run.sh
@@ -1,4 +1,4 @@
 python manage.py cf_startup && \
     (pkill gunicorn || true) && \
-    newrelic-admin run-program gunicorn webservices.rest:app --bind 127.0.0.1:8000 --daemon && \
+    gunicorn webservices.rest:app --bind 127.0.0.1:8000 --daemon && \
     bash ./boot.sh
