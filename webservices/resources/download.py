9c8b77b47fcf409d353d0247a592a5ac7609b607
Joshua Carp <jm.carp@gmail.com>
2015-12-02 17:10:49 -0500

    Allow custom filenames.

--- webservices/resources/download.py
+++ webservices/resources/download.py
@@ -1,4 +1,6 @@
 import furl
+from marshmallow import fields
+from flask_apispec import use_kwargs
 
 import boto3
 from botocore.exceptions import ClientError
@@ -11,13 +13,16 @@ from webservices.tasks import utils as task_utils
 
 client = boto3.client('s3')
 
+URL_EXPIRY = 7 * 24 * 60 * 60
+
 class DownloadView(utils.Resource):
 
-    def get(self, path):
+    @use_kwargs({'filename': fields.Str(missing=None)})
+    def post(self, path, filename=None):
         parts = request.path.split('/')
         parts.remove('download')
         path = '/'.join(parts)
-        cached_file = get_cached_file(path, request.query_string)
+        cached_file = get_cached_file(path, request.query_string, filename=filename)
         if cached_file:
             return {
                 'status': 'complete',
@@ -26,7 +31,7 @@ class DownloadView(utils.Resource):
         download.export_query.delay(path, request.query_string)
         return {'status': 'queued'}
 
-def get_cached_file(path, qs):
+def get_cached_file(path, qs, filename=None):
     key = download.get_s3_name(path, qs)
     obj = task_utils.get_bucket().Object(key=key)
     try:
@@ -34,6 +39,19 @@ def get_cached_file(path, qs):
         client = obj.meta.client
         url = furl.furl(client.meta.endpoint_url)
         url.path.segments = [obj.bucket_name, obj.key]
-        return url.url
+        return get_download_url(obj, filename=filename)
     except ClientError:
         return None
+
+def get_download_url(obj, filename=None):
+    params = {
+        'Key': obj.key,
+        'Bucket': obj.bucket_name,
+    }
+    if filename:
+        params['ResponseContentDisposition'] = 'filename={}'.format(filename)
+    return obj.meta.client.generate_presigned_url(
+        'get_object',
+        Params=params,
+        ExpiresIn=URL_EXPIRY,
+    )
