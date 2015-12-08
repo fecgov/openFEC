e67cfe94ee14c6ff0c31217312f8dfddcda678e3
Joshua Carp <jm.carp@gmail.com>
2015-12-07 23:58:45 -0500

    Weight typeahead search by committee receipts.

--- data/sql_updates/create_committee_history.sql
+++ data/sql_updates/create_committee_history.sql
@@ -132,31 +132,3 @@ create index on ofec_committee_detail_mv_tmp(organization_type_full);
 
 create index on ofec_committee_detail_mv_tmp using gin (cycles);
 create index on ofec_committee_detail_mv_tmp using gin (candidate_ids);
-
-drop table if exists dimcmte_fulltext;
-drop materialized view if exists ofec_committee_fulltext_mv_tmp;
-create materialized view ofec_committee_fulltext_mv_tmp as
-with pacronyms as (
-    select
-        "ID NUMBER" as committee_id,
-        string_agg("PACRONYM", ' ') as pacronyms
-    from ofec_pacronyms
-    group by committee_id
-)
-select distinct on (committee_id)
-    row_number() over () as idx,
-    committee_id as id,
-    name,
-    case
-        when name is not null then
-            setweight(to_tsvector(name), 'A') ||
-            setweight(to_tsvector(coalesce(pac.pacronyms, '')), 'A') ||
-            setweight(to_tsvector(committee_id), 'B')
-        else null::tsvector
-    end as fulltxt
-from ofec_committee_detail_mv_tmp cd
-left join pacronyms pac using (committee_id)
-;
-
-create unique index on ofec_committee_fulltext_mv_tmp(idx);
-create index on ofec_committee_fulltext_mv_tmp using gin(fulltxt);
