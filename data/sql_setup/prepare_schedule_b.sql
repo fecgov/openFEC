ca5cebb4b54aacbfc8579e9ea9aac5ca0772d14a
Joshua Carp <jm.carp@gmail.com>
2015-12-29 17:49:52 -0500

    Delete unused SQL code.

--- data/sql_setup/prepare_schedule_b.sql
+++ data/sql_setup/prepare_schedule_b.sql
@@ -1,48 +1,5 @@
--- Create Schedule B table
-drop table if exists ofec_sched_b_tmp;
-create table ofec_sched_b_tmp as
-select
-    *,
-    cast(null as timestamp) as timestamp,
-    to_tsvector(recipient_nm) as recipient_name_text,
-    to_tsvector(disb_desc) as disbursement_description_text,
-    disbursement_purpose(disb_tp, disb_desc) as disbursement_purpose_category,
-    clean_repeated(recipient_cmte_id, cmte_id) as clean_recipient_cmte_id
-from sched_b
-where rpt_yr >= :START_YEAR_ITEMIZED
-;
-
-alter table ofec_sched_b_tmp add primary key (sched_b_sk);
-
--- Create simple indices on filtered columns
-create index on ofec_sched_b_tmp (rpt_yr);
-create index on ofec_sched_b_tmp (image_num);
-create index on ofec_sched_b_tmp (sched_b_sk);
-create index on ofec_sched_b_tmp (recipient_st);
-create index on ofec_sched_b_tmp (recipient_city);
-create index on ofec_sched_b_tmp (clean_recipient_cmte_id);
-
--- Create composite indices on sortable columns
-create index on ofec_sched_b_tmp (disb_dt, sched_b_sk);
-create index on ofec_sched_b_tmp (disb_amt, sched_b_sk);
-
--- Create composite indices on `cmte_id`; else filtering by committee can be very slow
-create index on ofec_sched_b_tmp (cmte_id, sched_b_sk);
-create index on ofec_sched_b_tmp (cmte_id, disb_dt, sched_b_sk);
-create index on ofec_sched_b_tmp (cmte_id, disb_amt, sched_b_sk);
-
--- Create indices on fulltext columns
-create index on ofec_sched_b_tmp using gin (recipient_name_text);
-create index on ofec_sched_b_tmp using gin (disbursement_description_text);
-
 -- Create index for join on electioneering costs
 create index on sched_b (link_id);
-
--- Use smaller histogram bins on state column for faster queries on rare states (AS, PR)
-alter table ofec_sched_b_tmp alter column recipient_st set statistics 1000;
-
--- Analyze tables
-analyze ofec_sched_b_tmp;
 
 -- Create queue tables to hold changes to Schedule B
 drop table if exists ofec_sched_b_queue_new;
@@ -89,6 +46,3 @@ drop trigger if exists ofec_sched_b_queue_trigger on sched_b;
 create trigger ofec_sched_b_queue_trigger before insert or update or delete
     on sched_b for each row execute procedure ofec_sched_b_update_queues(:START_YEAR_AGGREGATE)
 ;
-
-drop table if exists ofec_sched_b;
-alter table ofec_sched_b_tmp rename to ofec_sched_b;
