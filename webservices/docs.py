9d8e17cd990a01bcccb7eebcc78ff3e0b13f223b
LindsayYoung <lindsayn.young@gsa.gov>
2016-01-07 13:32:46 -0800

    Fixed typos. thanks @emelighoutlaw

--- webservices/docs.py
+++ webservices/docs.py
@@ -17,24 +17,24 @@ A unique identifier assigned to each committee or filer registered with the FEC.
 
 CANDIDATE_CYCLE = '''
 Two-year election cycle in which a candidate runs for office.
-Calculated from FEC form 2. The cycle begins with
-an odd year and is named for its even ending year. This cycle follows
+Calculated from FEC Form 2. The cycle begins with
+an odd year and is named for its ending, even year. This cycle follows
 the traditional house election cycle and subdivides the presidential
-and senate elections into comparable two-year blocks. To see data for
-the entire 4-years of a presidential term or 6-years of a senatorial term,
+and Senate elections into comparable two-year blocks. To see data for
+the entire four years of a presidential term or six years of a senatorial term,
 you will need the `election_full` flag.
 '''
 
 COMMITTEE_CYCLE = '''
 A two year election cycle that the committee was active- (after original registration
-date but before expiration date in FEC form 1s) The cycle begins with
-an odd year and is named for its even ending year.
+date but before expiration date in FEC Form 1s) The cycle begins with
+an odd year and is named for its ending, even year.
 '''
 
 RECORD_CYCLE = '''
 Filter records to only those that were applicable to a given
 two-year period.The cycle begins with an odd year and is named
-for its even ending year.
+for its ending, even year.
 '''
 
 RECORD_YEAR = '''
@@ -44,10 +44,10 @@ Filter records to only those that were applicable to a given year.
 # committee uses a different definition for cycle because it is less straight forward
 CYCLE = '''
 Filter records to only those that are applicable to a given two-year
-period. This cycle follows the traditional house election cycle and
-subdivides the presidential and senate elections into comparable
+period. This cycle follows the traditional House election cycle and
+subdivides the presidential and Senate elections into comparable
 two-year blocks. The cycle begins with an odd year and is named for its
-even ending year.
+ending, even year.
 '''
 
 DESCRIPTION = '''
@@ -219,9 +219,9 @@ paper filings take longer to process.
 
 The financial endpoints use data from FEC [form 5](http://www.fec.gov/pdf/forms/fecfrm5.pdf),
 for independent expenditors; or the summary and detailed summary pages of the FEC
-[form 3](http://www.fec.gov/pdf/forms/fecfrm3.pdf), for House and Senate committees;
-[form 3X](http://www.fec.gov/pdf/forms/fecfrm3x.pdf), for PACs and parties;
-and [form 3P](http://www.fec.gov/pdf/forms/fecfrm3p.pdf), for presidential committees.
+[Form 3](http://www.fec.gov/pdf/forms/fecfrm3.pdf), for House and Senate committees;
+[Form 3X](http://www.fec.gov/pdf/forms/fecfrm3x.pdf), for PACs and parties;
+and [Form 3P](http://www.fec.gov/pdf/forms/fecfrm3p.pdf), for presidential committees.
 '''
 
 REPORTS = '''
