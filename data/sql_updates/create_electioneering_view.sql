3ee6948eab72f979cec12cfde0c891ce6abeff01
LindsayYoung <lindsayn.young@gsa.gov>
2016-01-08 16:25:27 -0800

    fix typos

--- data/sql_updates/create_electioneering_view.sql
+++ data/sql_updates/create_electioneering_view.sql
@@ -4,10 +4,10 @@ create materialized view ofec_electioneering_mv_tmp as
 select
     electioneering_com_vw.*,
     row_number() over () as idx,
-    -- doube check with FEC, but they all say F94 in the current site, not sure we need this
+    -- double check with FEC, but they all say F94 in the current site, not sure we need this
     'F9'::text as form_tp
 from electioneering_com_vw
-where rpt_yr >= 2007--:START_YEAR
+where rpt_yr >= :START_YEAR
 ;
 
 create unique index on ofec_electioneering_mv_tmp (idx);
@@ -25,7 +25,7 @@ create index on ofec_electioneering_mv_tmp (cand_office_district);
 -- create index on ofec_electioneering_mv_tmp (amndt_ind);
 -- Need this to match other tables
 -- create index on ofec_electioneering_mv_tmp (receipt_dt);
--- It is eaither E or P. I am not sure what that stands for, it is not in the new table
+-- It is either E or P. I am not sure what that stands for, it is not in the new table
 -- create index on ofec_electioneering_mv_tmp (filing_type);
 -- not disb_amt in this table
 create index on ofec_electioneering_mv_tmp (reported_disb_amt);
