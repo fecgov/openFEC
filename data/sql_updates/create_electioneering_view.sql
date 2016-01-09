d47a38a18b533bbd68f7b371fe4c5fd502ba4dd9
LindsayYoung <lindsayn.young@gsa.gov>
2016-01-08 16:21:11 -0800

    transitioning to the new table, some fields are missing

--- data/sql_updates/create_electioneering_view.sql
+++ data/sql_updates/create_electioneering_view.sql
@@ -1,27 +1,32 @@
 drop materialized view if exists ofec_electioneering_mv_tmp;
 create materialized view ofec_electioneering_mv_tmp as
-select distinct on (electioneering_com_vw.form_94_sk)
+-- there is no unique identifier
+select
     electioneering_com_vw.*,
+    row_number() over () as idx,
     -- doube check with FEC, but they all say F94 in the current site, not sure we need this
-    'F9' as form_tp
+    'F9'::text as form_tp
 from electioneering_com_vw
-where sched_b.rpt_yr >= :START_YEAR
+where rpt_yr >= 2007--:START_YEAR
 ;
 
-create unique index on ofec_electioneering_mv_tmp (form_94_sk);
+create unique index on ofec_electioneering_mv_tmp (idx);
 
--- These are all from F9 Schedule B, I guess we can keep this for consistency 
+-- These are all from F9 Schedule B, I guess we can keep this for consistency
 create index on ofec_electioneering_mv_tmp (form_tp);
 create index on ofec_electioneering_mv_tmp (cmte_id);
 create index on ofec_electioneering_mv_tmp (cand_id);
 create index on ofec_electioneering_mv_tmp (cand_office);
 create index on ofec_electioneering_mv_tmp (cand_office_st);
 create index on ofec_electioneering_mv_tmp (cand_office_district);
-create index on ofec_electioneering_mv_tmp (election_tp);
+-- not in this table
+-- create index on ofec_electioneering_mv_tmp (election_tp);
+-- not in this table, confirm all amendment info has been applied
 -- create index on ofec_electioneering_mv_tmp (amndt_ind);
-create index on ofec_electioneering_mv_tmp (receipt_dt);
--- It is eaither E or P. I am not sure what that stands for 
-create index on ofec_electioneering_mv_tmp (filing_type);
+-- Need this to match other tables
+-- create index on ofec_electioneering_mv_tmp (receipt_dt);
+-- It is eaither E or P. I am not sure what that stands for, it is not in the new table
+-- create index on ofec_electioneering_mv_tmp (filing_type);
 -- not disb_amt in this table
 create index on ofec_electioneering_mv_tmp (reported_disb_amt);
 -- this is want we want to use for per candidate calculations
