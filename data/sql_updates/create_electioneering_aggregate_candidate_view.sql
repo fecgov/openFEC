bb8aef6a546d171d4875153b448bee42fe790fb4
LindsayYoung <lindsayn.young@gsa.gov>
2016-01-22 13:46:38 -0500

    Adding report year filtering and removing the disbursements join

--- data/sql_updates/create_electioneering_aggregate_candidate_view.sql
+++ data/sql_updates/create_electioneering_aggregate_candidate_view.sql
@@ -1,10 +1,5 @@
 drop materialized view if exists ofec_electioneering_aggregate_candidate_mv_tmp;
 create materialized view ofec_electioneering_aggregate_candidate_mv_tmp as
-with disbursements as (
-    select * from sched_b
-    where link_id in (select link_id from form_94)
-    and rpt_yr >= 2002
-)
 select
     row_number() over () as idx,
     cmte_id,
@@ -13,6 +8,7 @@ select
     count(calculated_cand_share) as count,
     rpt_yr + rpt_yr % 2 as cycle
 from electioneering_com_vw
+    where rpt_yr >= :START_YEAR
 group by
     cmte_id,
     cand_id,
