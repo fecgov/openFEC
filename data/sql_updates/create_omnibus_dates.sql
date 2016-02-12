3573702132dd9496dd003c03a61625aa08dd0da6
LindsayYoung <lindsayn.young@gsa.gov>
2016-02-12 18:16:34 -0500

    reformats report code, Resolves #1508

--- data/sql_updates/create_omnibus_dates.sql
+++ data/sql_updates/create_omnibus_dates.sql
@@ -139,9 +139,8 @@ with elections_raw as(
         array_to_string(array[
             expand_office_description(office_sought::text),
             clean_report(rpt_tp_desc::text),
-            'Due, Report (',
+            'Due, Report code:',
             report_type::text,
-            ') ',
             array_to_string(array_agg(election_state order by election_state)::text[], ', ')
         ], ' ') as summary,
         array_agg(election_state)::text[] as states,
