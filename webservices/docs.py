711f1a2672f98fad085ffdab91beb93eab56ed67
LindsayYoung <lindsayn.young@gsa.gov>
2016-03-03 14:55:14 -0500

    This is not working because I tried replacing the table at the end and adding more fields, but that kills the linkages.

--- webservices/docs.py
+++ webservices/docs.py
@@ -552,6 +552,8 @@ CANDIDATE_STATUS = 'One-letter code explaining if the candidate is:\n\
 INCUMBENT_CHALLENGE = "One-letter code ('I', 'C', 'O') explaining if the candidate is an incumbent, a challenger, or if the seat is open."
 INCUMBENT_CHALLENGE_FULL = 'Explains if the candidate is an incumbent, a challenger, or if the seat is open.'
 ACTIVE_THROUGH = 'Last year a candidate was active. This field is specific to the candidate_id so if the same person runs for another office, there may be a different record for them.'
+FIVE_THOUSAND_FLAG = 'A boolean that describes if a candidate has ever raised $5,000 in their campaign for this particular office. (Candidates have separate candidate IDs for each office.)'
+FEDERAL_FUNDS_FLAG = 'A boolean the describes if a presidential candidate has accepted federal funds. The flag will be false for House and Senate candidates.'
 
 # committees
 COMMITTEE_NAME = 'The name of the committee. If a committee changes its name, \
