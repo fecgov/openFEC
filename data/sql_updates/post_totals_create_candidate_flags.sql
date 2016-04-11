07cf14564d4ce6ddd227ca9649eb6485baed84f8
LindsayYoung <lindsayn.young@gsa.gov>
2016-04-11 17:29:41 -0400

    Fix refresh error

--- data/sql_updates/post_totals_create_candidate_flags.sql
+++ data/sql_updates/post_totals_create_candidate_flags.sql
@@ -2,6 +2,7 @@
 
 create materialized view ofec_candidate_flag_tmp as
 select
+    row_number() over () as idx,
     candidate_id,
     array_agg(five_thousand_flag)::boolean array @> array[cast('true' as boolean)] as five_thousand_flag,
     array_agg(federal_funds_flag)::boolean array @> array[cast('true' as boolean)] as federal_funds_flag
@@ -9,6 +10,7 @@ from ofec_candidate_totals_mv_tmp
 group by candidate_id
 ;
 
+create unique index on ofec_candidate_flag_tmp(idx);
 create index on ofec_candidate_flag_tmp(candidate_id);
 create index on ofec_candidate_flag_tmp(five_thousand_flag);
 create index on ofec_candidate_flag_tmp(federal_funds_flag);
