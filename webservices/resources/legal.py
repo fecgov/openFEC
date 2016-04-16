1a3efe2d03d0f383dfa70f082144faea27c68176
Anthony Garvan <anthony.garvan@gsa.gov>
2016-04-15 19:48:03 -0500

    flake8ing

--- webservices/resources/legal.py
+++ webservices/resources/legal.py
@@ -1,11 +1,6 @@
-from flask_apispec import doc, marshal_with
-
 from webservices import args
-from webservices import docs
 from webservices import utils
 from webservices.utils import use_kwargs
-from webargs import fields
-from flask import request
 
 es = utils.get_elasticsearch_connection()
 
