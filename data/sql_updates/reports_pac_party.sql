b8f3e65e5c9783384dd521b0b3505a61feddc0b9
LindsayYoung <lindsayn.young@gsa.gov>
2016-04-28 11:28:46 -0400

    woriking on pac/party replacement

--- data/sql_updates/reports_pac_party.sql
+++ data/sql_updates/reports_pac_party.sql
@@ -1,12 +1,11 @@
-drop view if exists ofec_reports_pacs_parties_vw;
 drop materialized view if exists ofec_reports_pacs_parties_mv_tmp;
 create materialized view ofec_reports_pacs_parties_mv_tmp as
 select
     row_number() over () as idx,
     cmte_id as committee_id,
-    two_yr_period_sk as cycle,
-    start_date.dw_date as coverage_start_date,
-    end_date.dw_date as coverage_end_date,
+    cycle,
+    cvg_start_dt as coverage_start_date,
+    cvg_end_dt as coverage_end_date,
     all_loans_received_per as all_loans_received_period,
     all_loans_received_ytd as all_loans_received_ytd,
     shared_fed_actvy_nonfed_per as allocated_federal_election_levin_share_period,
@@ -46,9 +45,8 @@ select
     non_alloc_fed_elect_actvy_per as non_allocated_fed_election_activity_period,
     non_alloc_fed_elect_actvy_ytd as non_allocated_fed_election_activity_ytd,
     shared_nonfed_op_exp_per as nonfed_share_allocated_disbursements_period,
-    greatest(offsets_to_op_exp_per_i, offsets_to_op_exp_per_ii
-    ) as offsets_to_operating_expenditures_period,
-    greatest(offsets_to_op_exp_ytd_i, offsets_to_op_exp_ytd_ii) as offsets_to_operating_expenditures_ytd,
+    ttl_op_exp_per as offsets_to_operating_expenditures_period,
+    ttl_op_exp_ytd as offsets_to_operating_expenditures_ytd,
     other_disb_per as other_disbursements_period,
     other_disb_ytd as other_disbursements_ytd,
     other_fed_op_exp_per as other_fed_operating_expenditures_period,
@@ -57,12 +55,12 @@ select
     other_fed_receipts_ytd as other_fed_receipts_ytd,
     -- in this case, i and ii are not the same thing
     -- if they have ii they are refunds and i are contributions
-    other_pol_cmte_contb_per_ii as refunded_other_political_committee_contributions_period,
-    other_pol_cmte_contb_ytd_ii as refunded_other_political_committee_contributions_ytd,
-    other_pol_cmte_contb_per_i as other_political_committee_contributions_period,
+    --other_pol_cmte_contb_per_ii as refunded_other_political_committee_contributions_period,
+    --other_pol_cmte_contb_ytd_ii as refunded_other_political_committee_contributions_ytd,
+    --other_pol_cmte_contb_per_i as other_political_committee_contributions_period,
     other_pol_cmte_contb_ytd_i as other_political_committee_contributions_ytd,
-    pol_pty_cmte_contb_per_ii as refunded_political_party_committee_contributions_period,
-    pol_pty_cmte_contb_ytd_ii as refunded_political_party_committee_contributions_ytd,
+    --pol_pty_cmte_contb_per_ii as refunded_political_party_committee_contributions_period,
+    --pol_pty_cmte_contb_ytd_ii as refunded_political_party_committee_contributions_ytd,
     pol_pty_cmte_contb_per_i as political_party_committee_contributions_period,
     pol_pty_cmte_contb_ytd_i as political_party_committee_contributions_ytd,
     rpt_yr as report_year,
@@ -75,12 +73,12 @@ select
     shared_nonfed_op_exp_ytd as shared_nonfed_operating_expenditures_ytd,
     subttl_sum_page_per as subtotal_summary_page_period,
     subttl_sum_ytd as subtotal_summary_ytd,
-    greatest(ttl_contb_ref_per_i, ttl_contb_ref_per_ii) as total_contribution_refunds_period,
-    greatest(ttl_contb_ref_ytd_i, ttl_contb_ref_ytd_ii) as total_contribution_refunds_ytd,
+    ttl_contb_refund as total_contribution_refunds_period,
+    ttl_contb_refund_ytd as total_contribution_refunds_ytd,
     ttl_contb_per as total_contributions_period,
     ttl_contb_ytd as total_contributions_ytd,
-    greatest(ttl_disb_sum_page_per, ttl_disb_per) as total_disbursements_period,
-    greatest(ttl_disb_sum_page_ytd, ttl_disb_ytd) as total_disbursements_ytd,
+    ttl_disb as total_disbursements_period,
+    ttl_disb_ytd as total_disbursements_ytd,
     ttl_fed_disb_per as total_fed_disbursements_period,
     ttl_fed_disb_ytd as total_fed_disbursements_ytd,
     ttl_fed_elect_actvy_per as total_fed_election_activity_period,
@@ -95,8 +93,8 @@ select
     ttl_nonfed_tranf_ytd as total_nonfed_transfers_ytd,
     ttl_op_exp_per as total_operating_expenditures_period,
     ttl_op_exp_ytd as total_operating_expenditures_ytd,
-    greatest(ttl_receipts_sum_page_per, ttl_receipts_per) as total_receipts_period,
-    greatest(ttl_receipts_sum_page_ytd, ttl_receipts_ytd) as total_receipts_ytd,
+    ttl_receipts as total_receipts_period,
+    ttl_receipts_ytd as total_receipts_ytd,
     tranf_from_affiliated_pty_per as transfers_from_affiliated_party_period,
     tranf_from_affiliated_pty_ytd as transfers_from_affiliated_party_ytd,
     tranf_from_nonfed_acct_per as transfers_from_nonfed_account_period,
@@ -107,25 +105,21 @@ select
     tranf_to_affilitated_cmte_ytd as transfers_to_affilitated_committees_ytd,
     rpt_tp as report_type,
     rpt_tp_desc as report_type_full,
-    f3x.expire_date as expire_date,
-    f3x.load_date as load_date
+    most_recent_filing_flag like 'N' as is_amended,
+    receipt_dt as receipt_date
 from
-    dimcmte c
-    inner join factpacsandparties_f3x f3x using (cmte_sk)
-    inner join dimreporttype rt using (reporttype_sk)
-    left join dimdates start_date on cvg_start_dt_sk = start_date.date_sk and cvg_start_dt_sk != 1
-    left join dimdates end_date on cvg_end_dt_sk = end_date.date_sk and cvg_end_dt_sk != 1
-where
-    two_yr_period_sk >= :START_YEAR
+    fec_vsum_f3x f3x
+-- where
+--     cycle >= :START_YEAR
 ;
 
 create unique index on ofec_reports_pacs_parties_mv_tmp(idx);
 
 create index on ofec_reports_pacs_parties_mv_tmp(cycle, idx);
-create index on ofec_reports_pacs_parties_mv_tmp(expire_date, idx);
 create index on ofec_reports_pacs_parties_mv_tmp(report_type, idx);
 create index on ofec_reports_pacs_parties_mv_tmp(report_year, idx);
 create index on ofec_reports_pacs_parties_mv_tmp(committee_id, idx);
 create index on ofec_reports_pacs_parties_mv_tmp(coverage_end_date, idx);
 create index on ofec_reports_pacs_parties_mv_tmp(coverage_start_date, idx);
 create index on ofec_reports_pacs_parties_mv_tmp(beginning_image_number, idx);
+create index on ofec_reports_pacs_parties_mv_tmp(is_amended, idx);
