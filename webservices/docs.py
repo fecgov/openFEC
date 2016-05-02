194653961f0701d4b79c19004539025ff4eac417
Eric Mill <eric@konklone.com>
2016-05-02 10:37:48 -0400

    typo and URL fix

--- webservices/docs.py
+++ webservices/docs.py
@@ -73,7 +73,7 @@ out contributor information in schedule_a.
 Get an [API key here](https://api.data.gov/signup/). That will enable you to place up to 1,000
 calls an hour. Each call is limited to 100 results per page. You can also discuss the data in the
 [FEC data google group](https://groups.google.com/forum/#!forum/fec-data) or post questions
-to the [Open Data Stack Exchange](https://opendata.stackexchange.com/questions/ask?tags=fec). The model definations and schema are available at [/swagger](/swagger/). This is useful for making wrappers and exploring the data.
+to the [Open Data Stack Exchange](https://opendata.stackexchange.com/questions/ask?tags=fec). The model definitions and schema are available at [/swagger](/swagger/). This is useful for making wrappers and exploring the data.
 
 A few restrictions limit the way you can use FEC data. For example, you can’t use contributor
 lists for commercial purposes or to solicit donations.
@@ -446,7 +446,7 @@ Unitemized contributions are included in the `0` category.
 '''
 
 API_KEY_DESCRIPTION = '''
-API key for https://api.data.gov. Get one at http://api.data.gov/signup.
+API key for https://api.data.gov. Get one at https://api.data.gov/signup.
 '''
 SEARCH_TAG = '''
 Search for candidates or committees by name.
