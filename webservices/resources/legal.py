e0eeae3d15ff9783cdf25e50c0ff2735d4790f23
Anthony Garvan <anthony.garvan@gsa.gov>
2016-05-12 15:56:44 -0500

    adding regulations, reformatting query

--- webservices/resources/legal.py
+++ webservices/resources/legal.py
@@ -6,25 +6,38 @@ es = utils.get_elasticsearch_connection()
 
 class Search(utils.Resource):
     @use_kwargs(args.query)
-    def get(self, q, from_hit=0, hits_returned=20, **kwargs):
-        query = {"query": {"bool": {
-                            "must": {"match": {"_all": q}},
-                            "should": [{"match": {"AO_No": q}},
-                                           {"match_phrase": {"_all": {"query": q,
-                                                                      "slop":  50
-                                                                      }
-                                                             }
-                                            }]
-                                    }
-                            },
-                    "highlight": {"fields": {"text": {}}},
-                    "_source": {"exclude": "text"}
-                 }
+    def get(self, q, from_hit=0, hits_returned=20, type='all', **kwargs):
+        if type == 'all':
+            types = ['advisory_opinions', 'regulations']
+        else:
+            types = [type]
 
-        hits_returned = min([200, hits_returned])
-        results = es.search(query, index='docs', size=hits_returned,
-                         es_from=from_hit)
-        hits = results['hits']['hits']
+        results = {}
+        total_count = 0
+        for type in types:
+            query = {"query": {"bool": {
+                     "must": [{"match": {"_all": q}}, {"term": {"_type": type}}],
+                               "should": [{"match": {"no": q}},
+                                               {"match_phrase": {"_all": {"query": q,
+                                                                          "slop": 50}
+                                                                 }
+                                                }]
+                     }},
+                "highlight": {"fields": {"text": {}}},
+                "_source": {"exclude": "text"}}
 
-        return {'results': hits, 'count': results['hits']['total']}
+            hits_returned = min([200, hits_returned])
+            es_results = es.search(query, index='docs', size=hits_returned,
+                             es_from=from_hit)
+            hits = es_results['hits']['hits']
+            for hit in hits:
+                hit['_source']['highlights'] = hit['highlight']['text']
+            count = es_results['hits']['total']
+            total_count += count
+            formatted_hits = [h['_source'] for h in hits]
 
+            results[type] = formatted_hits
+            results['total_%s' % type] = count
+
+        results['total_all'] = total_count
+        return results
