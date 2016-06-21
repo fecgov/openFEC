90f3665723b9eefca06aa0bee617a51d9c44fda7
LindsayYoung <lindsayn.young@gsa.gov>
2016-06-20 17:10:38 -0700

    simplify API resource

--- webservices/resources/rad_analyst.py
+++ webservices/resources/rad_analyst.py
@@ -1,43 +1,43 @@
-import sqlalchemy as sa
-
-from flask_apispec import doc, marshal_with
+from flask_apispec import doc
 
 from webservices import args
 from webservices import docs
 from webservices import utils
-from webservices import filters
 from webservices import schemas
 from webservices.common import models
-from webservices.utils import use_kwargs
+from webservices.common.views import ApiResource
 
 
 @doc(
     tags=['filer_resources'],
-    description=docs.FILER_RESOURCES,
+    description=docs.RAD_ANALYST,
 )
-class RadAnalystView(utils.Resource):
+class RadAnalystView(ApiResource):
 
     model = models.RadAnalyst
+    schema = schemas.RadAnalystSchema
+    page_schema = schemas.RadAnalystPageSchema
 
     filter_fulltext_fields = [
         ('name', model.name_txt),
     ]
+
     filter_multi_fields = [
-        ('last_name', model.last_name),
-        ('first_name', model.first_name),
-        ('anlyst_id', model.analyst_id),
-        ('rad_branch', model.rad_branch),
+        ('analyst_id', model.analyst_id),
         ('telephone_ext', model.telephone_ext),
         ('committee_id', model.committee_id),
-        ('committe_name', model.committee_name),
     ]
 
-    @use_kwargs(args.rad_analyst)
-    @use_kwargs(args.make_sort_args(default='-last_name'))
-    @marshal_with(schemas.RadAnalystPageSchema)
-    def get(self, **kwargs):
-        query = filters.filter_fulltext(models.RadAnalyst.query, kwargs, self.filter_fulltext_fields)
-        query = query.order_by(
-            sa.desc(models.RadAnalyst.last_name)
-        ).limit(20)
-        return {'results': query.all()}
+    @property
+    def args(self):
+        return utils.extend(
+            args.paging,
+            args.rad_analyst,
+            args.make_sort_args(
+                validator=args.IndexValidator(models.RadAnalyst),
+            ),
+        )
+
+    @property
+    def index_column(self):
+        return self.model.idx
