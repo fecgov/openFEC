fcfbe0fe4f5834dd86251cdd37846ecc30f7de2c
LindsayYoung <lindsayn.young@gsa.gov>
2016-06-21 10:28:16 -0700

    Added last name index for sorting Took out duplicate factory sqlalchmy statement Fixed comment about not making sortable to filterable Adding print to the the missing model tests because is it useful

--- data/sql_updates/rad_analyst.sql
+++ data/sql_updates/rad_analyst.sql
@@ -3,7 +3,7 @@ create materialized view ofec_rad_mv_tmp as
 select
     row_number() over () as idx,
     cmte_id as committee_id,
-    -- I am not sure we want to sort on this, if the name is different here it will behave differently than the other committee searches by name
+    -- I am not sure we want to filter on this, if the name is different here it will behave differently than the other committee searches by name
     cmte_nm as committee_name,
     anlyst_id as analyst_id,
     rad_branch,
@@ -21,4 +21,5 @@ create index on ofec_rad_mv_tmp (analyst_id);
 create index on ofec_rad_mv_tmp (rad_branch);
 create index on ofec_rad_mv_tmp (telephone_ext);
 create index on ofec_rad_mv_tmp (committee_name);
+create index on ofec_rad_mv_tmp (last_name);
 create index on ofec_rad_mv_tmp using gin(name_txt);
