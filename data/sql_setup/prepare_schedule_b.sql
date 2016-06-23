c7c968400cddf1b4db7fc1785cd24678c730058e
Carlo Costino <carlo.costino@gsa.gov>
2016-06-22 22:28:32 -0400

    Fix Schedule A and B updates
    This changeset addresses an issue with failing updates for the Schedule A and B tables.  The update process did not take into account the table partitioning and was still basing the checks off of the report year (`rpt_yr`) instead of the new calculated value (`two_year_transaction_period`).  In order to fix this, I had to do the following:
    
    * Add the `two_year_transaction_period` column to both sets of queue tables for Schedule A and B.
    * Remove the `two_year_transaction_period` column from the column factory in the select statement generation for processing updates.
    
    I do not believe this is entirely straightforward so there is a comment in the code that refreshes the child tables during an update process to help explain the reasoning behind what is happening.
    
    h/t to @LindsayYoung for working through the approach with me!

--- data/sql_setup/prepare_schedule_b.sql
+++ data/sql_setup/prepare_schedule_b.sql
@@ -8,34 +8,43 @@ create table ofec_sched_b_queue_new as select * from sched_b limit 0;
 create table ofec_sched_b_queue_old as select * from sched_b limit 0;
 alter table ofec_sched_b_queue_new add column timestamp timestamp;
 alter table ofec_sched_b_queue_old add column timestamp timestamp;
+alter table ofec_sched_b_queue_new add column two_year_transaction_period smallint;
+alter table ofec_sched_b_queue_old add column two_year_transaction_period smallint;
 create index on ofec_sched_b_queue_new (sched_b_sk);
 create index on ofec_sched_b_queue_old (sched_b_sk);
 create index on ofec_sched_b_queue_new (timestamp);
 create index on ofec_sched_b_queue_old (timestamp);
+create index on ofec_sched_b_queue_new (two_year_transaction_period);
+create index on ofec_sched_b_queue_old (two_year_transaction_period);
 
 -- Create trigger to maintain Schedule B queues
 create or replace function ofec_sched_b_update_queues() returns trigger as $$
 declare
     start_year int = TG_ARGV[0]::int;
+    two_year_transaction_period_new smallint;
+    two_year_transaction_period_old smallint;
 begin
+    two_year_transaction_period_new = get_transaction_year(new.disb_dt, new.rpt_yr);
+    two_year_transaction_period_old = get_transaction_year(old.disb_dt, old.rpt_yr);
+
     if tg_op = 'INSERT' then
-        if new.rpt_yr >= start_year then
+        if two_year_transaction_period_new >= start_year then
             delete from ofec_sched_b_queue_new where sched_b_sk = new.sched_b_sk;
             insert into ofec_sched_b_queue_new values (new.*);
         end if;
         return new;
     elsif tg_op = 'UPDATE' then
-        if new.rpt_yr >= start_year then
+        if two_year_transaction_period_new >= start_year then
             delete from ofec_sched_b_queue_new where sched_b_sk = new.sched_b_sk;
             delete from ofec_sched_b_queue_old where sched_b_sk = old.sched_b_sk;
-            insert into ofec_sched_b_queue_new values (new.*);
-            insert into ofec_sched_b_queue_old values (old.*);
+            insert into ofec_sched_b_queue_new values (new.*, timestamp, two_year_transaction_period_new);
+            insert into ofec_sched_b_queue_old values (old.*, timestamp, two_year_transaction_period_old);
         end if;
         return new;
     elsif tg_op = 'DELETE' then
-        if old.rpt_yr >= start_year then
+        if two_year_transaction_period_old >= start_year then
             delete from ofec_sched_b_queue_old where sched_b_sk = old.sched_b_sk;
-            insert into ofec_sched_b_queue_old values (old.*);
+            insert into ofec_sched_b_queue_old values (old.*, timestamp, two_year_transaction_period_old);
         end if;
         return old;
     end if;
