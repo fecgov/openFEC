4f48914c9c4e1a4ed949b718a8cd9fd1d8aa33f2
Jonathan Carmack <jonathancarmack@C02QQ0N5FVH8.local>
2016-06-30 12:18:44 -0400

    Now referencing new tables

--- data/sql_setup/prepare_schedule_b.sql
+++ data/sql_setup/prepare_schedule_b.sql
@@ -1,17 +1,17 @@
 -- Create index for join on electioneering costs
-create index on sched_b (link_id);
+create index on fec_vsum_sched_b (link_id);
 
 -- Create queue tables to hold changes to Schedule B
 drop table if exists ofec_sched_b_queue_new;
 drop table if exists ofec_sched_b_queue_old;
-create table ofec_sched_b_queue_new as select * from sched_b limit 0;
-create table ofec_sched_b_queue_old as select * from sched_b limit 0;
+create table ofec_sched_b_queue_new as select * from fec_vsum_sched_b limit 0;
+create table ofec_sched_b_queue_old as select * from fec_vsum_sched_b limit 0;
 alter table ofec_sched_b_queue_new add column timestamp timestamp;
 alter table ofec_sched_b_queue_old add column timestamp timestamp;
 alter table ofec_sched_b_queue_new add column two_year_transaction_period smallint;
 alter table ofec_sched_b_queue_old add column two_year_transaction_period smallint;
-create index on ofec_sched_b_queue_new (sched_b_sk);
-create index on ofec_sched_b_queue_old (sched_b_sk);
+create index on ofec_sched_b_queue_new (sub_id);
+create index on ofec_sched_b_queue_old (sub_id);
 create index on ofec_sched_b_queue_new (timestamp);
 create index on ofec_sched_b_queue_old (timestamp);
 create index on ofec_sched_b_queue_new (two_year_transaction_period);
@@ -29,7 +29,7 @@ begin
         two_year_transaction_period_new = get_transaction_year(new.disb_dt, new.rpt_yr);
 
         if two_year_transaction_period_new >= start_year then
-            delete from ofec_sched_b_queue_new where sched_b_sk = new.sched_b_sk;
+            delete from ofec_sched_b_queue_new where sub_id = new.sub_id;
             insert into ofec_sched_b_queue_new values (new.*, timestamp, two_year_transaction_period_new);
         end if;
         return new;
@@ -38,8 +38,8 @@ begin
         two_year_transaction_period_old = get_transaction_year(old.disb_dt, old.rpt_yr);
 
         if two_year_transaction_period_new >= start_year then
-            delete from ofec_sched_b_queue_new where sched_b_sk = new.sched_b_sk;
-            delete from ofec_sched_b_queue_old where sched_b_sk = old.sched_b_sk;
+            delete from ofec_sched_b_queue_new where sub_id = new.sub_id;
+            delete from ofec_sched_b_queue_old where sub_id = old.sub_id;
             insert into ofec_sched_b_queue_new values (new.*, timestamp, two_year_transaction_period_new);
             insert into ofec_sched_b_queue_old values (old.*, timestamp, two_year_transaction_period_old);
         end if;
@@ -48,7 +48,7 @@ begin
         two_year_transaction_period_old = get_transaction_year(old.disb_dt, old.rpt_yr);
 
         if two_year_transaction_period_old >= start_year then
-            delete from ofec_sched_b_queue_old where sched_b_sk = old.sched_b_sk;
+            delete from ofec_sched_b_queue_old where sub_id = old.sub_id;
             insert into ofec_sched_b_queue_old values (old.*, timestamp, two_year_transaction_period_old);
         end if;
         return old;
@@ -56,7 +56,7 @@ begin
 end
 $$ language plpgsql;
 
-drop trigger if exists ofec_sched_b_queue_trigger on sched_b;
+drop trigger if exists ofec_sched_b_queue_trigger on fec_vsum_sched_b;
 create trigger ofec_sched_b_queue_trigger before insert or update or delete
-    on sched_b for each row execute procedure ofec_sched_b_update_queues(:START_YEAR_AGGREGATE)
+    on fec_vsum_sched_b for each row execute procedure ofec_sched_b_update_queues(:START_YEAR_AGGREGATE)
 ;
