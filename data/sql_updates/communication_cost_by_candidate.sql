64c17a963173792477fd472132be7ad71dfacc16
Jonathan Carmack <jonathancarmack@C02QQ0N5FVH8.local>
2016-07-11 10:54:39 -0400

    Sql changes to move to 'nml_form_76' table

--- data/sql_updates/communication_cost_by_candidate.sql
+++ data/sql_updates/communication_cost_by_candidate.sql
@@ -2,26 +2,26 @@ drop materialized view if exists ofec_communication_cost_aggregate_candidate_mv_
 create materialized view ofec_communication_cost_aggregate_candidate_mv_tmp as
 select
     row_number() over () as idx,
-    cmte_id,
-    cand_id,
-    support_oppose_ind as support_oppose_indicator,
-    sum(transaction_amt) as total,
-    count(transaction_amt) as count,
-    rpt_yr + rpt_yr % 2 as cycle
-from communication_costs_vw
-where rpt_yr >= :START_YEAR
-and cand_id is not null
+    org_id,
+    s_o_cand_id,
+    s_o_cand_id as support_oppose_indicator,
+    sum(communication_cost) as total,
+    count(communication_cost) as count,
+    extract(year from communication_dt) + extract(year from communication_dt) % 2 as cycle
+from nml_form_76
+where extract(year from communication_dt) >= :START_YEAR
+and s_o_cand_id is not null
 group by
-    cmte_id,
-    cand_id,
+    org_id,
+    s_o_cand_id,
     support_oppose_indicator,
     cycle
 ;
 
 create unique index on ofec_communication_cost_aggregate_candidate_mv_tmp (idx);
 
-create index on ofec_communication_cost_aggregate_candidate_mv_tmp (cmte_id);
-create index on ofec_communication_cost_aggregate_candidate_mv_tmp (cand_id);
+create index on ofec_communication_cost_aggregate_candidate_mv_tmp (org_id);
+create index on ofec_communication_cost_aggregate_candidate_mv_tmp (s_o_cand_id);
 create index on ofec_communication_cost_aggregate_candidate_mv_tmp (support_oppose_indicator);
 create index on ofec_communication_cost_aggregate_candidate_mv_tmp (cycle);
 create index on ofec_communication_cost_aggregate_candidate_mv_tmp (total);
