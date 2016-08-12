10fc1e0e56e2a86c7c9bae8a82f3366b896b3dd9
Jonathan Carmack <jonathancarmack@C02QQ0N5FVH8.local>
2016-08-12 16:18:46 -0400

    Most columns are being mapped, some may be left to map?

--- webservices/common/models/efiling.py
+++ webservices/common/models/efiling.py
@@ -27,12 +27,21 @@ class BaseFiling(db.Model):
     city = db.Column(db.String)
     state = db.Column(db.String)
     zip = db.Column(db.String)
+    election_date = db.Column('el_date', db.Date)
+    election_state = db.Column('el_state', db.String)
+    create_date = db.Column('create_dt', db.Date)
+    sign_date = db.Column(db.Date)
 
 class BaseF3PFiling(BaseFiling):
     __tablename__ = 'real_efile_f3p'
     repid = db.Column(db.Integer, primary_key=True)
 
-    candidate_name = db.Column('c_name', db.String, index=True, doc=docs.CANDIDATE_NAME)
+    treasurer_last_name = db.Column('lname', db.String)
+    treasurer_middle_name = db.Column('mname', db.String)
+    treasurer_first_name = db.Column('fname', db.String)
+    prefix = db.Column(db.String)
+    suffix = db.Column(db.String)
+    committee_name = db.Column('c_name', db.String, index=True, doc=docs.CANDIDATE_NAME)
     street_address_one = db.Column('c_str1', db.String)
     street_address_two = db.Column('c_str2', db.String)
     city = db.Column('c_city', db.String)
@@ -41,6 +50,14 @@ class BaseF3PFiling(BaseFiling):
     total_receipts = db.Column('tot_rec', db.Float)
     total_disbursements = db.Column('tot_dis', db.Float)
     cash = db.Column(db.Float)
+    cash_close = db.Column(db.Float)
+    debts_to = db.Column(db.Float)
+    expe = db.Column(db.Float)
+    net_contributions = db.Column('net_con', db.Float)
+    net_operating_expenditures = db.Column('net_op', db.Float)
+    primary_election = db.Column('act_pri', db.String)
+    general_election = db.Column('act_gen', db.String)
+    sub_total_sum = db.Column('sub', db.String)
 
     summary_lines = db.relationship(
         'BaseFilingSummary',
@@ -54,7 +71,18 @@ class BaseF3PFiling(BaseFiling):
 class BaseF3Filing(BaseFiling):
     __tablename__ = 'real_efile_f3'
     repid = db.Column(db.Integer, primary_key=True)
-
+    candidate_last_name = db.Column('can_lname', db.String)
+    candidate_first_name = db.Column('can_fname', db.String)
+    candidate_middle_name = db.Column('can_mname', db.String)
+    candidate_prefix = db.Column('can_prefix', db.String)
+    candidate_suffix = db.Column('can_suffix', db.String)
+    f3z1 = db.Column(db.Integer)
+    primary_election = db.Column('act_pri', db.String)
+    general_election = db.Column('act_gen', db.String)
+    special_election = db.Column('act_spe', db.String)
+    runoff_election = db.Column('act_run', db.String)
+    election_district = db.Column('eld', db.Integer)
+    amend_address = db.Column('amend_addr', db.String)
 
     summary_lines = db.relationship(
         'BaseFilingSummary',
@@ -70,7 +98,9 @@ class BaseF3XFiling(BaseFiling):
     repid = db.Column(db.Integer, primary_key=True)
 
     committee_name = db.Column('com_name', db.String, index=True, doc=docs.COMMITTEE_NAME)
-
+    sign_date = db.Column('date_signed', db.Date)
+    amend_address = db.Column('amend_addr', db.String)
+    qual = db.Column(db.String)
 
 
 
