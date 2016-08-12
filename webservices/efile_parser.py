1f52878bdd40caf14268f1b3cdc3e92a50bb3d86
Jonathan Carmack <jonathancarmack@C02QQ0N5FVH8.local>
2016-08-12 16:25:51 -0400

    Added relative reference for data file

--- webservices/efile_parser.py
+++ webservices/efile_parser.py
@@ -12,7 +12,7 @@ def parse_f3psummary_column_b(dataframe=None):
 def get_dataframe(sheetname=None):
     dataframe = pd.read_excel(
         # /Users/jonathancarmack/Documents/repos/openFEC
-        io="/Users/jonathancarmack/Documents/repos/openFEC/data/real_efile_to_form_line_numbers.xlsx",
+        io="data/real_efile_to_form_line_numbers.xlsx",
         index_col="summary line number",
         sheetname=sheetname,
         skiprows=7,
