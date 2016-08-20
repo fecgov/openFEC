bfeba69f44f117d277eb8194f04a3c4b2a382613
Anthony Garvan <anthony.garvan@gsa.gov>
2016-08-20 13:18:29 -0500

    style changes per code review

--- tests/test_legal.py
+++ tests/test_legal.py
@@ -31,35 +31,33 @@ class AdvisoryOpinionTest(unittest.TestCase):
     def test_advisory_opinion_search(self, es_search):
         app = rest.app.test_client()
         response = app.get('/v1/legal/advisory_opinion/1993-02?api_key=1234')
-        self.assertEquals(response.status_code, 200)
+        assert response.status_code == 200
         result = json.loads(codecs.decode(response.data))
-        self.assertNotEqual(result, [], "Empty response!")
-        self.assertDictEqual(result, {'docs': [{'text': 'abc'}, {'no': '123'}]})
+        assert result == {'docs': [{'text': 'abc'}, {'no': '123'}]}
 
-@patch('webservices.rest.legal.es.search', side_effect=es_search)
+@patch('webservices.rest.legal.es.search', es_search)
 class SearchTest(unittest.TestCase):
     def setUp(self):
         self.app = rest.app.test_client()
 
-    def test_default_search(self, es_search):
+    def test_default_search(self):
         response = self.app.get('/v1/legal/search/?q=president&api_key=1234')
-        self.assertEquals(response.status_code, 200)
+        assert response.status_code == 200
         result = json.loads(codecs.decode(response.data))
-        self.assertNotEqual(result, [], "Empty response!")
-        self.assertDictEqual(result, {
+        assert result == {
             'regulations': [{'highlights': ['a', 'b']}],
             'total_advisory_opinions': 2,
             'statutes': [{'highlights': ['e']}], 'total_statutes': 3,
             'total_regulations': 1,
             'advisory_opinions': [{'highlights': ['a', 'b']},
-              {'highlights': ['c', 'd']}], 'total_all': 6})
+              {'highlights': ['c', 'd']}], 'total_all': 6}
 
-    def test_type_search(self, es_search):
-        response = self.app.get('/v1/legal/search/?q=president&type=advisory_opinions')
-        self.assertEquals(response.status_code, 200)
+    def test_type_search(self):
+        response = self.app.get('/v1/legal/search/' +
+                                '?q=president&type=advisory_opinions')
+        assert response.status_code == 200
         result = json.loads(codecs.decode(response.data))
-        self.assertNotEqual(result, [], "Empty response!")
-        self.assertDictEqual(result, {
+        assert result == {
             'total_advisory_opinions': 2,
             'advisory_opinions': [{'highlights': ['a', 'b']},
-              {'highlights': ['c', 'd']}], 'total_all': 2})
+              {'highlights': ['c', 'd']}], 'total_all': 2}
