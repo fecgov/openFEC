c7bfd20fe2a1462e2b6e23a56e5e4b9ce8180bff
Aaron D Borden <adborden@a14n.net>
2016-08-19 18:56:00 -0700

    Legal phrase search

--- tests/test_legal.py
+++ tests/test_legal.py
@@ -4,6 +4,8 @@ import codecs
 import unittest
 from mock import patch
 
+from webservices.resources.legal import parse_query_string
+
 # TODO: integrate more with API Schema so that __API_VERSION__ is returned
 # self.assertEqual(result['api_version'], __API_VERSION__)
 
@@ -61,3 +63,17 @@ class SearchTest(unittest.TestCase):
             'total_advisory_opinions': 2,
             'advisory_opinions': [{'highlights': ['a', 'b']},
               {'highlights': ['c', 'd']}], 'total_all': 2}
+
+
+class LegalPhraseSearchTests(unittest.TestCase):
+    def test_parse_query_no_phrase(self):
+        parsed = parse_query_string('hello world')
+        self.assertEquals(parsed, dict(terms=['hello world'], phrases=[]))
+
+    def test_parse_query_with_phrase(self):
+        parsed = parse_query_string('require "electronic filing" 2016')
+        self.assertEquals(parsed, dict(terms=['require', '2016'], phrases=['electronic filing']))
+
+    def test_parse_query_with_many_phrases(self):
+        parsed = parse_query_string('require "electronic filing" 2016 "sans computer"')
+        self.assertEquals(parsed, dict(terms=['require', '2016'], phrases=['electronic filing', 'sans computer']))
