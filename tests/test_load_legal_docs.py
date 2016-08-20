722aec3877521a00ffd1bf457ec8b673a8f428bb
Anthony Garvan <anthony.garvan@gsa.gov>
2016-08-20 12:39:16 -0500

    test coverage to 100 on load_legal_docs

--- tests/test_load_legal_docs.py
+++ tests/test_load_legal_docs.py
@@ -10,8 +10,14 @@ class ElasticSearchMock:
         pass
 
     def index(self, index, doc_type, doc, id):
-        print(doc)
         assert self.dictToIndex == doc
+
+    def delete_index(self, index):
+        assert index == 'docs'
+
+    def create_index(self, index, mappings):
+        assert index == 'docs'
+        assert mappings
 
 def get_es_with_doc(doc):
     def get_es():
@@ -31,6 +37,9 @@ def mock_xml(xml):
     return request_zip
 
 class Engine:
+    def __init__(self, legal_loaded):
+        self.legal_loaded = legal_loaded
+
     def __iter__(self):
         return self.result.__iter__()
 
@@ -38,11 +47,24 @@ class Engine:
         return self.result.__next__()
 
     def fetchone(self):
+        return self.result[0]
+
+    def fetchall(self):
         return self.result
 
+    def connect(self):
+        return self
+
+    def execution_options(self, stream_results):
+        return self
+
     def execute(self, sql):
+        if sql == 'select document_id from document':
+            self.result = [(1,), (2,)]
+        if 'fileimage' in sql:
+            return [(1, 'ABC'.encode('utf8'))]
         if 'EXISTS' in sql:
-            self.result = [(True,)]
+            self.result = [(self.legal_loaded,)]
         if 'count' in sql:
             self.result = [(5,)]
         if 'DOCUMENT_ID' in sql:
@@ -52,10 +74,10 @@ class Engine:
 
 
 class Db:
-    engine = Engine()
+    def __init__(self, legal_loaded=True):
+        self.engine = Engine(legal_loaded)
 
 def get_credential_mock(var, default):
-    print(var)
     return 'https://eregs.api.com/'
 
 class RequestResult:
@@ -69,13 +91,39 @@ def mock_get_regulations(url):
     if url.endswith('regulation'):
         return RequestResult({'versions': [{'version': 'versionA',
                              'regulation': 'reg104'}]})
-    print(url)
     if url.endswith('reg104/versionA'):
         return RequestResult({'children': [{'children': [{'label': ['104', '1'],
                                'title': 'Section 104.1 Title',
                                'text': 'sectionContentA',
                                'children': [{'text': 'sectionContentB',
                                'children': []}]}]}]})
+
+class obj:
+    def __init__(self, key):
+        self.key = key
+
+    def delete(self):
+        pass
+
+class S3Objects:
+    def __init__(self, objects):
+        self.objects = objects
+
+    def filter(self, Prefix):
+        return self.objects
+
+class BucketMock:
+    def __init__(self, existing_pdfs):
+        self.objects = S3Objects(existing_pdfs)
+
+    def put_object(self, Key, Body, ContentType, ACL):
+        assert Key == 'legal/aos/1.pdf'
+
+def get_bucket_mock(existing_pdfs):
+    def get_bucket():
+        return BucketMock(existing_pdfs)
+    return get_bucket
+
 class IndexStatutesTest(unittest.TestCase):
     @patch('webservices.load_legal_docs.requests.get', mock_xml('<test></test>'))
     def test_get_xml_tree_from_url(self):
@@ -121,6 +169,11 @@ class IndexStatutesTest(unittest.TestCase):
     def test_title_52(self):
         get_title_52_statutes()
 
+    @patch('webservices.load_legal_docs.get_title_52_statutes', lambda: '')
+    @patch('webservices.load_legal_docs.get_title_26_statutes', lambda: '')
+    def test_index_statutes(self):
+        index_statutes()
+
 class IndexRegulationsTest(unittest.TestCase):
     @patch('webservices.load_legal_docs.env.get_credential', get_credential_mock)
     @patch('webservices.load_legal_docs.requests.get', mock_get_regulations)
@@ -130,6 +183,10 @@ class IndexRegulationsTest(unittest.TestCase):
             'url': '/regulations/104-1/versionA#104-1',
             'doc_id': '104_1'}))
     def test_index_regulations(self):
+        index_regulations()
+
+    @patch('webservices.load_legal_docs.env.get_credential', lambda e, d: '')
+    def test_no_env_variable(self):
         index_regulations()
 
 class IndexAdvisoryOpinionsTest(unittest.TestCase):
@@ -143,3 +200,31 @@ class IndexAdvisoryOpinionsTest(unittest.TestCase):
             'doc_id': 123, 'id': 'id123'}))
     def test_advisory_opinion_load(self, es_mock):
         index_advisory_opinions()
+
+    @patch('webservices.load_legal_docs.db', Db(False))
+    def test_no_legal_loaded(self):
+        index_advisory_opinions()
+
+class LoadAdvisoryOpinionsIntoS3Test(unittest.TestCase):
+    @patch('webservices.load_legal_docs.db', Db())
+    @patch('webservices.load_legal_docs.get_bucket',
+     get_bucket_mock([obj('legal/aos/2.pdf')]))
+    def test_load_advisory_opinions_into_s3(self):
+        load_advisory_opinions_into_s3()
+
+    @patch('webservices.load_legal_docs.db', Db())
+    @patch('webservices.load_legal_docs.get_bucket',
+     get_bucket_mock([obj('legal/aos/1.pdf'), obj('legal/aos/2.pdf')]))
+    def test_load_advisory_opinions_into_s3_already_loaded(self):
+        load_advisory_opinions_into_s3()
+
+    @patch('webservices.load_legal_docs.get_bucket',
+     get_bucket_mock([obj('legal/aos/2.pdf')]))
+    def test_delete_advisory_opinions_from_s3(self):
+        delete_advisory_opinions_from_s3()
+
+class RemoveLegalDocsTest(unittest.TestCase):
+    @patch('webservices.utils.get_elasticsearch_connection',
+    get_es_with_doc({}))
+    def test_remove_legal_docs(self):
+        remove_legal_docs()
