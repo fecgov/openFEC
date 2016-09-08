b538de6cbac083846195327a24e9daea6dc62982
Jonathan Carmack <jonathancarmack@C02QQ0N5FVH8.local>
2016-09-08 15:54:27 -0400

    Removed filter for loaner_name and candidate_name

--- webservices/resources/sched_c.py
+++ webservices/resources/sched_c.py
@@ -7,23 +7,19 @@ from webservices import utils
 from webservices import filters
 from webservices import schemas
 from webservices.common import models
-from webservices.common.views import ItemizedResource
+from webservices.common.views import ApiResource
 
 
 @doc(
     tags=['loans'],
     description=docs.SCHEDULE_C,
 )
-class ScheduleCView(ItemizedResource):
+class ScheduleCView(ApiResource):
 
     model = models.ScheduleC
     schema = schemas.ScheduleCSchema
     page_schema = schemas.ScheduleCPageSchema
-    """
-    @property
-    def year_column(self):
-        return self.model.two_year_transaction_period
-    """
+
     @property
     def index_column(self):
         return self.model.sub_id
@@ -32,23 +28,14 @@ class ScheduleCView(ItemizedResource):
         ('image_number', models.ScheduleC.image_number),
         ('committee_id', models.ScheduleC.committee_id),
     ]
-
+    """
+    Features to be added
     filter_fulltext_fields = [
         ('loaner_name', models.ScheduleC.loan_source_name),
-    ]
-
-    """
-    filter_match_fields = [
-        ('is_individual', models.ScheduleA.is_individual),
-        ('two_year_transaction_period', models.ScheduleA.two_year_transaction_period),
-    ]
-
-
-    query_options = [
-        sa.orm.joinedload(models.ScheduleA.committee),
-        sa.orm.joinedload(models.ScheduleA.contributor),
+        ('candidate_name', models.ScheduleC.candidate_name)
     ]
     """
+
     filter_range_fields = [
         (('min_date', 'max_date'), models.ScheduleC.incurred_date),
         (('min_amount', 'max_amount'), models.ScheduleC.original_loan_amount),
@@ -61,15 +48,14 @@ class ScheduleCView(ItemizedResource):
         return utils.extend(
             args.itemized,
             args.schedule_c,
-            args.make_seek_args(),
+            args.paging,
             args.make_sort_args(
                 default='incurred_date',
-            )
+            ),
         )
 
     def build_query(self, **kwargs):
         query = super().build_query(**kwargs)
-        #query = filters.filter_contributor_type(query, self.model.entity_type, kwargs)
         if kwargs.get('sub_id'):
             query = query.filter_by(sub_id= int(kwargs.get('sub_id')))
         return query
