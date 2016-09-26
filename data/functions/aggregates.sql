f8d553d4c0df018fdded97aa22e2d4d13079f21d
Jonathan Carmack <jonathancarmack@C02QQ0N5FVH8.local>
2016-09-26 16:33:35 -0400

    More sql added (all the sql has been tested and verified)

--- data/functions/aggregates.sql
+++ data/functions/aggregates.sql
@@ -16,5 +16,11 @@ begin
     perform ofec_sched_e_update();
     delete from ofec_sched_e_queue_new;
     delete from ofec_sched_e_queue_old;
+
+    perform ofec_sched_e_notice_update();
+    delete from ofec_nml_24_queue_old;
+    --in case of inner join failing, keep this one in queue until related rows
+    -- are addded in disclousre.nml_form_24 (not likely to happen)
+    delete from ofec_nml_24_queue_new where sub_id in (select sub_id from ofec_sched_e_notice);
 end
 $$ language plpgsql;
