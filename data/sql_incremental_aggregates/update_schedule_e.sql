f6f944b439018066d887af7f25fc54b0a8c9e847
Jonathan Carmack <jonathancarmack@C02QQ0N5FVH8.local>
2016-10-03 16:41:54 -0400

    Fixed one of the update functions

--- data/sql_incremental_aggregates/update_schedule_e.sql
+++ data/sql_incremental_aggregates/update_schedule_e.sql
@@ -101,7 +101,7 @@ create or replace function ofec_sched_e_notice_update_from_f24() returns void as
 begin
     -- Drop all queued deletes
     delete from ofec_sched_e
-    where link_id = any(select sub_id from ofec_nml_24_queue_old)
+    where sub_id = any(select sub_id from ofec_nml_24_queue_old)
     ;
     -- Insert all queued updates, unless a row with the same key exists in the
     -- delete queue with a later timestamp
