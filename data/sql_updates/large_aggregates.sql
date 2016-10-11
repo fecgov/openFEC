42fc177cc05f41da38bf3eb1da6830b620a7c115
LindsayYoung <lindsayn.young@gsa.gov>
2016-10-10 21:48:49 -0400

    Got a rought version of the candidate totals working

--- data/sql_updates/large_aggregates.sql
+++ data/sql_updates/large_aggregates.sql
@@ -1,41 +1,45 @@
+with cand as (
+    select
+        extract(month from to_date(cast(cvg_end_dt as text), 'YYYYMMDD')) as month,
+        extract(year from to_date(cast(cvg_end_dt as text), 'YYYYMMDD')) as year,
+        ttl_receipts_per as ttl_receipts,
+        pol_pty_cmte_contb_per as pty_cmte_contb,
+        other_pol_cmte_contb_per as oth_cmte_contb,
+        ttl_disb_per as ttl_disb,
+        -- double check
+        tranf_to_other_auth_cmte_per as tranf_to_other_auth_cmte,
+        offsets_to_op_exp_per as offsets_to_op_exp,
+        ttl_loans_received_per as ttl_loan_repymts,
+        ttl_contb_ref_per as ttl_contb_ref,
+        other_disb_per
+    from fec_vsum_f3p
+    where
+        rpt_yr = 2016 or rpt_yr = 2015
+    union all
+    select
+        extract(month from to_date(cast(cvg_end_dt as text), 'YYYYMMDD')) as month,
+        extract(year from to_date(cast(cvg_end_dt as text), 'YYYYMMDD')) as year,
+        ttl_receipts_per as ttl_receipts,
+        pol_pty_cmte_contb_per as pty_cmte_contb,
+        other_pol_cmte_contb_per as oth_cmte_contb,
+        ttl_disb_per as ttl_disb,
+        tranf_to_other_auth_cmte_per as tranf_to_other_auth_cmte,
+        offsets_to_op_exp_per as offsets_to_op_exp,
+        ttl_loan_repymts_per as ttl_loan_repymts,
+        ttl_contb_ref_col_ttl_per as ttl_contb_ref,
+        other_disb_per
+    from fec_vsum_f3
+    where
+        rpt_yr = 2016 or rpt_yr = 2015
+)
 select
--- do we need this if we are doing the group by later
--- confirm that this is as-amended
-extract(month from to_date(cast(vs.cvg_end_dt as text), 'YYYYMMDD')) as month,
-extract(year from to_date(cast(vs.cvg_end_dt as text), 'YYYYMMDD')) as year,
-sum(coalesce(vs.ttl_receipts,0) - (coalesce(vs.pty_cmte_contb ,0) + coalesce(vs.oth_cmte_contb ,0) + coalesce(vs.offsets_to_op_exp,0) + coalesce(vs.ttl_loan_repymts,0) + coalesce(vs.ttl_contb_ref,0))) as adj_tr,
-sum(coalesce(vs.ttl_disb, 0) - (coalesce(vs.tranf_to_other_auth_cmte,0) + coalesce(vs.ttl_loan_repymts,0) + coalesce(vs.ttl_contb_ref,0) + coalesce(vs.other_disb_per,0))) as adj_td
-from disclosure.v_sum_and_det_sum_report vs, disclosure.cand_cmte_linkage ccl, disclosure.candidate_summary cs
-where vs.cmte_id = ccl.cmte_id
-and ccl.cand_id = cs.cand_id
-and ccl.cmte_tp = substr(ccl.cand_id, 1,1)
-and ccl.cmte_dsgn in ('P', 'A')
-and ccl.cand_election_yr in (2015, 2016)
-and ccl.fec_election_yr = 2016
-and cs.cand_election_yr in (2015, 2016)
-and cs.fec_election_yr = 2016
-and ccl.cand_id not in (select cand_id from disclosure.cand_inactive where election_yr = 2016)
-and (vs.ttl_receipts > 0 or vs.ttl_disb > 0)
-and vs.cvg_end_dt between 20150101 and 20160831
--- group by month
-group by vs.cvg_end_dt
--- shouldn't this be reciept date ?? check with Paul
-order by vs.cvg_end_dt;
-
-
-
-select
--- do we need this if we are doing the group by later
--- confirm that this is as-amended
--- extract(month from to_date(cast(vs.cvg_end_dt as text), 'YYYYMMDD')) month_order,
-vs.cvg_end_dt as original,
-extract(month from to_date(cast(vs.cvg_end_dt as text), 'YYYYMMDD')) as month,
-extract(year from to_date(cast(vs.cvg_end_dt as text), 'YYYYMMDD')) as year,
-sum(coalesce(vs.ttl_receipts,0) - (coalesce(vs.pty_cmte_contb ,0) + coalesce(vs.oth_cmte_contb ,0) + coalesce(vs.offsets_to_op_exp,0) + coalesce(vs.ttl_loan_repymts,0) + coalesce(vs.ttl_contb_ref,0))) as adj_tr
-
-from disclosure.v_sum_and_det_sum_report vs--, disclosure.cand_cmte_linkage ccl, disclosure.candidate_summary cs
-group by vs.cvg_end_dt
--- shouldn't this be reciept date ?? check with Paul
-order by vs.cvg_end_dt
-limit 2
+    'candidate' as type,
+    month,
+    year,
+    sum(coalesce(cand.ttl_receipts,0) - (coalesce(cand.pty_cmte_contb ,0) + coalesce(cand.oth_cmte_contb ,0) + coalesce(cand.offsets_to_op_exp,0) + coalesce(cand.ttl_loan_repymts,0) + coalesce(cand.ttl_contb_ref,0))) as adjusted_total_reciepts,
+    sum(coalesce(cand.ttl_disb, 0) - (coalesce(cand.tranf_to_other_auth_cmte,0) + coalesce(cand.ttl_loan_repymts,0) + coalesce(cand.ttl_contb_ref,0) + coalesce(cand.other_disb_per,0))) as adjusted_total_disbursements
+from cand
+group by
+    month,
+    year
 ;
