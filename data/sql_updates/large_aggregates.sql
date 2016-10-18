e2fced081db61271ad2480e7050d2f50f25baee6
LindsayYoung <lindsayn.young@gsa.gov>
2016-10-18 14:09:01 -0400

    think I got the sql going

--- data/sql_updates/large_aggregates.sql
+++ data/sql_updates/large_aggregates.sql
@@ -63,90 +63,156 @@ cand_totals as (
         year
 ),
 -- PACs
-pac_totals as (select
-    'pac'::text as type,
-    extract(month from to_date(cast(cvg_end_dt as text), 'YYYY-MM-DD')) as month,
-    extract(year from to_date(cast(cvg_end_dt as text), 'YYYY-MM-DD')) as year,
-    sum(coalesce(ttl_receipts, 0) -
-        (
-            coalesce(pol_pty_cmte_contb_per_i,0) +
-            coalesce(other_pol_cmte_contb_per_i,0) +
-            coalesce(ttl_op_exp_per,0) +
-            coalesce(fed_cand_contb_ref_per,0) +
-            coalesce(tranf_from_nonfed_acct_per,0) +
-            coalesce(loan_repymts_received_per,0) +
-            coalesce(ttl_contb_refund,0)
-        )
-    ) as adjusted_total_reciepts,
-    sum(coalesce(ttl_disb,0) -
-        (
-            coalesce(shared_nonfed_op_exp_per,0) +
-            -- confirm var
-            coalesce(tranf_to_affliliated_cmte_per,0) +
-            -- coalesce(tranf_to_other_auth_cmte,0) +
-            coalesce(fed_cand_cmte_contb_per,0) +
-            coalesce(loan_repymts_made_per,0) +
-            coalesce(ttl_contb_refund,0) +
-            coalesce(other_disb_per,0)
-        )
-    ) as adjusted_total_disbursements
-from fec_vsum_f3x
-left join
-    -- ofec_committee_detail_mv_tmp using (cmte_id)
-    ofec_committee_detail_mv on committee_id = cmte_id
-where
-    rpt_yr = 2016 or rpt_yr = 2015
-    and ofec_committee_detail_mv.committee_type in ('N', 'Q', 'O', 'V', 'W')
-    and ofec_committee_detail_mv.designation <> 'J'
-    -- see if this might make things quicker
-    --and (vs.ttl_receipts > 0 or vs.ttl_disb > 0 or vs.debts_owed_by_cmte > 0 or vs.coh_cop >0)
-group by
-    month,
-    year
+pac_totals as (
+    select
+        'pac'::text as type,
+        extract(month from to_date(cast(cvg_end_dt as text), 'YYYY-MM-DD')) as month,
+        extract(year from to_date(cast(cvg_end_dt as text), 'YYYY-MM-DD')) as year,
+        sum(coalesce(ttl_receipts, 0) -
+            (
+                coalesce(pol_pty_cmte_contb_per_i,0) +
+                coalesce(other_pol_cmte_contb_per_i,0) +
+                coalesce(ttl_op_exp_per,0) +
+                coalesce(fed_cand_contb_ref_per,0) +
+                coalesce(tranf_from_nonfed_acct_per,0) +
+                coalesce(loan_repymts_received_per,0) +
+                coalesce(ttl_contb_refund,0)
+            )
+        ) as adjusted_total_reciepts,
+        sum(coalesce(ttl_disb,0) -
+            (
+                coalesce(shared_nonfed_op_exp_per,0) +
+                -- confirm var
+                coalesce(tranf_to_affliliated_cmte_per,0) +
+                -- coalesce(tranf_to_other_auth_cmte,0) +
+                coalesce(fed_cand_cmte_contb_per,0) +
+                coalesce(loan_repymts_made_per,0) +
+                coalesce(ttl_contb_refund,0) +
+                coalesce(other_disb_per,0)
+            )
+        ) as adjusted_total_disbursements
+    from fec_vsum_f3x
+    left join
+        -- ofec_committee_detail_mv_tmp using (cmte_id)
+        ofec_committee_detail_mv on committee_id = cmte_id
+    where
+        rpt_yr = 2016 or rpt_yr = 2015
+        and ofec_committee_detail_mv.committee_type in ('N', 'Q', 'O', 'V', 'W')
+        and ofec_committee_detail_mv.designation <> 'J'
+        -- see if this might make things quicker
+        --and (vs.ttl_receipts > 0 or vs.ttl_disb > 0 or vs.debts_owed_by_cmte > 0 or vs.coh_cop >0)
+    group by
+        month,
+        year
 ),
 -- Parties
-party_totals as (select
-    'party'::text as type,
-    extract(month from to_date(cast(cvg_end_dt as text), 'YYYY-MM-DD')) as month,
-    extract(year from to_date(cast(cvg_end_dt as text), 'YYYY-MM-DD')) as year,
-    sum(coalesce(ttl_receipts, 0) -
-        (
-            coalesce(pol_pty_cmte_contb_per_i,0) +
-            coalesce(other_pol_cmte_contb_per_i,0) +
-            coalesce(ttl_op_exp_per,0) +
-            coalesce(fed_cand_contb_ref_per,0) +
-            coalesce(tranf_from_nonfed_acct_per,0) +
-            coalesce(loan_repymts_received_per,0) +
-            coalesce(ttl_contb_refund,0)
-        )
-    ) as adjusted_total_reciepts,
-    sum(coalesce(ttl_disb,0) -
-        (
-            coalesce(shared_nonfed_op_exp_per,0) +
-            -- confirm var
-            coalesce(tranf_to_affliliated_cmte_per,0) +
-            -- coalesce(tranf_to_other_auth_cmte,0) +
-            coalesce(fed_cand_cmte_contb_per,0) +
-            coalesce(loan_repymts_made_per,0) +
-            coalesce(ttl_contb_refund,0) +
-            coalesce(other_disb_per,0)
-        )
-    ) as adjusted_total_disbursements
-from fec_vsum_f3x
-left join
-    -- ofec_committee_detail_mv_tmp using (cmte_id)
-    ofec_committee_detail_mv on committee_id = cmte_id
-where
-    rpt_yr = 2016 or rpt_yr = 2015
-    and ofec_committee_detail_mv.committee_type in ('X', 'Y')
-    and ofec_committee_detail_mv.designation <> 'J'
-    -- do we have this
-    -- and cm.cmte_id not in (select cmte_id from pclark.ref_pty_host_convention)
-    -- see if this might make things quicker
-    -- and (vs.ttl_receipts > 0 or vs.ttl_disb > 0 or vs.debts_owed_by_cmte > 0 or vs.coh_cop >0)
-group by
-    month,
-    year
+party_totals as (
+    select
+        'party'::text as type,
+        extract(month from to_date(cast(cvg_end_dt as text), 'YYYY-MM-DD')) as month,
+        extract(year from to_date(cast(cvg_end_dt as text), 'YYYY-MM-DD')) as year,
+        sum(coalesce(ttl_receipts, 0) -
+            (
+                coalesce(pol_pty_cmte_contb_per_i,0) +
+                coalesce(other_pol_cmte_contb_per_i,0) +
+                coalesce(ttl_op_exp_per,0) +
+                coalesce(fed_cand_contb_ref_per,0) +
+                coalesce(tranf_from_nonfed_acct_per,0) +
+                coalesce(loan_repymts_received_per,0) +
+                coalesce(ttl_contb_refund,0)
+            )
+        ) as adjusted_total_reciepts,
+        sum(coalesce(ttl_disb,0) -
+            (
+                coalesce(shared_nonfed_op_exp_per,0) +
+                -- confirm var
+                coalesce(tranf_to_affliliated_cmte_per,0) +
+                -- coalesce(tranf_to_other_auth_cmte,0) +
+                coalesce(fed_cand_cmte_contb_per,0) +
+                coalesce(loan_repymts_made_per,0) +
+                coalesce(ttl_contb_refund,0) +
+                coalesce(other_disb_per,0)
+            )
+        ) as adjusted_total_disbursements
+    from fec_vsum_f3x
+    left join
+        -- ofec_committee_detail_mv_tmp using (cmte_id)
+        ofec_committee_detail_mv on committee_id = cmte_id
+    where
+        rpt_yr = 2016 or rpt_yr = 2015
+        -- confirm no Z
+        and ofec_committee_detail_mv.committee_type in ('X', 'Y')
+        and ofec_committee_detail_mv.designation <> 'J'
+        -- do we have this ?
+        -- and cm.cmte_id not in (select cmte_id from pclark.ref_pty_host_convention)
+        -- see if this might make things quicker
+        -- and (vs.ttl_receipts > 0 or vs.ttl_disb > 0 or vs.debts_owed_by_cmte > 0 or vs.coh_cop >0)
+    group by
+        month,
+        year
+),
+-- other
+-- Independent expenditors
+ie as (
+    select
+        extract(month from to_date(cast(coverage_end_date as text), 'YYYY-MM-DD')) as month,
+        extract(year from to_date(cast(coverage_end_date as text), 'YYYY-MM-DD')) as year,
+        sum(independent_expenditures_period) as receipts,
+        sum(independent_contributions_period) as disbursements
+    from ofec_reports_ie_only_mv--tmp
+    where
+        cycle = 2016
+    group by
+        month,
+        year
+),
+-- communicaiton cost
+communicaiton as (
+    select
+        extract(month from to_date(cast(communication_dt as text), 'YYYY-MM-DD')) as month,
+        extract(year from to_date(cast(communication_dt as text), 'YYYY-MM-DD')) as year,
+        null::float as receipts,
+        sum(communication_cost) as disbursements
+    from ofec_communication_cost_mv--_tmp
+    where
+        rpt_yr = 2016 or rpt_yr = 2015
+    group by
+        month,
+        year
+),
+-- electioneering
+electioneering as (
+    select
+        extract(month from to_date(cast(disb_dt as text), 'YYYY-MM-DD')) as month,
+        extract(year from to_date(cast(disb_dt as text), 'YYYY-MM-DD')) as year,
+        null::float as receipts,
+        sum(calculated_cand_share) as disbursements
+    from electioneering_com_vw
+    where
+        rpt_yr = 2016 or rpt_yr = 2015
+    group by
+        month,
+        year
+),
+-- (why not delegate?)
+other as(
+    select
+        'other'::text as type,
+        month,
+        year,
+        sum(receipts),
+        sum(disbursements)
+    from (
+        select * from ie
+        union all
+        select * from communicaiton
+        union all
+        select * from electioneering
+    ) conglomerate
+    --
+    group by
+        month,
+        year
 ),
 combined as (
     select * from cand_totals
@@ -154,11 +220,13 @@ combined as (
     select * from pac_totals
     union all
     select * from party_totals
+    union all
+    select * from other
 )
-
 select
     row_number() over () as idx,
     combined.*
 from combined
 ;
 
+create unique index on big_totals_vw_tmp (idx);
