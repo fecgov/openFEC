dc8abecc3637cd0dd596e33a75496e194ed10420
Carlo Costino <carlo.costino@gsa.gov>
2016-10-21 17:33:55 -0400

    Scale back to just Schedule A, B, and E, remove extra checks for determining the last_x column to sort on, and update documentation.

--- webservices/utils.py
+++ webservices/utils.py
@@ -64,11 +64,7 @@ def fetch_page(query, kwargs, model=None, aliases=None, join_columns=None, clear
 
 def fetch_seek_page(query, kwargs, index_column, clear=False, count=None, cap=100, eager=True):
     paginator = fetch_seek_paginator(query, kwargs, index_column, clear=clear, count=count, cap=cap)
-    if paginator.sort_column is not None:
-        sort_index = kwargs['last_{0}'.format(paginator.sort_column[0].key)]
-    else:
-        sort_index = None
-    return paginator.get_page(last_index=kwargs['last_index'], sort_index=sort_index, eager=eager)
+    return paginator.get_page(last_index=kwargs['last_index'], sort_index=None, eager=eager)
 
 
 def fetch_seek_paginator(query, kwargs, index_column, clear=False, count=None, cap=100):
