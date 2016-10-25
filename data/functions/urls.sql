3b880a2f4e5f9e6b7b6829ddb2723e2a0339dd5f
Jonathan Carmack <jonathancarmack@C02QQ0N5FVH8.local>
2016-10-25 18:03:50 -0400

    is_electronic method added to functions, also going to go ahead and move fec_url generation to a plpgsql function to be used by some of our materialized views.

--- data/functions/urls.sql
+++ data/functions/urls.sql
@@ -29,3 +29,30 @@ begin
     end;
 end
 $$ language plpgsql immutable;
+
+create or replace function report_fec_url(image_number text, file_number integer) returns text as $$
+begin
+
+    return case
+        when file_number < 0 then null
+        when image_number is not null and not is_electronic(image_number) then format(
+            'http://docquery.fec.gov/paper/posted/%1$s.fec',
+            file_number
+        )
+        when image_number is not null and is_electronic(image_number) then format(
+            'http://docquery.fec.gov/dcdev/posted/%1$s.fec',
+            file_number
+        )
+    end;
+end
+$$ language plpgsql immutable;
+
+create or replace function is_electronic(image_number text) returns boolean as $$
+begin
+    return case
+        when char_length(image_number) = 18 and substring(image_number from 9 for 1) = '9' then true
+        when char_length(image_number) = 11 and substring(image_number from 3 for 1) = '9' then true
+        else false
+    end;
+end
+$$ language plpgsql immutable;
