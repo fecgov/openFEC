74ca665704fad38c1a300c41537ad788d7559794
Jonathan Carmack <jonathancarmack@C02QQ0N5FVH8.fios-router.home>
2017-01-06 17:30:57 -0500

    Still need to verify filters (sorting is working), and get tests passing, but this is ready to go really

--- webservices/schemas.py
+++ webservices/schemas.py
@@ -673,6 +673,22 @@ EFilingsSchema = make_schema(
 )
 augment_schemas(EFilingsSchema)
 
+ItemizedScheduleBfilingsSchema = make_schema(
+    models.ScheduleBEfile,
+    fields={
+        'beginning_image_number': ma.fields.Str(),
+        'committee': ma.fields.Nested(schemas['CommitteeHistorySchema']),
+        'filing': ma.fields.Nested(schemas['EFilingsSchema']),
+        'pdf_url': ma.fields.Str(),
+        'fec_url': ma.fields.Str(),
+        'is_notice':ma.fields.Boolean(),
+        'payee_name': ma.fields.Str(),
+        'report_type': ma.fields.Str(),
+        #'csv_url': ma.fields.Str(),
+    }
+)
+augment_schemas(ItemizedScheduleBfilingsSchema)
+
 ItemizedScheduleEfilingsSchema = make_schema(
     models.ScheduleEEfile,
     fields={
