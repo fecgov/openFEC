2f81fce24575c493517ff00b204ecfffd634debd
Jonathan Carmack <jonathancarmack@C02QQ0N5FVH8.fios-router.home>
2017-01-19 15:22:01 -0500

    Added amendment_version field; modified most_recent functions

--- data/sql_updates/filings.sql
+++ data/sql_updates/filings.sql
@@ -78,8 +78,12 @@ with filings as (
         amendments.amendment_chain,
         --amendments.prev_file_num as previous_file_number,
         amendments.mst_rct_file_num as most_recent_file_number,
-        is_amended(amendments.mst_rct_file_num::integer, amendments.file_num::integer) as is_amended,
-        is_most_recent(amendments.mst_rct_file_num::integer, amendments.file_num::integer) as most_recent
+        is_amended(amendments.mst_rct_file_num::integer, amendments.file_num::integer, filing_history.form_tp) as is_amended,
+        is_most_recent(amendments.mst_rct_file_num::integer, amendments.file_num::integer, filing_history.form_tp) as most_recent,
+        case when upper(filing_history.form_tp) = 'FRQ' then 0
+             when upper(filing_history.form_tp) = 'F99' then 0
+		         else array_length(amendments.amendment_chain, 1) - 1
+	        end as amendment_version
     from disclosure.f_rpt_or_form_sub filing_history
         left join ofec_committee_history_mv_tmp com
             on filing_history.cand_cmte_id = com.committee_id and get_cycle(filing_history.rpt_yr) = com.cycle
@@ -143,7 +147,8 @@ rfai_filings as (
         null::numeric[] as amendment_chain,
         null::int as most_recent_file_number,
         null::boolean as is_amended,
-        True as most_recent
+        True as most_recent,
+        0 as amendement_version
     from disclosure.nml_form_rfai filing_history
     left join ofec_committee_history_mv_tmp com on filing_history.id = com.committee_id and get_cycle(filing_history.rpt_yr) = com.cycle
     left join ofec_candidate_history_mv_tmp cand on filing_history.id = cand.candidate_id and get_cycle(filing_history.rpt_yr) = cand.two_year_period
