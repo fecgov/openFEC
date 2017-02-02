4c4dd19e97de4d3afbaca041dd1750295a7b2346
pkfec <pkasireddy@fec.gov>
2017-02-02 13:44:56 -0500

    Commit adr and util modules

--- webservices/legal_docs/utils.py
+++ webservices/legal_docs/utils.py
@@ -13,8 +13,7 @@ from .reclassify_statutory_citation import reclassify_current_mur_statutory_cita
 
 logger = logging.getLogger(__name__)
 
-STATUTE_REGEX = re.compile(r'(?<!\(|\d)(?P<section>\d+([a-z](-1)?)?)')
-REGULATION_REGEX = re.compile(r'(?<!\()(?P<part>\d+)(\.(?P<section>\d+))?')
+""
 
 SUBJECTS = """
     SELECT subject.description AS subj, relatedsubject.description AS rel
@@ -23,11 +22,13 @@ SUBJECTS = """
     LEFT OUTER JOIN fecmur.relatedsubject USING (subject_id, relatedsubject_id)
     WHERE case_id = %s
 """
+
 ELECTION_CYCLES = """
     SELECT election_cycle::INT
     FROM fecmur.electioncycle
     WHERE case_id = %s
 """
+
 PARTICIPANTS = """
     SELECT entity_id, name, role.description AS role
     FROM fecmur.players
@@ -35,6 +36,7 @@ PARTICIPANTS = """
     JOIN fecmur.entity USING (entity_id)
     WHERE case_id = %s
 """
+
 DOCUMENTS = """
     SELECT document_id, category, description, ocrtext,
         fileimage, length(fileimage) AS length,
@@ -51,42 +53,38 @@ VIOLATIONS = """
     WHERE case_id = %s
     ;
 """
+
 OPEN_AND_CLOSE_DATES = """
     SELECT min(event_date), max(event_date)
     FROM fecmur.calendar
     WHERE case_id = %s;
 """
+
 DISPOSITION_DATA = """
     SELECT fecmur.event.event_name,
-    fecmur.settlement.final_amount, fecmur.entity.name, violations.statutory_citation,
-    violations.regulatory_citation
-    from fecmur.calendar
-    inner join fecmur.event on fecmur.calendar.event_id = fecmur.event.event_id
-    inner join fecmur.entity on fecmur.entity.entity_id = fecmur.calendar.entity_id
-    left join (select * from fecmur.relatedobjects where relation_id=1) AS relatedobjects
-    on relatedobjects.detail_key = fecmur.calendar.entity_id
-    left join fecmur.settlement on fecmur.settlement.settlement_id = relatedobjects.master_key
-    left join (select * from fecmur.violations where stage='Closed' and case_id={0})
-    as violations on violations.entity_id = fecmur.calendar.entity_id
-    where fecmur.calendar.case_id={0} and event_name not in ('Complaint/Referral', 'Disposition')
+        fecmur.settlement.final_amount, fecmur.entity.name, violations.statutory_citation,
+        violations.regulatory_citation
+    FROM fecmur.calendar
+    INNER JOIN fecmur.event ON fecmur.calendar.event_id = fecmur.event.event_id
+    INNER JOIN fecmur.entity ON fecmur.entity.entity_id = fecmur.calendar.entity_id
+    LEFT JOIN (SELECT * FROM fecmur.relatedobjects WHERE relation_id = 1) AS relatedobjects
+        ON relatedobjects.detail_key = fecmur.calendar.entity_id
+    LEFT JOIN fecmur.settlement ON fecmur.settlement.settlement_id = relatedobjects.master_key
+    LEFT JOIN (SELECT * FROM fecmur.violations WHERE stage = 'Closed' AND case_id = {0}) AS violations
+        ON violations.entity_id = fecmur.calendar.entity_id
+    WHERE fecmur.calendar.case_id = {0}
+        AND event_name NOT IN ('Complaint/Referral', 'Disposition')
     ORDER BY fecmur.event.event_name ASC, fecmur.settlement.final_amount DESC NULLS LAST, event_date DESC;
 """
-DISPOSITION_TEXT = """
+
+COMMISSION_VOTES = """
 SELECT vote_date, action from fecmur.commission
 WHERE case_id = %s
 ORDER BY vote_date desc;
 """
-def get_subjects(case_id):
-    subjects = []
-    with db.engine.connect() as conn:
-        rs = conn.execute(SUBJECTS, case_id)
-        for row in rs:
-            if row['rel']:
-                subject_str = row['subj'] + "-" + row['rel']
-            else:
-                subject_str = row['subj']
-            subjects.append(subject_str)
-    return subjects
+
+STATUTE_REGEX = re.compile(r'(?<!\(|\d)(?P<section>\d+([a-z](-1)?)?)')
+REGULATION_REGEX = re.compile(r'(?<!\()(?P<part>\d+)(\.(?P<section>\d+))?')
 
 def get_election_cycles(case_id):
     election_cycles = []
@@ -95,6 +93,12 @@ def get_election_cycles(case_id):
         for row in rs:
             election_cycles.append(row['election_cycle'])
     return election_cycles
+
+def get_open_and_close_dates(case_id):
+    with db.engine.connect() as conn:
+        rs = conn.execute(OPEN_AND_CLOSE_DATES, case_id)
+        open_date, close_date = rs.fetchone()
+    return open_date, close_date
 
 def get_disposition(case_id):
     with db.engine.connect() as conn:
@@ -106,12 +110,19 @@ def get_disposition(case_id):
             disposition_data.append({'disposition': row['event_name'], 'penalty': row['final_amount'],
                 'respondent': row['name'], 'citations': citations})
 
-        rs = conn.execute(DISPOSITION_TEXT, case_id)
+        rs = conn.execute(COMMISSION_VOTES, case_id)
         disposition_text = []
         for row in rs:
             disposition_text.append({'vote_date': row['vote_date'], 'text': row['action']})
         return {'text': disposition_text, 'data': disposition_data}
 
+def get_commission_votes(case_id):
+    with db.engine.connect() as conn:
+        rs = conn.execute(COMMISSION_VOTES, case_id)
+        commission_votes = []
+        for row in rs:
+            commission_votes.append({'vote_date': row['vote_date'], 'action': row['action']})
+        return commission_votes
 
 def get_participants(case_id):
     participants = {}
@@ -125,26 +136,27 @@ def get_participants(case_id):
             }
     return participants
 
-def get_documents(case_id, bucket, bucket_name):
-    documents = []
+def get_sorted_respondents(participants):
+    """
+    Returns the respondents in a MUR sorted in the order of most important to least important
+    """
+    SORTED_RESPONDENT_ROLES = ['Primary Respondent', 'Respondent', 'Previous Respondent']
+    respondents = []
+    for role in SORTED_RESPONDENT_ROLES:
+        respondents.extend(sorted([p['name'] for p in participants if p['role'] == role]))
+    return respondents
+
+def get_subjects(case_id):
+    subjects = []
     with db.engine.connect() as conn:
-        rs = conn.execute(DOCUMENTS, case_id)
+        rs = conn.execute(SUBJECTS, case_id)
         for row in rs:
-            document = {
-                'document_id': row['document_id'],
-                'category': row['category'],
-                'description': row['description'],
-                'length': row['length'],
-                'text': row['ocrtext'],
-                'document_date': row['document_date'],
-            }
-            pdf_key = 'legal/murs/current/%s.pdf' % row['document_id']
-            logger.info("S3: Uploading {}".format(pdf_key))
-            bucket.put_object(Key=pdf_key, Body=bytes(row['fileimage']),
-                    ContentType='application/pdf', ACL='public-read')
-            document['url'] = "https://%s.s3.amazonaws.com/%s" % (bucket_name, pdf_key)
-            documents.append(document)
-    return documents
+            if row['rel']:
+                subject_str = row['subj'] + "-" + row['rel']
+            else:
+                subject_str = row['subj']
+            subjects.append(subject_str)
+    return subjects
 
 def assign_citations(participants, case_id):
     with db.engine.connect() as conn:
@@ -158,7 +170,7 @@ def assign_citations(participants, case_id):
                 parse_statutory_citations(row['statutory_citation'], case_id, entity_id))
             participants[entity_id]['citations'][row['stage']].extend(
                 parse_regulatory_citations(row['regulatory_citation'], case_id, entity_id))
-            
+
 def parse_statutory_citations(statutory_citation, case_id, entity_id):
     citations = []
     if statutory_citation:
@@ -205,21 +217,26 @@ def parse_regulatory_citations(regulatory_citation, case_id, entity_id):
                     regulatory_citation, entity_id, case_id)
     return citations
 
-def get_open_and_close_dates(case_id):
+def get_documents(case_id, bucket, bucket_name):
+    documents = []
     with db.engine.connect() as conn:
-        rs = conn.execute(OPEN_AND_CLOSE_DATES, case_id)
-        open_date, close_date = rs.fetchone()
-    return open_date, close_date
-
-def get_sorted_respondents(participants):
-    """
-    Returns the respondents in a MUR sorted in the order of most important to least important
-    """
-    SORTED_RESPONDENT_ROLES = ['Primary Respondent', 'Respondent', 'Previous Respondent']
-    respondents = []
-    for role in SORTED_RESPONDENT_ROLES:
-        respondents.extend(sorted([p['name'] for p in participants if p['role'] == role]))
-    return respondents
+        rs = conn.execute(DOCUMENTS, case_id)
+        for row in rs:
+            document = {
+                'document_id': row['document_id'],
+                'category': row['category'],
+                'description': row['description'],
+                'length': row['length'],
+                'text': row['ocrtext'],
+                'document_date': row['document_date'],
+            }
+            pdf_key = 'legal/murs/current/%s.pdf' % row['document_id']
+            logger.info("S3: Uploading {}".format(pdf_key))
+            bucket.put_object(Key=pdf_key, Body=bytes(row['fileimage']),
+                    ContentType='application/pdf', ACL='public-read')
+            document['url'] = "https://%s.s3.amazonaws.com/%s" % (bucket_name, pdf_key)
+            documents.append(document)
+    return documents
 
 def remove_reclassification_notes(statutory_citation):
     """ Statutory citations include notes on reclassification of the form
