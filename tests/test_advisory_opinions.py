d4effd15bec5423ca148aba8f7297af47881588c
Vraj Mohan <radhakrishnan.vrajmohan@gsa.gov>
2017-02-13 17:18:39 -0800

    Use consistent quotes

--- tests/test_advisory_opinions.py
+++ tests/test_advisory_opinions.py
@@ -25,7 +25,7 @@ EMPTY_SET = set()
     ("1994-doesn't match pattern", {"1994-01"}, EMPTY_SET),
 ])
 def test_parse_regulatory_citations(text, filter_set, expected):
-    regex = re.compile(r'\b\d{4,4}-\d+\b')
+    regex = re.compile(r"\b\d{4,4}-\d+\b")
     assert get_filtered_matches(text, regex, filter_set) == expected
 
 
@@ -34,12 +34,12 @@ class TestLoadAdvisoryOpinions(BaseTestCase):
     def setUpClass(cls):
         super(TestLoadAdvisoryOpinions, cls).setUpClass()
         subprocess.check_call(
-            ['psql', TEST_CONN, '-f', 'data/load_advisory_opinions_schema.sql'])
+            ["psql", TEST_CONN, "-f", "data/load_advisory_opinions_schema.sql"])
 
     @classmethod
     def tearDownClass(cls):
         subprocess.check_call(
-            ['psql', TEST_CONN, '-c', 'DROP SCHEMA aouser CASCADE'])
+            ["psql", TEST_CONN, "-c", "DROP SCHEMA aouser CASCADE"])
         super(TestLoadAdvisoryOpinions, cls).tearDownClass()
 
     def setUp(self):
@@ -50,10 +50,10 @@ class TestLoadAdvisoryOpinions(BaseTestCase):
         self.connection.close()
         rest.db.session.remove()
 
-    @patch('webservices.legal_docs.advisory_opinions.get_bucket')
+    @patch("webservices.legal_docs.advisory_opinions.get_bucket")
     def test_pending_ao(self, get_bucket):
         expected_ao = {
-            "no": '2017-01',
+            "no": "2017-01",
             "name": "An AO name",
             "summary": "An AO summary",
             "is_pending": True,
@@ -68,12 +68,12 @@ class TestLoadAdvisoryOpinions(BaseTestCase):
 
         assert actual_ao == expected_ao
 
-    @patch('webservices.legal_docs.advisory_opinions.get_bucket')
+    @patch("webservices.legal_docs.advisory_opinions.get_bucket")
     def test_ao_with_requestors(self, get_bucket):
         expected_requestor_names = ["The Manchurian Candidate", "Federation of Interstate Truckers"]
         expected_requestor_types = ["Federal candidate/candidate committee/officeholder", "Labor Organization"]
         expected_ao = {
-            "no": '2017-01',
+            "no": "2017-01",
             "name": "An AO name",
             "summary": "An AO summary",
             "is_pending": True,
@@ -93,17 +93,17 @@ class TestLoadAdvisoryOpinions(BaseTestCase):
         assert set(actual_ao["requestor_names"]) == set(expected_requestor_names)
         assert set(actual_ao["requestor_types"]) == set(expected_requestor_types)
 
-    @patch('webservices.legal_docs.advisory_opinions.get_bucket')
+    @patch("webservices.legal_docs.advisory_opinions.get_bucket")
     def test_completed_ao_with_docs(self, get_bucket):
         expected_document = {
             "document_id": 1,
             "category": "Final Opinion",
-            "text": 'Some Text',
-            "description": 'Some Description',
+            "text": "Some Text",
+            "description": "Some Description",
             "document_date": datetime.datetime(2017, 2, 9, 0, 0)
         }
         expected_ao = {
-            "no": '2017-01',
+            "no": "2017-01",
             "name": "An AO name",
             "summary": "An AO summary",
             "is_pending": False,
@@ -119,21 +119,21 @@ class TestLoadAdvisoryOpinions(BaseTestCase):
         actual_ao = next(get_advisory_opinions())
         assert actual_ao["is_pending"] is False
 
-        actual_document = actual_ao['documents'][0]
+        actual_document = actual_ao["documents"][0]
         for key in expected_document:
             assert actual_document[key] == expected_document[key]
 
-    @patch('webservices.legal_docs.advisory_opinions.get_bucket')
+    @patch("webservices.legal_docs.advisory_opinions.get_bucket")
     def test_ao_citations(self, get_bucket):
         ao1_document = {
             "document_id": 1,
             "category": "Final Opinion",
-            "text": 'Not an AO reference 1776-01',
-            "description": 'Some Description',
+            "text": "Not an AO reference 1776-01",
+            "description": "Some Description",
             "document_date": datetime.datetime(2017, 2, 9, 0, 0)
         }
         ao1 = {
-            "no": '2017-01',
+            "no": "2017-01",
             "name": "1st AO name",
             "summary": "1st AO summary",
             "is_pending": False,
@@ -147,12 +147,12 @@ class TestLoadAdvisoryOpinions(BaseTestCase):
         ao2_document = {
             "document_id": 2,
             "category": "Final Opinion",
-            "text": 'Reference to AO 2017-01',
-            "description": 'Some Description',
+            "text": "Reference to AO 2017-01",
+            "description": "Some Description",
             "document_date": datetime.datetime(2017, 2, 9, 0, 0)
         }
         ao2 = {
-            "no": '2017-02',
+            "no": "2017-02",
             "name": "2nd AO name",
             "summary": "2nd AO summary",
             "is_pending": False,
@@ -171,19 +171,19 @@ class TestLoadAdvisoryOpinions(BaseTestCase):
         actual_aos = [ao for ao in get_advisory_opinions()]
         assert len(actual_aos) == 2
 
-        actual_ao1 = next(filter(lambda a: a['no'] == '2017-01', actual_aos))
-        actual_ao2 = next(filter(lambda a: a['no'] == '2017-02', actual_aos))
+        actual_ao1 = next(filter(lambda a: a["no"] == "2017-01", actual_aos))
+        actual_ao2 = next(filter(lambda a: a["no"] == "2017-02", actual_aos))
 
-        assert actual_ao1['citations'] == []
-        assert actual_ao1['cited_by'] == [{'no': '2017-02', 'name': '2nd AO name'}]
+        assert actual_ao1["citations"] == []
+        assert actual_ao1["cited_by"] == [{"no": "2017-02", "name": "2nd AO name"}]
 
-        assert actual_ao2['citations'] == [{'no': '2017-01', 'name': '1st AO name'}]
-        assert actual_ao2['cited_by'] == []
+        assert actual_ao2["citations"] == [{"no": "2017-01", "name": "1st AO name"}]
+        assert actual_ao2["cited_by"] == []
 
     def create_ao(self, ao_id, ao):
         self.connection.execute(
             "INSERT INTO aouser.ao (ao_id, ao_no, name, summary) "
-            "VALUES (%s, %s, %s, %s)", ao_id, ao['no'], ao['name'], ao['summary'])
+            "VALUES (%s, %s, %s, %s)", ao_id, ao["no"], ao["name"], ao["summary"])
 
     def create_document(self, ao_id, document):
         self.connection.execute(
@@ -191,13 +191,13 @@ class TestLoadAdvisoryOpinions(BaseTestCase):
             INSERT INTO aouser.document
             (document_id, ao_id, category, ocrtext, fileimage, description, document_date)
             VALUES (%s, %s, %s, %s, %s, %s, %s)""",
-            document['document_id'],
+            document["document_id"],
             ao_id,
-            document['category'],
-            document['text'],
-            document['text'],
-            document['description'],
-            document['document_date']
+            document["category"],
+            document["text"],
+            document["text"],
+            document["description"],
+            document["document_date"]
         )
 
     def create_requestor(self, ao_id, entity_id, requestor_name, requestor_type):
