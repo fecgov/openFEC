7c8a4c5998d312dea9f688691210dd6860ff783a
LindsayYoung <lindsayn.young@gsa.gov>
2017-02-24 17:16:18 -0500

    fixing namespace

--- tasks.py
+++ tasks.py
@@ -152,8 +152,8 @@ def _detect_space(repo, branch=None, yes=False):
 DEPLOY_RULES = (
     ('prod', _detect_prod),
     ('stage', lambda _, branch: branch.startswith('release')),
-    ('dev', lambda _, branch: branch == 'develop'),
-    ('gov', lambda _, branch: branch == 'feature/gov-cloud-support'),
+    # ('dev', lambda _, branch: branch == 'develop'),
+    ('dev', lambda _, branch: branch == 'feature/gov-cloud-support'),
 )
 
 
