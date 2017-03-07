e2fc27add749ece122c80ed2c1a91e3ea55624f4
LindsayYoung <lindsayn.young@gsa.gov>
2017-03-07 11:13:01 -0500

    simplyfing run code and andding conditional login

--- tasks.py
+++ tasks.py
@@ -183,9 +183,8 @@ def deploy(ctx, space=None, branch=None, yes=False):
     ctx.run('cf api {0}'.format(api), echo=True)
 
     # Log in if necessary
-    # if os.getenv('FEC_CF_USERNAME') and os.getenv('FEC_CF_PASSWORD'):
-    auth_command = 'cf auth "$FEC_CF_USERNAME_{0}" "$FEC_CF_PASSWORD_{0}"'.format(space.upper())
-    ctx.run(auth_command, echo=True)
+    if os.getenv('FEC_CF_USERNAME') and os.getenv('FEC_CF_PASSWORD'):
+        ctx.run('cf auth "$FEC_CF_USERNAME_{0}" "$FEC_CF_PASSWORD_{0}"'.format(space.upper()), echo=True)
 
     # Target space
     ctx.run('cf target -o fec-beta-fec -s {0}'.format(space), echo=True)
