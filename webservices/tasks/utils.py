ce9366adf1390da17f8bfcf82bd803e89fa56151
Vraj Mohan <radhakrishnan.vrajmohan@gsa.gov>
2017-03-09 11:05:07 -0800

    Set region for S3 service

--- webservices/tasks/utils.py
+++ webservices/tasks/utils.py
@@ -12,6 +12,7 @@ def get_bucket():
     session = boto3.Session(
         aws_access_key_id=env.get_credential('access_key_id'),
         aws_secret_access_key=env.get_credential('secret_access_key'),
+        region_name=env.get_credential('region')
     )
     s3 = session.resource('s3')
     return s3.Bucket(env.get_credential('bucket'))
