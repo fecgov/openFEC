15f144d5aa18593cfe1bfd0c2abe21deede0cf26
LindsayYoung <lindsayn.young@gsa.gov>
2017-03-17 12:18:36 -0400

    adding the normal deploy process back

--- tasks.py
+++ tasks.py
@@ -152,8 +152,7 @@ def _detect_space(repo, branch=None, yes=False):
 DEPLOY_RULES = (
     ('prod', _detect_prod),
     ('stage', lambda _, branch: branch.startswith('release')),
-    # ('dev', lambda _, branch: branch == 'develop'),
-    ('dev', lambda _, branch: branch == 'feature/gov-cloud'),
+    ('dev', lambda _, branch: branch == 'develop'),
 )
 
 
