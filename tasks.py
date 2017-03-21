4cd944af11e2936cfd782f2623ffa3ecd1977ecd
Lindsay Young <lindsayn.young@gsa.gov>
2017-03-21 13:25:57 -0400

    deploy to stage

--- tasks.py
+++ tasks.py
@@ -151,9 +151,9 @@ def _detect_space(repo, branch=None, yes=False):
 
 DEPLOY_RULES = (
     ('prod', _detect_prod),
-    ('stage', lambda _, branch: branch.startswith('release')),
-    # ('dev', lambda _, branch: branch == 'develop'),
-    ('dev', lambda _, branch: branch == 'feature/gov-cloud'),
+#     ('stage', lambda _, branch: branch.startswith('release')),
+    ('dev', lambda _, branch: branch == 'develop'),
+    ('stage', lambda _, branch: branch == 'feature/gov-cloud'),
 )
 
 
