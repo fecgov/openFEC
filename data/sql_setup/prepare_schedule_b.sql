cb74177ab78c9460fd0863b42d5688be569812cc
Carlo Costino <carlo.costino@gsa.gov>
2017-05-22 15:57:42 -0400

    Refactor itemized schedule A/B processing to use "upsert"
    This changeset introduces a major refactoring in how we keep track of and process the itemized schedule data.  Instead of using the queue tables like before, we are now leveraging the new `INSERT INTO ... ON CONFLICT ...` functionality that PostgreSQL provides as of 9.5+.  This has allowed us to greatly simplify the triggers we have associated with the itemized schedule A and B data and remove a lot of complicated data processing and management code with the table partitioning we have setup for the API.  It also provides us with updates to the table as the changes happen instead of during a nightly process.
    
    These changes are only for schedule A and B right now, but at least schedule E could also benefit from a similar refactor in the future.

--- data/sql_setup/prepare_schedule_b.sql
+++ data/sql_setup/prepare_schedule_b.sql
@@ -2,37 +2,23 @@
 drop index if exists disclosure.nml_sched_b_link_id_idx;
 create index nml_sched_b_link_id_idx on disclosure.nml_sched_b (link_id);
 
--- Create table to hold sub_ids of records that fail during the nightly
--- processing so that they can be tried at a later time.
--- The "action" column denotes what should happen with the record:
---    insert, update, or delete
+-- Create table to hold sub_ids of records that fail to process when the
+-- Schedule B triggers are run so that they can be tried at a later time.
 drop table if exists ofec_sched_b_nightly_retries;
 create table ofec_sched_b_nightly_retries (
-    sub_id numeric(19,0) not null primary key,
-    action varchar(6) not null
+    sub_id numeric(19,0) not null primary key
 );
 create index on ofec_sched_b_nightly_retries (sub_id);
 
--- Create queue tables to hold changes to Schedule B
+
+-- Remove any queue table remnants as they are no longer needed.
 drop table if exists ofec_sched_b_queue_new;
 drop table if exists ofec_sched_b_queue_old;
-create table ofec_sched_b_queue_new as select * from fec_vsum_sched_b_vw limit 0;
-create table ofec_sched_b_queue_old as select * from fec_vsum_sched_b_vw limit 0;
-alter table ofec_sched_b_queue_new add column timestamp timestamp;
-alter table ofec_sched_b_queue_old add column timestamp timestamp;
-alter table ofec_sched_b_queue_new add column two_year_transaction_period smallint;
-alter table ofec_sched_b_queue_old add column two_year_transaction_period smallint;
-create index on ofec_sched_b_queue_new (sub_id);
-create index on ofec_sched_b_queue_old (sub_id);
-create index on ofec_sched_b_queue_new (timestamp);
-create index on ofec_sched_b_queue_old (timestamp);
-create index on ofec_sched_b_queue_new (two_year_transaction_period);
-create index on ofec_sched_b_queue_old (two_year_transaction_period);
 
 
--- Support for processing of schedule A itemized records that need to be
+-- Support for processing of schedule B itemized records that need to be
 -- retried.
--- Retry processing schedule A itemized records
+-- Retry processing schedule B itemized records
 create or replace function retry_processing_schedule_b_records(start_year integer) returns void as $$
 declare
     timestamp timestamp = current_timestamp;
@@ -43,32 +29,16 @@ begin
     for schedule_b_record in select * from ofec_sched_b_nightly_retries loop
         select into view_row * from fec_vsum_sched_b_vw where sub_id = schedule_b_record.sub_id;
 
-        if found then
-            two_year_transaction_period = get_transaction_year(view_row.disb_dt, view_row.rpt_yr);
+        if FOUND then
+            two_year_transaction_period = get_transaction_year(view_row.contb_receipt_dt, view_row.rpt_yr);
 
             if two_year_transaction_period >= start_year then
-                -- Determine which queue(s) the found record should go into.
-                case schedule_b_record.action
-                    when 'insert' then
-                        delete from ofec_sched_b_queue_new where sub_id = view_row.sub_id;
-                        insert into ofec_sched_b_queue_new values (view_row.*, timestamp, two_year_transaction_period);
-
-                        delete from ofec_sched_b_nightly_retries where sub_id = schedule_b_record.sub_id;
-                    when 'delete' then
-                        delete from ofec_sched_b_queue_old where sub_id = view_row.sub_id;
-                        insert into ofec_sched_b_queue_old values (view_row.*, timestamp, two_year_transaction_period);
-
-                        delete from ofec_sched_b_nightly_retries where sub_id = schedule_b_record.sub_id;
-                    when 'update' then
-                        delete from ofec_sched_b_queue_new where sub_id = view_row.sub_id;
-                        delete from ofec_sched_b_queue_old where sub_id = view_row.sub_id;
-                        insert into ofec_sched_b_queue_new values (view_row.*, timestamp, two_year_transaction_period);
-                        insert into ofec_sched_b_queue_old values (view_row.*, timestamp, two_year_transaction_period);
-
-                        delete from ofec_sched_b_nightly_retries where sub_id = schedule_b_record.sub_id;
-                    else
-                        raise warning 'Invalid action supplied, record not processed: %', schedule_b_record.action;
-                end case;
+                -- TODO:  Figure out how to get this in the right child table
+                --        automatically.
+                -- TODO:  Add the rest of the required SET component of the ON
+                --        CONFLICT clause.
+                insert into ofec_sched_b_master values (view_row.*, timestamp, two_year_transaction_period) on conflict (sub_id) do update;
+                delete from ofec_sched_b_nightly_retries where sub_id = schedule_b_record.sub_id;
             end if;
         else
             raise notice 'sub_id % still not found', schedule_b_record.sub_id;
@@ -78,132 +48,54 @@ end
 $$ language plpgsql;
 
 
--- Create trigger to maintain Schedule A queues for inserts and updates
--- These happen after a row is inserted/updated so that we can leverage pulling
--- the new record information from the view itself, which contains the data in
--- the structure that our tables expect it to be in.
-create or replace function ofec_sched_b_insert_update_queues() returns trigger as $$
+-- Create trigger method to monitor Schedule B records for inserts and updates.
+-- These happen after a row is inserted/updated so that we can leverage
+-- pulling the new record information from the view itself, which contains the
+-- data in the structure that our tables expect it to be in.
+create or replace function ofec_sched_b_upsert_record() returns trigger as $$
 declare
     start_year int = TG_ARGV[0]::int;
     timestamp timestamp = current_timestamp;
     two_year_transaction_period smallint;
     view_row fec_vsum_sched_b_vw%ROWTYPE;
 begin
-    if tg_op = 'INSERT' then
-        select into view_row * from fec_vsum_sched_b_vw where sub_id = new.sub_id;
+    select into view_row * from fec_vsum_sched_b_vw where sub_id = new.sub_id;
 
-        -- Check to see if the resultset returned anything from the view.  If
-        -- it did not, skip the processing of the record, otherwise we'll end
-        -- up with a record full of NULL values.
-        -- "FOUND" is a PL/pgSQL boolean variable set to false initially in
-        -- any PL/pgSQL function and reset whenever certain statements are
-        -- run, e.g., a "SELECT INTO..." statement.  For more information,
-        -- visit here:
-        -- https://www.postgresql.org/docs/current/static/plpgsql-statements.html#PLPGSQL-STATEMENTS-DIAGNOSTICS
-        if FOUND then
-            two_year_transaction_period = get_transaction_year(new.disb_dt, view_row.rpt_yr);
+    -- Check to see if the resultset returned anything from the view.  If it
+    -- did not, skip the processing of the record, otherwise we'll end up with
+    -- a record full of NULL values.
+    -- "FOUND" is a PL/pgSQL boolean variable set to false initially in any
+    -- PL/pgSQL function and reset whenever certain statements are run, e.g.,
+    -- a "SELECT INTO..." statement.  For more information, visit here:
+    -- https://www.postgresql.org/docs/current/static/plpgsql-statements.html#PLPGSQL-STATEMENTS-DIAGNOSTICS
+    if FOUND then
+        two_year_transaction_period = get_transaction_year(new.disb_dt, view_row.rpt_yr);
 
-            if two_year_transaction_period >= start_year then
-                delete from ofec_sched_b_queue_new where sub_id = view_row.sub_id;
-                insert into ofec_sched_b_queue_new values (view_row.*, timestamp, two_year_transaction_period);
-            end if;
-        else
-            -- We weren't able to successfully retrieve a row from the view,
-            -- so keep track of this sub_id if we haven't already so we can
-            -- try processing it again each night until we're able to
-            -- successfully process it.
-            delete from ofec_sched_b_nightly_retries where sub_id = new.sub_id;
-            insert into ofec_sched_b_nightly_retries values (new.sub_id, 'insert');
+        -- TODO:  Do we still want this check?
+        if two_year_transaction_period >= start_year then
+            -- TODO:  Figure out how to get this in the right child table
+            --        automatically.
+            -- TODO:  Add the rest of the required SET component of the ON
+            --        CONFLICT clause.
+            insert into ofec_sched_b_master values (view_row.*, timestamp, two_year_transaction_period) on conflict (sub_id) do update;
         end if;
-
-        return new;
-    elsif tg_op = 'UPDATE' then
-        select into view_row * from fec_vsum_sched_b_vw where sub_id = new.sub_id;
-
-        if FOUND then
-            two_year_transaction_period = get_transaction_year(new.disb_dt, view_row.rpt_yr);
-
-            if two_year_transaction_period >= start_year then
-                delete from ofec_sched_b_queue_new where sub_id = view_row.sub_id;
-                insert into ofec_sched_b_queue_new values (view_row.*, timestamp, two_year_transaction_period);
-            end if;
-        else
-            -- We weren't able to successfully retrieve a row from the view,
-            -- so keep track of this sub_id if we haven't already so we can
-            -- try processing it again each night until we're able to
-            -- successfully process it.
-            delete from ofec_sched_b_nightly_retries where sub_id = new.sub_id;
-            insert into ofec_sched_b_nightly_retries values (new.sub_id, 'update');
-        end if;
-
-        return new;
+    else
+        -- We weren't able to successfully retrieve a row from the view, so
+        -- keep track of this sub_id to try processing it again each night
+        -- until we're able to successfully process it.
+        insert into ofec_sched_a_nightly_retries values (new.sub_id) on conflict do nothing;
     end if;
+
+    return new;
 end
 $$ language plpgsql;
 
 
--- Create trigger to maintain Schedule A queues deletes and updates
--- These happen before a row is removed/updated so that we can leverage pulling
--- the new record information from the view itself, which contains the data in
--- the structure that our tables expect it to be in.
-create or replace function ofec_sched_b_delete_update_queues() returns trigger as $$
-declare
-    start_year int = TG_ARGV[0]::int;
-    timestamp timestamp = current_timestamp;
-    two_year_transaction_period smallint;
-    view_row fec_vsum_sched_b_vw%ROWTYPE;
+-- Create trigger method to remove deleted Schedule A data
+create or replace function ofec_sched_b_delete_record() returns trigger as $$
 begin
-    if tg_op = 'DELETE' then
-        select into view_row * from fec_vsum_sched_b_vw where sub_id = old.sub_id;
-
-        -- Check to see if the resultset returned anything from the view.  If
-        -- it did not, skip the processing of the record, otherwise we'll end
-        -- up with a record full of NULL values.
-        -- "FOUND" is a PL/pgSQL boolean variable set to false initially in
-        -- any PL/pgSQL function and reset whenever certain statements are
-        -- run, e.g., a "SELECT INTO..." statement.  For more information,
-        -- visit here:
-        -- https://www.postgresql.org/docs/current/static/plpgsql-statements.html#PLPGSQL-STATEMENTS-DIAGNOSTICS
-        if FOUND then
-            two_year_transaction_period = get_transaction_year(view_row.disb_dt, view_row.rpt_yr);
-
-            if two_year_transaction_period >= start_year then
-                delete from ofec_sched_b_queue_old where sub_id = view_row.sub_id;
-                insert into ofec_sched_b_queue_old values (view_row.*, timestamp, two_year_transaction_period);
-            end if;
-        else
-            -- We weren't able to successfully retrieve a row from the view,
-            -- so keep track of this sub_id if we haven't already so we can
-            -- try processing it again each night until we're able to
-            -- successfully process it.
-            delete from ofec_sched_b_nightly_retries where sub_id = old.sub_id;
-            insert into ofec_sched_b_nightly_retries values (old.sub_id, 'delete');
-        end if;
-
-        return old;
-    elsif tg_op = 'UPDATE' then
-        select into view_row * from fec_vsum_sched_b_vw where sub_id = old.sub_id;
-
-        if FOUND then
-            two_year_transaction_period = get_transaction_year(old.disb_dt, view_row.rpt_yr);
-
-            if two_year_transaction_period >= start_year then
-                delete from ofec_sched_b_queue_old where sub_id = view_row.sub_id;
-                insert into ofec_sched_b_queue_old values (view_row.*, timestamp, two_year_transaction_period);
-            end if;
-        else
-            -- We weren't able to successfully retrieve a row from the view,
-            -- so keep track of this sub_id if we haven't already so we can
-            -- try processing it again each night until we're able to
-            -- successfully process it.
-            delete from ofec_sched_b_nightly_retries where sub_id = old.sub_id;
-            insert into ofec_sched_b_nightly_retries values (old.sub_id, 'update');
-        end if;
-
-        -- We have to return new here because this record is intended to change
-        -- with an update.
-        return new;
-    end if;
+    delete from ofec_sched_b_master where sub_id = old.sub_id;
+    return old;
 end
 $$ language plpgsql;
 
@@ -211,10 +103,12 @@ $$ language plpgsql;
 -- Drop old trigger if it exists
 drop trigger if exists ofec_sched_b_queue_trigger on fec_vsum_sched_b_vw;
 
+
+-- Create new triggers
 drop trigger if exists nml_sched_b_after_trigger on disclosure.nml_sched_b;
 create trigger nml_sched_b_after_trigger after insert or update
-    on disclosure.nml_sched_b for each row execute procedure ofec_sched_b_insert_update_queues(:START_YEAR_AGGREGATE);
+    on disclosure.nml_sched_b for each row execute procedure ofec_sched_b_upsert_record(:START_YEAR_AGGREGATE);
 
 drop trigger if exists nml_sched_b_before_trigger on disclosure.nml_sched_b;
 create trigger nml_sched_b_before_trigger before delete or update
-    on disclosure.nml_sched_b for each row execute procedure ofec_sched_b_delete_update_queues(:START_YEAR_AGGREGATE);
+    on disclosure.nml_sched_b for each row execute procedure ofec_sched_b_delete_record(:START_YEAR_AGGREGATE);
