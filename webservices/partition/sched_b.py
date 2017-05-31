2b14dc13d4f0bfa9ed6dfb58467eb10890ffa518
LindsayYoung <lindsayn.young@gsa.gov>
2017-05-31 16:30:55 -0700

    Naming the indexes explicitly because sqlalchemy's auto naming was over writing them

--- webservices/partition/sched_b.py
+++ webservices/partition/sched_b.py
@@ -64,12 +64,12 @@ class SchedBGroup(TableGroup):
             sa.Index(None, c.clean_recipient_cmte_id),
             sa.Index(None, c.two_year_transaction_period),
 
-            sa.Index(None, c.disb_dt, c[cls.primary]),
-            sa.Index(None, c.disb_amt, c[cls.primary]),
+            sa.Index('ix_{0}_sub_id_date_tmp'.format(child.name[:-4]), c.disb_dt, c[cls.primary]),
+            sa.Index('ix_{0}_sub_id_amount_tmp'.format(child.name[:-4]), c.disb_amt, c[cls.primary]),
 
-            sa.Index(None, c.cmte_id, c[cls.primary]),
-            sa.Index(None, c.cmte_id, c.disb_dt, c[cls.primary]),
-            sa.Index(None, c.cmte_id, c.disb_amt, c[cls.primary]),
+            sa.Index('ix_{0}_cmte_id_tmp'.format(child.name[:-4]), c.cmte_id, c[cls.primary]),
+            sa.Index('ix_{0}_cmte_id_date_tmp'.format(child.name[:-4]), c.cmte_id, c.disb_dt, c[cls.primary]),
+            sa.Index('ix_{0}_cmte_id_amount_tmp'.format(child.name[:-4]), c.cmte_id, c.disb_amt, c[cls.primary]),
 
             sa.Index(None, c.recipient_name_text, postgresql_using='gin'),
             sa.Index(None, c.disbursement_description_text, postgresql_using='gin'),
