7641f4fb5f29a8dec0be4a15febcdb6d1e84a0cc
LindsayYoung <lindsayn.young@gsa.gov>
2017-06-14 18:39:59 -0400

    Bases the election year off of dates. We were missing special elections before. We can't use the candidate history directly because people file for the wrong election. The date data starts in 2003.

--- data/sql_updates/election_outcome.sql
+++ data/sql_updates/election_outcome.sql
@@ -1,42 +1,48 @@
--- See discussion in https://github.com/18F/openFEC-web-app/issues/514
+-- Powers election search merges with ofec_cand_history to make election views
 drop materialized view if exists ofec_election_result_mv_tmp;
 create materialized view ofec_election_result_mv_tmp as
-(
-    select fec_election_yr - 4 as election_yr, *
+-- get the years with election records by office, state and district
+-- this data starts in 2003
+with election_dates as(
+    select
+        election_state,
+        office_sought,
+        election_district,
+        election_yr
+    from fecapp.trc_election
+        group by
+        election_state,
+        office_sought,
+        election_district,
+        election_yr
+),
+-- Pull background info for the races
+-- See discussion in https://github.com/18F/openFEC-web-app/issues/514
+incumbent_info as(
+    select distinct on (cand_office_st, cand_office_district, cand_election_yr) *
     from disclosure.cand_valid_fec_yr
     where
         cand_ici = 'I' and
-        cand_office = 'P' and
-        fec_election_yr in (cand_election_yr, cand_election_yr + 4)
-    order by fec_election_yr desc
-)
-union all
-(
-    select distinct on (cand_office_st, cand_election_yr)
-        cand_election_yr - 6 as election_yr, *
-    from disclosure.cand_valid_fec_yr
-    where
-        cand_ici = 'I' and
-        cand_office = 'S'
-    order by
-        cand_office_st,
-        cand_election_yr desc,
-        latest_receipt_dt desc
-)
-union all
-(
-    select distinct on (cand_office_st, cand_office_district, cand_election_yr)
-        cand_election_yr - 2 as election_yr, *
-    from disclosure.cand_valid_fec_yr
-    where
-        cand_ici = 'I' and
-        cand_office = 'H'
+        fec_election_yr >= 2003
     order by
         cand_office_st,
         cand_office_district,
         cand_election_yr desc,
         latest_receipt_dt desc
 )
+select *, incumbent_info.*
+from election_dates
+left join
+    incumbent_info fec on
+    election_dates.election_state = fec.cand_office_st and
+    election_dates.office_sought = fec.cand_office and
+    election_dates.election_district = fec.cand_office_district and
+    election_dates.election_yr = fec.cand_election_yr
+order by
+    cand_office_st,
+    cand_office_district,
+    cand_election_yr desc,
+    latest_receipt_dt desc
 ;
 
 create unique index on ofec_election_result_mv_tmp (cand_valid_yr_id);
