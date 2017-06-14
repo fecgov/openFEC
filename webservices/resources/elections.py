52e474a778201dc2683514aca3ce6a3a40bfec1e
LindsayYoung <lindsayn.young@gsa.gov>
2017-06-14 18:39:59 -0400

    Bases the election year off of dates. We were missing special elections before. We can't use the candidate history directly because people file for the wrong election. The date data starts in 2003.

--- webservices/resources/elections.py
+++ webservices/resources/elections.py
@@ -67,7 +67,7 @@ class ElectionList(utils.Resource):
                 ],
                 else_=4,
             ).label('_office_status'),
-        ).join(
+        ).innerjoin(
             ElectionResult,
             sa.and_(
                 elections.c.state == ElectionResult.cand_office_st,
