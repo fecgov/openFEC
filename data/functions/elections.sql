c2c8cd4935e0c196f36407a5c25433b8ba4b916b
LindsayYoung <lindsayn.young@gsa.gov>
2017-06-19 14:54:55 -0400

    Moved functions to the function folder. changed unique id to idx, because we will look up the last incumbent record so that can now be repeated.

---
+++ data/functions/elections.sql
@@ -0,0 +1,34 @@
+-- Incumbents are not put in for future elections I am giving the data a little
+-- padding in figuring out when to look up the most recent incumbent in that seat.
+
+create or replace function check_incumbent_char(office char, election_yr numeric, cand_election_yr numeric, value char)
+returns text as $$
+    begin
+        return case
+        -- add upper?
+            when 'office' = 'H' and election_yr - cand_election_yr <= 3
+                then value
+            when 'office' = 'S' and election_yr - cand_election_yr <= 7
+                then value
+            when 'office' = 'P' and election_yr - cand_election_yr <= 5
+                then value
+            else null
+        end;
+    end
+$$ language plpgsql;
+
+create or replace function check_incumbent_numeric(office char, election_yr numeric, cand_election_yr numeric, value numeric)
+returns text as $$
+    begin
+        return case
+        -- add upper?
+            when 'office' = 'H' and election_yr - cand_election_yr <= 3
+                then value
+            when 'office' = 'S' and election_yr - cand_election_yr <= 7
+                then value
+            when 'office' = 'P' and election_yr - cand_election_yr <= 5
+                then value
+            else null
+        end;
+    end
+$$ language plpgsql;
