3bd8997eaf668b8bb96afd6c13804de30650d8c9
LindsayYoung <lindsayn.young@gsa.gov>
2017-06-19 14:54:55 -0400

    Moved functions to the function folder. changed unique id to idx, because we will look up the last incumbent record so that can now be repeated.

--- data/sql_updates/election_outcome.sql
+++ data/sql_updates/election_outcome.sql
@@ -1,47 +1,17 @@
-create or replace function check_incumbent_char(office char, election_yr numeric, cand_election_yr numeric, value char)
-returns text as $$
-    begin
-        return case acronym
-        -- add upper?
-            when 'office' = 'H' and election_yr - cand_election_yr <= 2
-                then value
-            when 'office' = 'S' and election_yr - cand_election_yr <= 6
-                then value
-            when 'office' = 'P' and election_yr - cand_election_yr <= 4
-                then value
-            else null
-        end;
-    end
-$$ language plpgsql;
-
-create or replace function check_incumbent_numeric(office char, election_yr numeric, cand_election_yr numeric, value numeric)
-returns text as $$
-    begin
-        return case acronym
-        -- add upper?
-            when 'office' = 'H' and election_yr - cand_election_yr <= 2
-                then value
-            when 'office' = 'S' and election_yr - cand_election_yr <= 6
-                then value
-            when 'office' = 'P' and election_yr - cand_election_yr <= 4
-                then value
-            else null
-        end;
-    end
-$$ language plpgsql;
-
-
-
 -- Powers election search
-drop materialized view if exists ofec_election_result_mv_tmp;
-create materialized view ofec_election_result_mv_tmp as
+------------------------- drop materialized view if exists ofec_election_result_mv_tmp;
+------------------------- create materialized view ofec_election_result_mv_tmp as
 -- get the years with election records by office, state and district
 -- this data starts in 2003
 with election_dates as(
     select
-        election_state,
+        case when office_sought = 'P' then null
+            else election_state
+        end as election_state,
         office_sought,
-        election_district,
+       case when office_sought = 'H' and (trim(election_district) = '' or election_district is null) then '00'
+            else election_district
+        end as election_district,
         election_yr
     from fecapp.trc_election
         group by
@@ -65,27 +35,27 @@ incumbent_info as(
         cand_election_yr desc,
         latest_receipt_dt desc
 ),
-records_with_incuments as (
+records_with_incumbents as (
     select
         fec.cand_valid_yr_id,
         fec.cand_id,
-        fec.fec_election_yr,
-        fec.cand_election_yr,
+        ed.election_yr + ed.election_yr % 2 as fec_election_yr,
+        fec.cand_election_yr::numeric,
         fec.cand_status,
         fec.cand_ici,
-        fec.cand_office,
-        fec.cand_office_st,
-        fec.cand_office_district,
+        ed.office_sought as cand_office,
+        ed.election_state as cand_office_st,
+        ed.election_district as cand_office_district,
         fec.cand_pty_affiliation,
         fec.cand_name,
-        cand_election_yr as election_yr
-    from election_dates
-    inner join
+        ed.election_yr::numeric as election_yr
+    from election_dates ed
+    left join
         incumbent_info fec on
-        election_dates.election_state = fec.cand_office_st and
-        election_dates.office_sought = fec.cand_office and
-        election_dates.election_district = fec.cand_office_district and
-        election_dates.election_yr = fec.cand_election_yr
+        ed.election_state = fec.cand_office_st and
+        ed.office_sought = fec.cand_office and
+        ed.election_district = fec.cand_office_district and
+        ed.election_yr = fec.cand_election_yr
     order by
         cand_office_st,
         cand_office,
@@ -94,73 +64,72 @@ records_with_incuments as (
         latest_receipt_dt desc
 ),
 -- We don't have future candidates in the ICI data, so we want to look that up
-record_without_incubents as (
-    select distinct on (ed.cand_office_st, ed.cand_office_district, ed.cand_office, ed.cand_election_yr)
+record_without_incumbents as (
+    select * from records_with_incumbents
+    where cand_id is null
+),
+elections_without_incumbents as (
+    select distinct on (rwi.cand_office_st, rwi.cand_office_district, rwi.cand_office, rwi.election_yr)
         fec.cand_valid_yr_id,
         check_incumbent_char(
-            fec.cand_office::char, ed.cand_election_yr::numeric,
+            fec.cand_office::char, rwi.election_yr::numeric,
             fec.cand_election_yr::numeric, fec.cand_id::char
         ) as cand_id,
-        ed.cand_election_yr + ed.cand_election_yr % 2 as fec_election_yr,
+        rwi.fec_election_yr,
         check_incumbent_numeric(
-            fec.cand_office::char, ed.cand_election_yr::numeric,
+            fec.cand_office::char, rwi.election_yr::numeric,
             fec.cand_election_yr::numeric, fec.cand_election_yr::numeric
-        ) as cand_election_yr,
+        )::numeric as cand_election_yr,
         check_incumbent_char(
-            fec.cand_office::char, ed.cand_election_yr::numeric,
+            fec.cand_office::char, rwi.election_yr::numeric,
             fec.cand_election_yr::numeric, fec.cand_status::char
         ) cand_status,
         check_incumbent_char(
-            fec.cand_office::char, ed.cand_election_yr::numeric,
+            fec.cand_office::char, rwi.election_yr::numeric,
             fec.cand_election_yr::numeric, fec.cand_ici::char
         ) cand_ici,
-        ed.cand_office,
-        ed.cand_office_st,
-        ed.cand_office_district,
+        rwi.cand_office,
+        rwi.cand_office_st,
+        rwi.cand_office_district,
         check_incumbent_char(
-            fec.cand_office::char, ed.cand_election_yr::numeric,
+            fec.cand_office::char, rwi.election_yr::numeric,
             fec.cand_election_yr::numeric, fec.cand_pty_affiliation::char
         ) cand_pty_affiliation,
         check_incumbent_char(
-            fec.cand_office::char, ed.cand_election_yr::numeric,
+            fec.cand_office::char, rwi.election_yr::numeric,
             fec.cand_election_yr::numeric, fec.cand_name::char
         ) cand_name,
-        ed.cand_election_yr as election_yr
-    from election_dates ed
+        rwi.election_yr
+    from record_without_incumbents rwi
     left join
         incumbent_info fec on
-        election_dates.election_state = fec.cand_office_st and
-        election_dates.office_sought = fec.cand_office and
-        election_dates.election_district = fec.cand_office_district and
-        election_dates.election_yr < fec.cand_election_yr
---- trying to find no incumbent records
-    -- where fec.xx not exist (
-    --     select xx
-    --     from election_dates
-    --     left join on records_with_incuments
-    --     where fec.cand_id is null
-    --     order by
-    --         cand_office_st,
-    --         cand_office,
-    --         cand_office_district,
-    --         cand_election_yr desc,
-    --         latest_receipt_dt desc
-    -- )
+        rwi.cand_office_st = fec.cand_office_st and
+        rwi.cand_office_district = fec.cand_office_district and
+        rwi.cand_office = fec.cand_office and
+        rwi.cand_election_yr > fec.cand_election_yr
     order by
-        cand_office_st,
-        cand_office,
-        cand_office_district,
-        cand_election_yr desc,
-        latest_receipt_dt desc
+        rwi.cand_office_st,
+        rwi.cand_office,
+        rwi.cand_office_district,
+        rwi.election_yr desc,
+        fec.cand_election_yr desc,
+        fec.latest_receipt_dt desc
+),
+combined as (
+    select * from
+        records_with_incumbents
+        where cand_id is not null
+    union all
+    select * from
+        record_without_incumbents
 )
-select * from
-    records_with_incuments
-union all
-select * from
-    record_without_incubents
+select
+    *,
+    row_number() over () as idx
+from combined
 ;
 -- change to idx
-create unique index on ofec_election_result_mv_tmp (cand_valid_yr_id);
+create unique index on ofec_election_result_mv_tmp (idx);
 
 create index on ofec_election_result_mv_tmp (election_yr);
 create index on ofec_election_result_mv_tmp (cand_office);
