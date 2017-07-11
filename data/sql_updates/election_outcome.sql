239690a1b8a83a822345255200eaeda0b2eac477
LindsayYoung <lindsayn.young@gsa.gov>
2017-07-11 18:33:41 -0400

    The data looks good for all offices in my spot check.

--- data/sql_updates/election_outcome.sql
+++ data/sql_updates/election_outcome.sql
@@ -3,15 +3,28 @@ drop materialized view if exists ofec_election_result_mv_tmp;
 create materialized view ofec_election_result_mv_tmp as
 -- get the years with election records by office, state and district
 -- this data starts in 2003
-with election_dates as(
+with election_dates_house_senate as(
     select
-        case when office_sought = 'P' then 'US'
-            else election_state
-        end as election_state,
+        election_state,
         office_sought,
         election_district,
         election_yr
     from fecapp.trc_election
+    where office_sought != 'P'
+        group by
+        election_state,
+        office_sought,
+        election_district,
+        election_yr
+),
+election_dates_president as(
+    select distinct on (election_yr)
+        'US'::char(2) as election_state,
+        office_sought,
+        election_district,
+        election_yr
+    from fecapp.trc_election
+    where office_sought = 'P'
         group by
         election_state,
         office_sought,
@@ -24,8 +37,7 @@ incumbent_info as(
     select distinct on (cand_office_st, cand_office_district, cand_office, cand_election_yr) *
     from disclosure.cand_valid_fec_yr
     where
-        cand_ici = 'I' and
-        fec_election_yr >= 2003
+        cand_ici = 'I'
     order by
         cand_office_st,
         cand_office,
@@ -51,7 +63,7 @@ records_with_incumbents_districts as (
         fec.cand_pty_affiliation,
         fec.cand_name,
         ed.election_yr::numeric as election_yr
-    from election_dates ed
+    from election_dates_house_senate ed
     left join
         incumbent_info fec on
         ed.election_state = fec.cand_office_st and
@@ -71,7 +83,7 @@ records_with_incumbents_districts as (
         latest_receipt_dt desc
 ),
 -- This creates election records and the incumbent info if applicable
--- for Senate, President and (House races without districts)
+-- for Senate and House races without districts
 records_with_incumbents as (
     select
         fec.cand_valid_yr_id,
@@ -90,24 +102,56 @@ records_with_incumbents as (
         fec.cand_pty_affiliation,
         fec.cand_name,
         ed.election_yr::numeric as election_yr
-    from election_dates ed
+    from election_dates_house_senate ed
     left join
         incumbent_info fec on
         ed.election_state = fec.cand_office_st and
         ed.office_sought = fec.cand_office and
         ed.election_yr = fec.cand_election_yr
-    where (ed.election_district is null or ed.election_district = '' or ed.election_district = ' ')
+    where (ed.election_district is null or ed.election_district = '' or ed.election_district = ' ') and
+        ed.office_sought != 'P'
     order by
         cand_office_st,
         cand_office,
         cand_office_district,
         cand_election_yr desc,
         latest_receipt_dt desc
+),
+records_with_incumbents_president as (
+     select
+        fec.cand_valid_yr_id,
+        fec.cand_id,
+        ed.election_yr + ed.election_yr % 2 as fec_election_yr,
+        fec.cand_election_yr::numeric,
+        fec.cand_status,
+        fec.cand_ici,
+        ed.office_sought as cand_office,
+        'US'::char(2) as cand_office_st,
+        cast('00' as varchar(2)) as cand_office_district,
+        fec.cand_pty_affiliation,
+        fec.cand_name,
+        ed.election_yr::numeric as election_yr
+    from election_dates_president ed
+    left join
+        incumbent_info fec on
+        ed.election_yr = fec.cand_election_yr
+    where
+        fec.cand_office = 'P'
+    order by
+        cand_election_yr desc,
+        latest_receipt_dt desc
+),
+combined as(
+    select * from records_with_incumbents_districts
+    union all
+    select * from records_with_incumbents
+    union all
+    select * from records_with_incumbents_president
 )
 select
     *,
     row_number() over () as idx
-from records_with_incumbents
+from combined
 ;
 
 create unique index on ofec_election_result_mv_tmp (idx);
