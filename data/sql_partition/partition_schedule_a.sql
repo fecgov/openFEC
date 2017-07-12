ce251b13ec568d59c199a33b75a8d9471c98002d
Vraj Mohan <radhakrishnan.vrajmohan@gsa.gov>
2017-07-12 14:15:17 -0700

    Rename indexes when renaming tables
    As index names have to be unique within the schema, rename them when
    renaming the underlying tables (ie dropping the '_tmp').
    Thanks, @ccostino!

--- data/sql_partition/partition_schedule_a.sql
+++ data/sql_partition/partition_schedule_a.sql
@@ -258,5 +258,5 @@ SELECT
     expand_line_number(filing_form, line_num) AS line_number_label
 FROM fec_fitem_sched_a_vw;
 
-SELECT rename_table_cascade('ofec_sched_a_master');
 SELECT finalize_itemized_schedule_a_tables(:PARTITION_START_YEAR, :PARTITION_END_YEAR);
+SELECT rename_table_cascade('ofec_sched_a_master');
