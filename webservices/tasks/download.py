028361c1b4b348d105c81dde6c9cba80b7e8b43e
Vraj Mohan <radhakrishnan.vrajmohan@gsa.gov>
2017-08-03 09:43:08 -0700

    Send downloads directly to S3
    This avoids using the local filesystem, which is good because we have
    occasionally run into "No space left on device" errors.
    
    We also avoid creating a zip file and a manifest and instead upload a
    CSV file directly. This has 2 consequences:
    - most users would prefer this because it avoids the step of having to
    unzip the downloaded zip file.
    - as there is no manifest, there is no way to show the query criteria
    that were used to create the download.
    
    This uses the package `smart_open` which unfortunately uses `boto` and
    not `boto3`. That is not too bad, as `boto` and `boto3` can co-exist.

--- webservices/tasks/download.py
+++ webservices/tasks/download.py
@@ -1,15 +1,13 @@
-import os
 import base64
 import hashlib
 import logging
-import zipfile
 import datetime
-import tempfile
 
 from webargs import flaskparser
 from flask_apispec.utils import resolve_annotations
 from postgres_copy import query_entities, copy_to
 from celery_once import QueueOnce
+from smart_open import smart_open
 
 from webservices import utils
 from webservices.common import counts
@@ -22,11 +20,6 @@ logger = logging.getLogger(__name__)
 
 IGNORE_FIELDS = {'page', 'per_page', 'sort', 'sort_hide_null'}
 
-COUNT_NOTE = (
-    '*Note: The record count displayed on the website is an estimate. The record '
-    'count in this manifest is accurate and will equal the rows in the accompanying '
-    'CSV file.'
-)
 
 def call_resource(path, qs):
     app = task_utils.get_app()
@@ -119,65 +112,23 @@ def get_s3_name(path, qs):
     # TODO: consider base64 vs hash
     raw = '{}{}'.format(path, qs)
     hashed = hashlib.sha224(raw.encode('utf-8')).hexdigest()
-    return '{}.zip'.format(hashed)
+    return '{}.csv'.format(hashed)
 
-def upload_s3(key, body):
-    task_utils.get_bucket().put_object(Key=key, Body=body)
-
-def make_manifest(resource, row_count, path):
-    with open(os.path.join(path, 'manifest.txt'), 'w') as fp:
-        fp.write('Time: {} (UTC)\n'.format(resource['timestamp']))
-        fp.write('Resource: {}\n'.format(resource['path']))
-        fp.write('*Count: {}\n'.format(row_count))
-        fp.write('Filters:\n\n')
-        fp.write('{}\n\n'.format(COUNT_NOTE))
-        fp.write(make_filters(resource))
-
-def make_filters(resource):
-    lines = []
-    for key, value in resource['kwargs'].items():
-        if key in resource['fields']:
-            value = ', '.join(map(format, value)) if isinstance(value, list) else value
-            description = resource['fields'][key].metadata.get('description')
-            lines.append(make_filter(key, value, description))
-    return '\n\n'.join(lines)
-
-def make_filter(key, value, description):
-    lines = []
-    lines.append('{}: {}'.format(key, value))
-    if description:
-        lines.append(description.strip())
-    return '\n'.join(lines)
-
-def wc(path):
-    with open(path) as fp:
-        return sum(1 for _ in fp.readlines())
 
 def make_bundle(resource):
-    with tempfile.TemporaryDirectory(dir=os.getenv('TMPDIR')) as tmpdir:
-        csv_path = os.path.join(tmpdir, 'data.csv')
-        with open(csv_path, 'w') as fp:
-            query = query_with_labels(
-                resource['query'],
-                resource['schema']
-            )
-            copy_to(
-                query,
-                fp,
-                db.session.connection().engine,
-                format='csv',
-                header=True
-            )
-        row_count = wc(csv_path) - 1
-        make_manifest(resource, row_count, tmpdir)
-        with tempfile.TemporaryFile(mode='w+b', dir=os.getenv('TMPDIR')) as tmpfile:
-            archive = zipfile.ZipFile(tmpfile, 'w')
-            for path in os.listdir(tmpdir):
-                _, arcname = os.path.split(path)
-                archive.write(os.path.join(tmpdir, path), arcname=arcname)
-            archive.close()
-            tmpfile.seek(0)
-            upload_s3(resource['name'], tmpfile)
+    s3_key = task_utils.get_s3_key(resource['name'])
+    with smart_open(s3_key, "wb") as fp:
+        query = query_with_labels(
+            resource['query'],
+            resource['schema']
+        )
+        copy_to(
+            query,
+            fp,
+            db.session.connection().engine,
+            format='csv',
+            header=True
+        )
 
 @app.task(base=QueueOnce, once={'graceful': True})
 def export_query(path, qs):
@@ -198,4 +149,3 @@ def clear_bucket():
     for obj in task_utils.get_bucket().objects.all():
         if not obj.key.startswith(permanent_dir):
             obj.delete()
-
