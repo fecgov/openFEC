af605d975945bfc859048a1677b38487945abb29
Vraj Mohan <radhakrishnan.vrajmohan@gsa.gov>
2017-08-28 17:26:09 -0700

    Use composite key for aggregate tables
    This allows us to use ON CONFLICT for handling UPSERTs.
    Choosing unique constraint instead of primary key as NULLs are allowed.
    Cannot drop `idx` column although it is no longer needed because it is part
    of the Base Model.

--- data/sql_incremental_aggregates/prepare_schedule_a_aggregate_occupation.sql
+++ data/sql_incremental_aggregates/prepare_schedule_a_aggregate_occupation.sql
@@ -14,14 +14,15 @@ and is_individual(contb_receipt_amt, receipt_tp, line_num, memo_cd, memo_text, c
 group by cmte_id, cycle, occupation
 ;
 
-alter table ofec_sched_a_aggregate_occupation_tmp add column idx serial primary key;
+alter table ofec_sched_a_aggregate_occupation_tmp add column idx serial;
+alter table ofec_sched_a_aggregate_occupation_tmp add constraint uq_cmte_id_cycle_occupation unique (cmte_id, cycle, occupation);
 
 -- Create indices on aggregate
-create index on ofec_sched_a_aggregate_occupation_tmp (cmte_id, idx);
-create index on ofec_sched_a_aggregate_occupation_tmp (cycle, idx);
-create index on ofec_sched_a_aggregate_occupation_tmp (occupation, idx);
-create index on ofec_sched_a_aggregate_occupation_tmp (total, idx);
-create index on ofec_sched_a_aggregate_occupation_tmp (count, idx);
+create index on ofec_sched_a_aggregate_occupation_tmp (cmte_id);
+create index on ofec_sched_a_aggregate_occupation_tmp (cycle);
+create index on ofec_sched_a_aggregate_occupation_tmp (occupation);
+create index on ofec_sched_a_aggregate_occupation_tmp (total);
+create index on ofec_sched_a_aggregate_occupation_tmp (count);
 create index on ofec_sched_a_aggregate_occupation_tmp (cycle, cmte_id);
 
 -- Remove previous aggregate and rename new aggregate
