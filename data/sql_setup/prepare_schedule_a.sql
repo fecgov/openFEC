6891eeb5ff5c7b995ec64314962b178d872b4e30
Vraj Mohan <radhakrishnan.vrajmohan@gsa.gov>
2017-08-29 14:38:25 -0700

    Remove Schedule A queues

--- data/sql_setup/prepare_schedule_a.sql
+++ data/sql_setup/prepare_schedule_a.sql
@@ -3,27 +3,11 @@ drop table if exists ofec_sched_a_nightly_retries;
 drop function if exists retry_processing_schedule_a_records(start_year integer);
 
 
--- Create queue tables to hold changes to Schedule A
-drop table if exists ofec_sched_a_queue_new;
-drop table if exists ofec_sched_a_queue_old;
-create table ofec_sched_a_queue_new as select * from fec_fitem_sched_a_vw limit 0;
-create table ofec_sched_a_queue_old as select * from fec_fitem_sched_a_vw limit 0;
-alter table ofec_sched_a_queue_new add column timestamp timestamp;
-alter table ofec_sched_a_queue_old add column timestamp timestamp;
-alter table ofec_sched_a_queue_new add column two_year_transaction_period smallint;
-alter table ofec_sched_a_queue_old add column two_year_transaction_period smallint;
-create index on ofec_sched_a_queue_new (sub_id);
-create index on ofec_sched_a_queue_old (sub_id);
-create index on ofec_sched_a_queue_new (timestamp);
-create index on ofec_sched_a_queue_old (timestamp);
-create index on ofec_sched_a_queue_new (two_year_transaction_period);
-create index on ofec_sched_a_queue_old (two_year_transaction_period);
-
--- Create trigger to maintain Schedule A queues for inserts and updates
+-- Create trigger to maintain Schedule A for inserts and updates
 -- These happen after a row is inserted/updated so that we can leverage pulling
 -- the new record information from the view itself, which contains the data in
 -- the structure that our tables expect it to be in.
-create or replace function ofec_sched_a_insert_update_queues() returns trigger as $$
+create or replace function ofec_sched_a_insert_update() returns trigger as $$
 declare
     start_year int = TG_ARGV[0]::int;
     view_row fec_fitem_sched_a_vw%ROWTYPE;
@@ -41,8 +25,6 @@ begin
     -- https://www.postgresql.org/docs/current/static/plpgsql-statements.html#PLPGSQL-STATEMENTS-DIAGNOSTICS
     if FOUND then
         if view_row.election_cycle >= start_year then
-            delete from ofec_sched_a_queue_new where sub_id = view_row.sub_id;
-            insert into ofec_sched_a_queue_new values (view_row.*, current_timestamp, view_row.election_cycle);
             child_table_name = 'ofec_sched_a_' || get_partition_suffix(view_row.election_cycle);
             EXECUTE format('
             INSERT INTO %I
@@ -304,11 +286,11 @@ end
 $$ language plpgsql;
 
 
--- Create trigger to maintain Schedule A queues deletes and updates
+-- Create trigger to maintain Schedule A deletes and updates
 -- These happen before a row is removed/updated so that we can leverage pulling
 -- the new record information from the view itself, which contains the data in
 -- the structure that our tables expect it to be in.
-create or replace function ofec_sched_a_delete_update_queues() returns trigger as $$
+create or replace function ofec_sched_a_delete_update() returns trigger as $$
 declare
     start_year int = TG_ARGV[0]::int;
     view_row fec_fitem_sched_a_vw%ROWTYPE;
@@ -325,8 +307,6 @@ begin
         -- https://www.postgresql.org/docs/current/static/plpgsql-statements.html#PLPGSQL-STATEMENTS-DIAGNOSTICS
         if FOUND then
             if view_row.election_cycle >= start_year then
-                delete from ofec_sched_a_queue_old where sub_id = view_row.sub_id;
-                insert into ofec_sched_a_queue_old values (view_row.*, current_timestamp, view_row.election_cycle);
                 IF tg_op = 'DELETE' THEN
                     DELETE FROM ofec_sched_a_master WHERE sub_id = view_row.sub_id;
                 END IF;
@@ -348,19 +328,19 @@ $$ language plpgsql;
 -- Create new triggers
 drop trigger if exists nml_sched_a_after_trigger on disclosure.nml_sched_a;
 create trigger nml_sched_a_after_trigger after insert or update
-    on disclosure.nml_sched_a for each row execute procedure ofec_sched_a_insert_update_queues(:START_YEAR_AGGREGATE);
+    on disclosure.nml_sched_a for each row execute procedure ofec_sched_a_insert_update(:START_YEAR_AGGREGATE);
 
 drop trigger if exists nml_sched_a_before_trigger on disclosure.nml_sched_a;
 create trigger nml_sched_a_before_trigger before delete or update
-    on disclosure.nml_sched_a for each row execute procedure ofec_sched_a_delete_update_queues(:START_YEAR_AGGREGATE);
+    on disclosure.nml_sched_a for each row execute procedure ofec_sched_a_delete_update(:START_YEAR_AGGREGATE);
 
 drop trigger if exists f_item_sched_a_after_trigger on disclosure.f_item_receipt_or_exp;
 create trigger f_item_sched_a_after_trigger after insert or update
-    on disclosure.f_item_receipt_or_exp for each row execute procedure ofec_sched_a_insert_update_queues(:START_YEAR_AGGREGATE);
+    on disclosure.f_item_receipt_or_exp for each row execute procedure ofec_sched_a_insert_update(:START_YEAR_AGGREGATE);
 
 drop trigger if exists f_item_sched_a_before_trigger on disclosure.f_item_receipt_or_exp;
 create trigger f_item_sched_a_before_trigger before delete or update
-    on disclosure.f_item_receipt_or_exp for each row execute procedure ofec_sched_a_delete_update_queues(:START_YEAR_AGGREGATE);
+    on disclosure.f_item_receipt_or_exp for each row execute procedure ofec_sched_a_delete_update(:START_YEAR_AGGREGATE);
 
 CREATE OR REPLACE FUNCTION increment_aggregates(view_row fec_fitem_sched_a_vw) RETURNS VOID AS $$
 BEGIN
