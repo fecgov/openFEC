14e063fa6bc0cc885e63cca08291731b5e74be0b
pkfec <pkasireddy@fec.gov>
2017-09-13 00:12:31 -0400

    2631#: materialized view should be updated first with the new fields.temporaliry commenting the code, since travis build is failing due to missing fields from the ofec_totals_parties_mv

--- webservices/common/models/totals.py
+++ webservices/common/models/totals.py
@@ -35,6 +35,12 @@ class CommitteeTotals(BaseModel):
     last_debts_owed_by_committee = db.Column(db.Numeric(30, 2))
     last_debts_owed_to_committee = db.Column(db.Numeric(30, 2))
 
+    #Add additional fields and filters to /totals/{committee-type} endpoint#2631
+    # committee_name = db.Column(db.String, doc=docs.COMMITTEE_NAME)
+    # committee_type_full = db.Column(db.String, index=True, doc=docs.COMMITTEE_TYPE_FULL)
+    # committee_designation_full = db.Column(db.String, index=True, doc=docs.COMMITTEE_DESIGNATION_FULL)
+    # party_full = db.Column(db.String, doc=docs.COMMITTEE_PARTY_FULL)
+
 class CandidateCommitteeTotals(db.Model):
     __abstract__ = True
     #making this it's own model hieararchy until can figure out
