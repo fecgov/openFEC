bd209c4b9c4a952f06b6ebc79dc15e1fdb49e7b6
LindsayYoung <lindsayn.young@gsa.gov>
2017-09-14 13:21:21 -0400

    Testing circle build

--- tasks.py
+++ tasks.py
@@ -151,7 +151,9 @@ def _detect_space(repo, branch=None, yes=False):
 DEPLOY_RULES = (
     ('prod', _detect_prod),
     ('stage', lambda _, branch: branch.startswith('release')),
-    ('dev', lambda _, branch: branch == 'develop'),
+    # debug will remove if successful
+    ('dev', lambda _, branch: branch == 'feature/add-circleci-support'),
+    # ('dev', lambda _, branch: branch == 'develop'),
 )
 
 
