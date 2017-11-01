32e912c507c7b739609a19ac9c1139d0e3abbf6a
Vraj Mohan <radhakrishnan.vrajmohan@gsa.gov>
2017-11-01 12:39:58 -0700

    Revert "Update Schedule A aggregates from trigger"
    This reverts commit 53ac90566da502a83a87448d7226c5685339bf0d.

--- data/sql_incremental_aggregates/prepare_schedule_a_aggregate_employer.sql
+++ data/sql_incremental_aggregates/prepare_schedule_a_aggregate_employer.sql
@@ -32,3 +32,47 @@ create index ofec_sched_a_aggregate_employer_tmp_cycle_cmte_id on ofec_sched_a_a
 drop table if exists ofec_sched_a_aggregate_employer;
 alter table ofec_sched_a_aggregate_employer_tmp rename to ofec_sched_a_aggregate_employer;
 select rename_indexes('ofec_sched_a_aggregate_employer');
+
+-- Create update function
+create or replace function ofec_sched_a_update_aggregate_employer() returns void as $$
+begin
+    with new as (
+        select 1 as multiplier, cmte_id, rpt_yr, contbr_employer, contb_receipt_amt, receipt_tp, line_num, memo_cd, memo_text, contbr_id
+        from ofec_sched_a_queue_new
+    ),
+    old as (
+        select -1 as multiplier, cmte_id, rpt_yr, contbr_employer, contb_receipt_amt, receipt_tp, line_num, memo_cd, memo_text, contbr_id
+        from ofec_sched_a_queue_old
+    ),
+    patch as (
+        select
+            cmte_id,
+            rpt_yr + rpt_yr % 2 as cycle,
+            contbr_employer as employer,
+            sum(contb_receipt_amt * multiplier) as total,
+            sum(multiplier) as count
+        from (
+            select * from new
+            union all
+            select * from old
+        ) t
+        where contb_receipt_amt is not null
+        and is_individual(contb_receipt_amt, receipt_tp, line_num, memo_cd, memo_text, contbr_id, cmte_id)
+        group by cmte_id, cycle, employer
+    ),
+    inc as (
+        update ofec_sched_a_aggregate_employer ag
+        set
+            total = ag.total + patch.total,
+            count = ag.count + patch.count
+        from patch
+        where (ag.cmte_id, ag.cycle, ag.employer) = (patch.cmte_id, patch.cycle, patch.employer)
+    )
+    insert into ofec_sched_a_aggregate_employer (
+        select patch.* from patch
+        left join ofec_sched_a_aggregate_employer ag using (cmte_id, cycle, employer)
+        where ag.cmte_id is null
+    )
+    ;
+end
+$$ language plpgsql;
