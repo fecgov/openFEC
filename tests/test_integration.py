7f6ae2846fea2731728d60d31f36c293867f8ef5
Vraj Mohan <radhakrishnan.vrajmohan@gsa.gov>
2017-11-22 12:01:26 -0800

    Refactor test for last_day_of_month function

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -712,6 +712,19 @@ class TestViews(common.IntegrationTestCase):
         assert expected_tables.issubset(actual_tables)
         assert "ofec_sched_a_3005_3006" not in actual_tables
 
+    def test_last_day_of_month(self):
+        connection = db.engine.connect()
+        fixtures = [
+            (datetime.datetime(1999, 3, 21, 10, 20, 30), datetime.datetime(1999, 3, 31, 0, 0, 0)),
+            (datetime.datetime(2007, 4, 21, 10, 20, 30), datetime.datetime(2007, 4, 30, 0, 0, 0)),
+            (datetime.datetime(2017, 2, 21, 10, 20, 30), datetime.datetime(2017, 2, 28, 0, 0, 0)),
+        ]
+        for fixture in fixtures:
+            test_value, expected = fixture
+            returned_date = connection.execute("SELECT last_day_of_month(%s)", test_value).scalar()
+
+            assert returned_date == expected
+
     def test_filter_individual_sched_a(self):
         individuals = [
             factories.ScheduleAFactory(receipt_type='15J', filing_form='F3X'),
