3963cf5d9a06b1f757e8358908474ca48861eb87
LindsayYoung <lindsayn.young@gsa.gov>
2017-12-01 16:56:10 -0500

    impoved the table name and added back calendar formatting helper function

--- data/functions/calendar.sql
+++ data/functions/calendar.sql
@@ -1,3 +1,18 @@
+-- Descriptions and summaries are usually the same, so we are trying to only show the
+-- descriptions when it is not already included, That works for most things except court
+-- cases, advisory opinions and conferences.
+create or replace function describe_cal_event(event_name text, summary text, description text)
+returns text as $$
+    begin
+        return case
+            when event_name in ('Litigation', 'AOs and Rules', 'Conferences') then
+                summary || ' ' || description
+            else
+                description
+        end;
+    end
+$$ language plpgsql;
+
 -- Retrieves the last day of the month for a given timestamp.
 create or replace function last_day_of_month(timestamp)
 returns timestamp as $$
