c55ea5ec2eb83843a89e547e9b647b32b6296ced
Jun Li <jli@fec.gov>
2017-12-15 17:08:40 -0500

    Modify .sql follow flyway format

--- data/migrations/V44__auditsearch_mv.sql
+++ data/migrations/V44__auditsearch_mv.sql
@@ -1,5 +1,8 @@
--- 1) M_View: public.ofec_audit_case_mv
-DROP MATERIALIZED VIEW if exists ofec_audit_case_mv cascade;
+SET search_path = auditsearch, pg_catalog;
+
+--
+-- 1)Name: ofec_audit_case_mv; Type: MATERIALIZED VIEW; Schema: auditsearch; Owner: fec
+--
 CREATE MATERIALIZED VIEW ofec_audit_case_mv AS
 WITH 
 cmte_info AS (
@@ -9,20 +12,20 @@ cmte_info AS (
         cmte_audit_vw.cmte_dsgn,
         cmte_audit_vw.cmte_tp,
         cmte_audit_vw.cmte_desc
-    FROM auditsearch.cmte_audit_vw
+    FROM cmte_audit_vw
 ), 
 cand_info AS (
     SELECT DISTINCT ON (cand_audit_vw.cand_id, cand_audit_vw.fec_election_yr)          cand_audit_vw.cand_id,
         cand_audit_vw.fec_election_yr,
         cand_audit_vw.cand_name
-    FROM auditsearch.cand_audit_vw
+    FROM cand_audit_vw
 ),
 audit_case_finding_info AS (
     SELECT
         audit_case_finding.audit_case_id::integer as audit_case_id,    
         audit_case_finding.parent_finding_pk::integer as primary_category_id,
         audit_case_finding.child_finding_pk::integer as sub_category_id
-    FROM auditsearch.audit_case_finding
+    FROM audit_case_finding
 ),
 audit_case_init AS (
     SELECT  
@@ -40,7 +43,7 @@ audit_case_init AS (
         ac.audit_id::integer AS audit_id,
         ac.cand_id AS candidate_id,
         coalesce(cand_info.cand_name, 'No authorized candidate'::text) AS candidate_name
-    FROM auditsearch.audit_case ac
+    FROM audit_case ac
     LEFT JOIN cmte_info ON cmte_info.cmte_id::text = ac.cmte_id::text AND cmte_info.fec_election_yr = ac.election_cycle
     LEFT JOIN cand_info ON cand_info.cand_id::text = ac.cand_id::text AND cand_info.fec_election_yr = ac.election_cycle
 ),
@@ -49,21 +52,21 @@ audit_case_sebset AS (
         audit_case_finding_info.primary_category_id,
         audit_case_finding_info.sub_category_id,
         audit_case_finding_info.audit_case_id,
-        auditsearch.audit_case.election_cycle::integer AS cycle,
-        auditsearch.audit_case.cmte_id AS committee_id,
+        election_cycle::integer AS cycle,
+        audit_case.cmte_id AS committee_id,
         cmte_info.cmte_nm AS committee_name,
         cmte_info.cmte_dsgn AS committee_designation,
         cmte_info.cmte_tp AS committee_type,
         cmte_info.cmte_desc AS committee_description,
-        auditsearch.audit_case.far_release_date::date AS far_release_date,
-        auditsearch.audit_case.link_to_report,
-        auditsearch.audit_case.audit_id::integer AS audit_id,
-        auditsearch.audit_case.cand_id AS candidate_id,
+        audit_case.far_release_date::date AS far_release_date,
+        audit_case.link_to_report,
+        audit_case.audit_id::integer AS audit_id,
+        audit_case.cand_id AS candidate_id,
         coalesce(cand_info.cand_name, 'No authorized candidate'::text) AS candidate_name
     FROM audit_case_finding_info
-    LEFT JOIN auditsearch.audit_case ON (audit_case_finding_info.audit_case_id= auditsearch.audit_case.audit_case_id::integer)
-    LEFT JOIN cmte_info ON (cmte_info.cmte_id::text = auditsearch.audit_case.cmte_id::text and cmte_info.fec_election_yr = auditsearch.audit_case.election_cycle)
-    LEFT JOIN cand_info ON (cand_info.cand_id::text = auditsearch.audit_case.cand_id::text and cand_info.fec_election_yr = auditsearch.audit_case.election_cycle)
+    LEFT JOIN audit_case ON (audit_case_finding_info.audit_case_id= audit_case.audit_case_id::integer)
+    LEFT JOIN cmte_info ON (cmte_info.cmte_id::text = audit_case.cmte_id::text and cmte_info.fec_election_yr = audit_case.election_cycle)
+    LEFT JOIN cand_info ON (cand_info.cand_id::text = audit_case.cand_id::text and cand_info.fec_election_yr = audit_case.election_cycle)
 ),
 audit_case_all AS (
     SELECT
@@ -121,30 +124,34 @@ SELECT
 FROM audit_case_all
 ORDER BY cycle DESC
 WITH DATA;
+ALTER TABLE ofec_audit_case_mv OWNER TO fec;
 
 create unique index on ofec_audit_case_mv(idx);
 create index on ofec_audit_case_mv(audit_case_id);
 create index on ofec_audit_case_mv(primary_category_id,sub_category_id,audit_case_id);
 
 
--- 2)M_View: public.ofec_audit_case_category_rel_mv
-DROP MATERIALIZED VIEW if exists ofec_audit_case_category_rel_mv cascade;
+--
+-- 2)Name: ofec_audit_case_category_rel_mv; Type: MATERIALIZED VIEW; Schema: auditsearch; Owner: fec
+--
 CREATE MATERIALIZED VIEW ofec_audit_case_category_rel_mv AS
 SELECT 
     DISTINCT acf.audit_case_id::integer AS audit_case_id,
     acf.parent_finding_pk::integer AS primary_category_id,
     btrim(f.finding) as primary_category_name
-FROM auditsearch.audit_case_finding acf 
-LEFT JOIN auditsearch.finding f
+FROM audit_case_finding acf 
+LEFT JOIN finding f
 ON (acf.parent_finding_pk = f.finding_pk) 
 ORDER BY audit_case_id, primary_category_name
 WITH DATA;
+ALTER TABLE ofec_audit_case_category_rel_mv OWNER TO fec;
 
 create unique index on ofec_audit_case_category_rel_mv(audit_case_id,primary_category_id);
 
 
---3)M_View: public.ofec_audit_case_sub_category_rel_mv
-DROP MATERIALIZED VIEW if exists ofec_audit_case_sub_category_rel_mv cascade;
+--
+-- 3)Name: ofec_audit_case_sub_category_rel_mv; Type: MATERIALIZED VIEW; Schema: auditsearch; Owner: fec
+--
 CREATE MATERIALIZED VIEW ofec_audit_case_sub_category_rel_mv AS
 SELECT 
     acf.audit_case_id::integer AS audit_case_id,
@@ -152,17 +159,19 @@ SELECT
     btrim(pf.finding) AS primary_category_name,
     acf.child_finding_pk::integer AS sub_category_id,
     btrim(cf.finding) AS sub_category_name
-FROM auditsearch.audit_case_finding acf 
-LEFT JOIN auditsearch.finding pf ON (acf.parent_finding_pk = pf.finding_pK) 
-LEFT JOIN auditsearch.finding cf ON (acf.child_finding_pk = cf.finding_pK) 
+FROM audit_case_finding acf 
+LEFT JOIN finding pf ON (acf.parent_finding_pk = pf.finding_pK) 
+LEFT JOIN finding cf ON (acf.child_finding_pk = cf.finding_pK) 
 ORDER BY (acf.audit_case_id::integer), primary_category_name, sub_category_name
 WITH DATA;
+ALTER TABLE ofec_audit_case_sub_category_rel_mv OWNER TO fec;
 
 create unique index on ofec_audit_case_sub_category_rel_mv(audit_case_id,primary_category_id,sub_category_id);
 
 
---4)M_View: public.ofec_committee_fulltext_audit_mv
-drop materialized view if exists ofec_committee_fulltext_audit_mv;
+--
+-- 4)Name: ofec_committee_fulltext_audit_mv; Type: MATERIALIZED VIEW; Schema: auditsearch; Owner: fec
+--
 create materialized view ofec_committee_fulltext_audit_mv as
 select distinct on (cmte_id, cmte_nm)
     row_number() over () as idx,
@@ -174,14 +183,16 @@ case
         setweight(to_tsvector(cmte_id), 'B')
     else null::tsvector
 end as fulltxt
-from auditsearch.cmte_audit_vw order by cmte_id;
+from cmte_audit_vw order by cmte_id;
+ALTER TABLE ofec_committee_fulltext_audit_mv OWNER TO fec;
 
 create unique index on ofec_committee_fulltext_audit_mv(idx);
 create index on ofec_committee_fulltext_audit_mv using gin(fulltxt);
 
 
---5)M_View: public.ofec_candidate_fulltext_audit_mv
-drop materialized view if exists ofec_candidate_fulltext_audit_mv;
+--
+-- 5)Name: ofec_candidate_fulltext_audit_mv; Type: MATERIALIZED VIEW; Schema: auditsearch; Owner: fec
+--
 create materialized view ofec_candidate_fulltext_audit_mv as
 select distinct on (cand_id, cand_name)
     row_number() over () as idx,
@@ -193,9 +204,10 @@ case
         setweight(to_tsvector(cand_id), 'B')
     else null::tsvector
 end as fulltxt
-from auditsearch.cand_audit_vw order by cand_id;
+from cand_audit_vw order by cand_id;
+ALTER TABLE ofec_candidate_fulltext_audit_mv OWNER TO fec;
 
 create unique index on ofec_candidate_fulltext_audit_mv(idx);
-create index on ofec_candidate_fulltext_audit_mvs using gin(fulltxt);
+create index on ofec_candidate_fulltext_audit_mv using gin(fulltxt);
 
 
