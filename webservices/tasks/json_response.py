9993980f33941de9eb83a51fd5917b7338d341a6
Priyanka Kasireddy <pkasireddy@fec.gov>
2017-12-22 13:04:52 -0500

    Retrieve Cache calls files

---
+++ webservices/tasks/json_response.py
@@ -0,0 +1,56 @@
+import json
+import logging
+import traceback
+
+import flask
+
+from dataactcore.utils.responseException import ResponseException
+
+
+logger = logging.getLogger(__name__)
+
+
+class JsonResponse:
+    """ Used to create an http response object containing JSON """
+    debugMode = True
+
+    @staticmethod
+    def create(code, dictionary_data):
+        """
+        Creates a JSON response object
+        if debugMode is enabled errors are added
+        """
+        jsondata = flask.Response()
+        jsondata.headers["Content-Type"] = "application/json"
+        jsondata.status_code = code
+        jsondata.set_data(json.dumps(dictionary_data))
+        return jsondata
+
+    @staticmethod
+    def error(exception, error_code, **kwargs):
+        """ Create an http response object for specified error. We assume
+        we're in an exception context
+        Args:
+            exception: Exception to be represented by response object
+            error_code: Status code to be used in response
+            kwargs: Extra fields and values to be included in response
+        Returns:
+            Http response object containing specified error
+        """
+        response_dict = {}
+        for key in kwargs:
+            response_dict[key] = kwargs[key]
+
+        trace = traceback.extract_tb(exception.__traceback__, 10)
+        logger.exception('Route Error')
+        if JsonResponse.debugMode:
+            response_dict["message"] = str(exception)
+            response_dict["errorType"] = str(type(exception))
+            if isinstance(exception, ResponseException) and exception.wrappedException:
+                response_dict["wrappedType"] = str(type(exception.wrappedException))
+                response_dict["wrappedMessage"] = str(exception.wrappedException)
+            response_dict["trace"] = [str(entry) for entry in trace]
+            return JsonResponse.create(error_code, response_dict)
+        else:
+            response_dict["message"] = "An error has occurred"
+            return JsonResponse.create(error_code, response_dict)
---
+++ webservices/tasks/response_exception.py
@@ -0,0 +1,29 @@
+class ResponseException(Exception):
+    """ Exception wrapper to be used in an http response, allows exceptions to specify status codes when raised """
+    def __init__(self, message, status=500, error_class=None, error_type=None, **kwargs):
+        """ Create a new ResponseException
+        Args:
+            message: Message to be attached to exception
+            status: HTTP status code to be returned with this exception
+            error_class: Nested exception will be created of this class if included
+            error_type: Type ID written to error DB, should be one of the values in
+            dataactvalidator.validation_handlers.validationError.py
+            **kwargs: Additional fields to be attached to exception, primarily used for providing extra data to error DB
+        Returns:
+            ResponseException object
+        """
+        super(ResponseException, self).__init__(message)
+        self.status = status  # This will be used for the HTTP response status code, 500 if unspecified
+        self.errorType = error_type  # This is used for writing to the error DB
+        self.extraInfo = {}
+        for key in kwargs:
+            # Include extra error info
+            self.extraInfo[key] = kwargs[key]  # This can be written to the error DB for some error types
+        if error_class is None:
+            self.wrappedException = None  # Can be used to wrap another type of exception into a ResponseException
+        else:
+            try:
+                self.wrappedException = error_class(message)
+            except Exception:
+                # Some errors cannot be created with just a message, in that case create a string representation
+                self.wrappedException = str(error_class) + str(message)
