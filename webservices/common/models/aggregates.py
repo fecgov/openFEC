4248d5c58e0a3b5c0c00640fe10a7eae6973e499
hhe <hcao@fec.gov>
2018-01-09 12:16:37 -0500

    change the table name that was missed on last commit

--- webservices/common/models/aggregates.py
+++ webservices/common/models/aggregates.py
@@ -28,7 +28,7 @@ class ScheduleAByState(BaseAggregate):
 
 class ScheduleAByZip(BaseAggregate):
     __table_args__ = {'schema' : 'disclosure'}
-    __tablename__ = 'ofec_sched_a_aggregate_zip'
+    __tablename__ = 'dsc_sched_a_aggregate_zip'
     zip = db.Column(db.String, primary_key=True)
     state = db.Column(db.String, doc=docs.STATE_GENERIC)
     state_full = db.Column(db.String, doc=docs.STATE_GENERIC)
@@ -36,25 +36,25 @@ class ScheduleAByZip(BaseAggregate):
 
 class ScheduleAByEmployer(BaseAggregate):
     __table_args__ = {'schema' : 'disclosure'}
-    __tablename__ = 'ofec_sched_a_aggregate_employer'
+    __tablename__ = 'dsc_sched_a_aggregate_employer'
     employer = db.Column(db.String, primary_key=True, doc=docs.EMPLOYER)
 
 
 class ScheduleAByOccupation(BaseAggregate):
     __table_args__ = {'schema' : 'disclosure'}
-    __tablename__ = 'ofec_sched_a_aggregate_occupation'
+    __tablename__ = 'dsc_sched_a_aggregate_occupation'
     occupation = db.Column(db.String, primary_key=True, doc=docs.OCCUPATION)
 
 
 class ScheduleBByRecipient(BaseAggregate):
     __table_args__ = {'schema' : 'disclosure'}
-    __tablename__ = 'ofec_sched_b_aggregate_recipient'
+    __tablename__ = 'dsc_sched_b_aggregate_recipient'
     recipient_name = db.Column('recipient_nm', db.String, primary_key=True, doc=docs.RECIPIENT_NAME)
 
 
 class ScheduleBByRecipientID(BaseAggregate):
     __table_args__ = {'schema' : 'disclosure'}
-    __tablename__ = 'ofec_sched_b_aggregate_recipient_id'
+    __tablename__ = 'dsc_sched_b_aggregate_recipient_id'
     recipient_id = db.Column('recipient_cmte_id', db.String, primary_key=True, doc=docs.RECIPIENT_ID)
     committee = utils.related_committee('committee_id')
     recipient = utils.related('CommitteeHistory', 'recipient_id', 'committee_id', cycle_label='cycle')
@@ -70,7 +70,7 @@ class ScheduleBByRecipientID(BaseAggregate):
 
 class ScheduleBByPurpose(BaseAggregate):
     __table_args__ = {'schema' : 'disclosure'}
-    __tablename__ = 'ofec_sched_b_aggregate_purpose'
+    __tablename__ = 'dsc_sched_b_aggregate_purpose'
     purpose = db.Column(db.String, primary_key=True, doc=docs.PURPOSE)
 
 
