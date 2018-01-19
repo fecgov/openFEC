e6e9110d21c4c337e076e35bc6f63858aa8a506f
Laura Beaufort <31420082+lbeaufort@users.noreply.github.com>
2018-01-19 16:48:45 -0500

    Update tests to work with new view, add factory

--- tests/factories.py
+++ tests/factories.py
@@ -336,6 +336,13 @@ class ElectionResultFactory(BaseFactory):
     fec_election_yr = 2016
 
 
+class ElectionsListFactory(BaseFactory):
+    class Meta:
+        model = models.ElectionsList
+    cycle = 2012
+    candidate_status = 'I'
+
+
 class CommunicationCostFactory(BaseFactory):
     class Meta:
         model = models.CommunicationCost
