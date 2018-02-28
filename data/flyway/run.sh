7ee92db17fbe221ee0394894d8e1596d444e37d4
Jean <jjeng@fec.gov>
2018-02-28 12:14:13 -0500

    change info to migrate

--- data/flyway/run.sh
+++ data/flyway/run.sh
@@ -1,2 +1,2 @@
 JDBC_URL=`echo $VCAP_SERVICES | jq -r '.["user-provided"][0]["credentials"]["JDBC_URL"]'`
-/home/vcap/app/flyway/bin/flyway -n -locations=filesystem:/home/vcap/app/flyway/migrations -url="${JDBC_URL}" info && echo SUCCESS
+/home/vcap/app/flyway/bin/flyway -n -locations=filesystem:/home/vcap/app/flyway/migrations -url="${JDBC_URL}" migrate && echo SUCCESS
