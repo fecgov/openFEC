2ecc83c2171d844cab77b9a1c04ead2ae8656d81
Laura Beaufort <31420082+lbeaufort@users.noreply.github.com>
2018-03-19 11:05:38 -0400

    Use CREATE OR REPLACE VIEW instead of tmp

--- data/migrations/V0067__filter_mur_view.sql
+++ data/migrations/V0067__filter_mur_view.sql
@@ -7,26 +7,17 @@ Filter existing view cases_with_parsed_case_serial_numbers
 
 SET search_path = fecmur, pg_catalog;
 
-DROP VIEW IF EXISTS cases_with_parsed_case_serial_numbers_tmp;
+CREATE OR REPLACE VIEW cases_with_parsed_case_serial_numbers AS
+    SELECT "case".case_id,
+        "case".case_no,
+        (regexp_replace(("case".case_no)::text, '(\d+).*'::text, '\1'::text))::integer AS case_serial,
+        "case".name,
+        "case".case_type,
+        "case".pg_date
+    FROM "case"
+    WHERE case_type = 'MUR';
 
-CREATE VIEW cases_with_parsed_case_serial_numbers_tmp AS
-
-SELECT "case".case_id,
-    "case".case_no,
-    (regexp_replace(("case".case_no)::text, '(\d+).*'::text, '\1'::text))::integer AS case_serial,
-    "case".name,
-    "case".case_type,
-    "case".pg_date
-FROM "case"
-WHERE case_type = 'MUR';
-
-ALTER TABLE cases_with_parsed_case_serial_numbers_tmp OWNER TO fec;
-GRANT ALL ON TABLE cases_with_parsed_case_serial_numbers_tmp TO fec;
-GRANT SELECT ON TABLE cases_with_parsed_case_serial_numbers_tmp TO fec_read;
-GRANT SELECT ON TABLE cases_with_parsed_case_serial_numbers_tmp TO openfec_read;
-
---------------------------------------------------------
-
-DROP VIEW IF EXISTS cases_with_parsed_case_serial_numbers;
-
-ALTER VIEW IF EXISTS cases_with_parsed_case_serial_numbers_tmp RENAME TO cases_with_parsed_case_serial_numbers;
+ALTER TABLE cases_with_parsed_case_serial_numbers OWNER TO fec;
+GRANT ALL ON TABLE cases_with_parsed_case_serial_numbers TO fec;
+GRANT SELECT ON TABLE cases_with_parsed_case_serial_numbers TO fec_read;
+GRANT SELECT ON TABLE cases_with_parsed_case_serial_numbers TO openfec_read;
