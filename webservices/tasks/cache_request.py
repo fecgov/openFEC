476aa4a9b357b2e7965ab50676c77dc15aa5be83
pkfec <pkasireddy@fec.gov>
2018-03-23 10:01:06 -0400

    Removed the duplicate scheduler task for cache calls.

--- webservices/tasks/cache_request.py
+++ webservices/tasks/cache_request.py
@@ -28,23 +28,13 @@ def cache_all_requests(json_data, formatted_url):
     except Exception as e:
         logger.error('Exception occured while uploading the cache request to S3.%s', e)
 
-def delete_cache_calls_from_s3():
+def delete_cached_calls_from_s3():
     """
     Deletes all files and folders under cached-calls from S3
     """
     bucket = utils.get_bucket()
     for obj in bucket.objects.filter(Prefix="cached-calls/"):
         obj.delete()
-    slack_message = 'Successfully deleted the cached-calls folder in {0} from S3'.format(env.get_credential('NEW_RELIC_APP_NAME'))
-    web_utils.post_to_slack(slack_message, '#bots')
-    logger.info(slack_message)
-
-@app.task
-def delete_cache_calls_folder():
-    permanent_dir = ('cached-calls')
-    for obj in utils.get_bucket().objects.all():
-        if obj.key.startswith(permanent_dir):
-            obj.delete()
     slack_message = 'Successfully deleted the cached-calls folder in {0} from S3'.format(env.get_credential('NEW_RELIC_APP_NAME'))
     web_utils.post_to_slack(slack_message, '#bots')
     logger.info(slack_message)
