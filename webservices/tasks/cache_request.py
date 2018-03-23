b796eb0a721efdbd5c13a20e3b3bb5e09901f9f8
Carlo Costino <carlo.costino@gsa.gov>
2018-03-23 17:13:19 -0400

    Fixed connectivity issue to S3 and added more logging

--- webservices/tasks/cache_request.py
+++ webservices/tasks/cache_request.py
@@ -17,7 +17,7 @@ def cache_all_requests(json_data, formatted_url):
         s3_key = utils.get_s3_key(cached_url)
 
         # upload the json_data to s3 bucket
-        with smart_open(s3_key, 'wb') as cached_file:
+        with smart_open(cached_url, 'wb') as cached_file:
             cached_file.write(json_data)
         logger.info(
             'The following request has been uploaded to S3 successfully: {}'.format(
@@ -33,11 +33,15 @@ def cache_all_requests(json_data, formatted_url):
 @app.task
 def delete_cached_calls_from_s3():
     """
-    Deletes all files and folders under cached-calls from S3
+    Deletes all files and folders under the cached-calls folder from S3.
     """
     bucket = utils.get_bucket()
-    for obj in bucket.objects.filter(Prefix="cached-calls/"):
+
+    for obj in bucket.objects.filter(Prefix='cached-calls/'):
         obj.delete()
-    slack_message = 'Successfully deleted the cached-calls folder in {0} from S3'.format(env.get_credential('NEW_RELIC_APP_NAME'))
+
+    slack_message = 'Successfully deleted the contents of the `cached-calls` folder in {0} from S3'.format(
+        env.space
+    )
     web_utils.post_to_slack(slack_message, '#bots')
     logger.info(slack_message)
