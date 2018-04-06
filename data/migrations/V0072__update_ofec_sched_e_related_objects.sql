a9c4169378fea5478f37fedb0aa8a619e117b60e
Jean <jjeng@fec.gov>
2018-04-06 16:12:17 -0400

    update ofec_sched_e table and cleanup unused objects for sched_e queue process

--- data/migrations/V0072__update_ofec_sched_e_related_objects.sql
+++ data/migrations/V0072__update_ofec_sched_e_related_objects.sql
@@ -4,6 +4,7 @@ alter table public.ofec_sched_e alter column schedule_type set data type varchar
 -- table ofec_sched_e is not populated by queue process, therefore queue process related tables/functions/columns are dropped
 alter table public.ofec_sched_e drop column "timestamp";
 
+drop TRIGGER nml_form_24_trigger on disclosure.nml_sched_e;
 drop FUNCTION public.ofec_sched_e_f57_notice_update(); 
 drop FUNCTION public.ofec_sched_e_notice_update_from_f24();
 drop FUNCTION public.ofec_sched_e_update();
@@ -12,15 +13,12 @@ drop FUNCTION public.ofec_sched_e_update_from_f57();
 drop FUNCTION public.ofec_sched_e_update_fulltext();
 drop FUNCTION public.ofec_sched_e_update_notice_queues();
 
-drop TRIGGER public.nml_form_24_trigger;
-drop PROCEDURE public.ofec_sched_e_update_notice_queues();
-
 drop table public.fec_vsum_f57_queue_new;
 drop table public.fec_vsum_f57_queue_old;
 drop table public.ofec_sched_e_queue_new;
 drop table public.ofec_sched_e_queue_old;
 -- 
-/Users/jjeng/Projects/openFEC/data/sample_db.sql
+
 
 
 
