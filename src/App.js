c890c2eedcfb2a04f6ab2ce4187249277e5b572b
Andrew Burnes <andrew.burnes@gsa.gov>
2018-05-30 12:40:05 -0300

    Switch to swagger-ui-dist to fix required field bug

--- src/App.js
+++ src/App.js
@@ -1,53 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import SwaggerUI from 'swagger-ui';
-import 'swagger-ui/dist/swagger-ui.css';
+import { SwaggerUIBundle } from 'swagger-ui-dist';
+import 'swagger-ui-dist/swagger-ui.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import './static/css/styles.css';
 import './static/css/custom.css';
 
-const AugmentingLayout = (props) => {
-  const BaseLayout = props.getComponent('BaseLayout', true);
-
-  return (
-    <div>
-      <Header />
-      <BaseLayout />
-      <Footer />
-    </div>
-  );
-};
-
-AugmentingLayout.defaultProps = {
-  getComponent: () => {},
-};
-
-AugmentingLayout.propTypes = {
-  getComponent: PropTypes.func,
-};
-
-const AugmentingLayoutPlugin = () => ({
-  components: {
-    AugmentingLayout,
-  },
-});
-
 class App extends React.Component {
   componentDidMount() {
-    SwaggerUI({
+    SwaggerUIBundle({
       dom_id: '#swagger-container',
       url: this.props.url,
-      presets: [SwaggerUI.presets.apis],
+      presets: [SwaggerUIBundle.presets.apis],
       deepLinking: true,
       docExpansion: 'none',
-      plugins: [AugmentingLayoutPlugin],
-      layout: 'AugmentingLayout',
     });
   }
 
   render() {
-    return <div id="swagger-container" />;
+    return (
+      <div>
+        <Header />
+        <div id="swagger-container" />
+        <Footer />
+      </div>
+    );
   }
 }
 
