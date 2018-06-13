b98138d3c30c10d4d3e704bd408926dc70313384
Priyanka Kasireddy <pkasireddy@fec.gov>
2018-06-13 18:38:35 -0400

    Add a python unit test which returns count of records with status_num=1
    Added new lines in the models file(from code review)

--- tests/test_operation_logs.py
+++ tests/test_operation_logs.py
@@ -8,16 +8,24 @@ class TestOperationsLog(ApiBaseTest):
 
     def setUp(self):
         super().setUp()
-        factories.OperationsLogFactory(candidate_committee_id='00', report_year=2000),
-        factories.OperationsLogFactory(candidate_committee_id='01', report_year=2012),
-        factories.OperationsLogFactory(candidate_committee_id='02', report_year=2014),
+        factories.OperationsLogFactory(candidate_committee_id='00', report_year=2000, status_num=1),
+        factories.OperationsLogFactory(candidate_committee_id='01', report_year=2012, status_num=0),
+        factories.OperationsLogFactory(candidate_committee_id='02', report_year=2014, status_num=1),
+        factories.OperationsLogFactory(candidate_committee_id='03', report_year=2016, status_num=0),
+        factories.OperationsLogFactory(candidate_committee_id='04', report_year=2018, status_num=1),
+        factories.OperationsLogFactory(candidate_committee_id='05', report_year=2020, status_num=1),
 
     def test_empty_query(self):
 
-        results = self._results(api.url_for(OperationsLogView, candidate_committee_id='04', report_year=2020))
+        results = self._results(api.url_for(OperationsLogView, candidate_committee_id='10', report_year=2030))
         self.assertEqual(len(results), 0)
 
     def test_search_cand_cmte_id(self):
 
-        response = self.app.get(api.url_for(OperationsLogView, candidate_committee_id='01', report_year=2000))
+        response = self.app.get(api.url_for(OperationsLogView, candidate_committee_id='01', report_year=2012))
         self.assertEquals(response.status_code, 200)
+
+    def test_search_cand_cmte_id_with_status_verified(self):
+
+        results = self._results(api.url_for(OperationsLogView, status_num=1))
+        self.assertEqual(len(results), 4)
--- webservices/common/models/operations_log.py
+++ webservices/common/models/operations_log.py
@@ -7,20 +7,26 @@ class OperationsLog(db.Model):
 
     sub_id = db.Column(db.Integer, primary_key=True, doc=docs.SUB_ID)
     status_num = db.Column(db.Integer, doc=docs.STATUS_NUM)
-    candidate_committee_id = db.Column('cand_cmte_id', db.String, doc=docs.CAND_CMTE_ID)
-    beginning_image_number = db.Column('beg_image_num', db.String, doc=docs.BEGINNING_IMAGE_NUMBER)
-    ending_image_number = db.Column('end_image_num', db.String, doc=docs.ENDING_IMAGE_NUMBER)
     form_type = db.Column('form_tp', db.String, doc=docs.FORM_TYPE)
     report_year = db.Column('rpt_yr', db.Integer, doc=docs.REPORT_YEAR)
-    receipt_date = db.Column('receipt_dt', db.DateTime, doc=docs.RECEIPT_DATE)
-    coverage_start_date = db.Column('beginning_coverage_dt', db.DateTime, doc=docs.COVERAGE_START_DATE)
-    coverage_end_date = db.Column('ending_coverage_dt', db.DateTime, doc=docs.COVERAGE_END_DATE)
+    candidate_committee_id = db.Column('cand_cmte_id', db.String,
+            doc=docs.CAND_CMTE_ID)
+    beginning_image_number = db.Column('beg_image_num', db.String,
+            doc=docs.BEGINNING_IMAGE_NUMBER)
+    ending_image_number = db.Column('end_image_num', db.String,
+            doc=docs.ENDING_IMAGE_NUMBER)
+    receipt_date = db.Column('receipt_dt', db.DateTime,
+            doc=docs.RECEIPT_DATE)
+    coverage_start_date = db.Column('beginning_coverage_dt', db.DateTime,
+            doc=docs.COVERAGE_START_DATE)
+    coverage_end_date = db.Column('ending_coverage_dt', db.DateTime,
+            doc=docs.COVERAGE_END_DATE)
     amendment_indicator = db.Column('amndt_ind', db.String,
             doc="Type of the report.N(new), A(amended) or T(cancel)")
     report_type = db.Column('rpt_tp', db.String,
             doc="Monthly, quarterly or other period covered reports")
     summary_data_complete_date = db.Column('pass_1_entry_dt', db.DateTime,
-            doc="Date when the report is loaded in the database")
+            doc="Date when the report is entered into the database")
     summary_data_verification_date = db.Column('pass_1_verified_dt', db.DateTime,
             doc="Same day or a day after the report is loaded in the database")
     transaction_data_complete_date = db.Column('pass_3_entry_done_dt', db.Date,
