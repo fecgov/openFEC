f6fcd12c2309e86975fc28fd0f7a404cfd87a6a9
Rohan Jayasekera <rjayasekera@fec.gov>
2018-06-19 09:33:13 -0400

    Modify schedule and create two separate tasks for daily and Sunday complete AO load Modify method names to be more descriptive names

--- webservices/tasks/legal_docs.py
+++ webservices/tasks/legal_docs.py
@@ -5,8 +5,10 @@ import datetime
 
 from celery_once import QueueOnce
 
+from webservices import utils
 from webservices.tasks import app
 from webservices.rest import db
+from webservices.env import env
 from webservices.legal_docs.advisory_opinions import load_advisory_opinions
 from webservices.legal_docs.current_murs import load_current_murs
 
@@ -42,27 +44,31 @@ def refresh():
         refresh_murs(conn)
 
 @app.task(once={'graceful': True}, base=QueueOnce)
-def reload_all_aos():
+def reload_all_aos_when_change():
     """
-    Reload all AOs daily (except Sunday) if there were any new or modified AOs found for the past 24 hour period
-    But on Sundays reload all AOs whether there were changes to AOs or not
+    Reload all AOs if there were any new or modified AOs found for the past 24 hour period
     """
-    if (datetime.date.today().strftime("%A") == 'Sunday'):
-        logger.info("Weekly (%s) reload of all AOs starting", datetime.date.today().strftime("%A"))
-        load_advisory_opinions()
-        logger.info("Weekly (%s) reload of all AOs completed", datetime.date.today().strftime("%A"))
-        slack_message = 'Weekly reload of all AOs completed in {0} space'.format(env.space)
-        web_utils.post_to_slack(slack_message, '#bots')
-    else:
-        with db.engine.connect() as conn:
-            row = conn.execute(DAILY_MODIFIED_STARTING_AO).first()
-            if row:
-                logger.info("Daily (%s) reload of all AOs starting", datetime.date.today().strftime("%A"))
-                load_advisory_opinions()
-                logger.info("Daily (%s) reload of all AOs completed", datetime.date.today().strftime("%A"))
-            else:
-                logger.info("No daily (%s) modified AOs found", datetime.date.today().strftime("%A"))  
+    with db.engine.connect() as conn:
+        row = conn.execute(DAILY_MODIFIED_STARTING_AO).first()
+        if row:
+            logger.info("AO found %s modified at %s", row["ao_no"], row["pg_date"])
+            logger.info("Daily (%s) reload of all AOs starting", datetime.date.today().strftime("%A"))
+            load_advisory_opinions()
+            logger.info("Daily (%s) reload of all AOs completed", datetime.date.today().strftime("%A"))
+            slack_message = 'Daily reload of all AOs completed in {0} space'.format(env.get_credential('NEW_RELIC_APP_NAME'))
+            utils.post_to_slack(slack_message, '#bots')
+        else:
+            logger.info("No daily (%s) modified AOs found", datetime.date.today().strftime("%A")) 
+            slack_message = 'No modified AOs found for the day - Reload of all AOs skipped in {0} space'.format(env.get_credential('NEW_RELIC_APP_NAME'))
+            utils.post_to_slack(slack_message, '#bots') 
 
+@app.task(once={'graceful': True}, base=QueueOnce)
+def reload_all_aos():
+    logger.info("Weekly (%s) reload of all AOs starting", datetime.date.today().strftime("%A"))
+    load_advisory_opinions()
+    logger.info("Weekly (%s) reload of all AOs completed", datetime.date.today().strftime("%A"))
+    slack_message = 'Weekly reload of all AOs completed in {0} space'.format(env.get_credential('NEW_RELIC_APP_NAME'))
+    utils.post_to_slack(slack_message, '#bots')
 
 def refresh_aos(conn):
     row = conn.execute(RECENTLY_MODIFIED_STARTING_AO).first()
