d1d408c53eaa845274d6212521877ea42602cb67
hhe <hcao@fec.gov>
2018-06-28 16:07:18 -0400

    add ofec_agg_coverage_date_mv; add entity_chart_vw; modify endpoint to use entity_chart_vw

--- webservices/common/models/large_aggregates.py
+++ webservices/common/models/large_aggregates.py
@@ -8,13 +8,13 @@ from webservices import docs
 
 class EntityReceiptDisbursementTotals(db.Model):
     """two year receipt and disbursement totals by entity type and two-year election period"""
-    __tablename__ = 'ofec_entity_chart_mv'
+    __tablename__ = 'entity_chart_vw'
 
     idx = db.Column(db.Integer, primary_key=True)
     cycle = db.Column(db.Integer, doc=docs.RECORD_CYCLE)
     month = db.Column(db.Integer, doc="Numeric representation of year")
     year = db.Column(db.Integer, doc="Numeric representation of month")
-    date = db.Column(db.DateTime, doc="Date representation of the month and year")
+    end_date = db.Column(db.Date, doc="Date representation of the month and year")
     cumulative_candidate_receipts = db.Column(db.Float, doc="Cumulative candidate receipts in a two year period, adjusted to avoid double counting.")
     cumulative_candidate_disbursements = db.Column(db.Float, doc="Cumulative candidate disbursements in a two year period, adjusted to avoid double counting.")
     cumulative_pac_receipts = db.Column(db.Float, doc="Cumulative PAC recipts in a two year period, adjusted to avoid double counting.")
