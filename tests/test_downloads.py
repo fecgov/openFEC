139cebef9f8c1c48ddfa2cd93b7d8d4806c8f691
Jun Li <jli@fec.gov>
2018-07-20 10:43:48 -0400

    Add 'elections/search' endpoint into test_download_forbidden function. 'elections/search' (resource.ElectionListView) don't support download function for now.

--- tests/test_downloads.py
+++ tests/test_downloads.py
@@ -143,8 +143,8 @@ class TestDownloadResource(ApiBaseTest):
         assert not export.delay.called
 
     def test_download_forbidden(self):
-        response = self.app.get(api.url_for(resource.DownloadView, path='elections', office='house', cycle=2018))
-        self.assertEqual(response.status_code, 405)
+        with pytest.raises(ApiError):
+            self.client.post_json(api.url_for(resource.DownloadView, path='elections/search', office='house', state='VA', cycle=2018))
 
     @mock.patch('webservices.resources.download.MAX_RECORDS', 2)
     @mock.patch('webservices.resources.download.get_cached_file')
