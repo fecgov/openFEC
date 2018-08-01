56ff59b899b41506aaec5b3663c5ab7210da0235
Jean <jjeng@fec.gov>
2018-07-31 20:28:52 -0400

    update column names in ofec_agg_coverage_date_mv; update logic to handle amendment sitiatiou and data prior to 2003

--- data/migrations/V0098__update_ofec_agg_coverage_date_mv_logic.sql
+++ data/migrations/V0098__update_ofec_agg_coverage_date_mv_logic.sql
@@ -1,24 +1,44 @@
 -- update ofec_agg_coverage_date_mv logic to take amendment situation into consideration
+-- don’t look at operations log before 2003 (no consistent data)  
 DROP MATERIALIZED VIEW IF EXISTS public.ofec_agg_coverage_date_mv_tmp;
 
 CREATE MATERIALIZED VIEW IF NOT EXISTS public.ofec_agg_coverage_date_mv_tmp AS
 WITH OPERATIONS_LOG AS
 (
-SELECT sub_id, max(pass_3_entry_done_dt) over (partition by cand_cmte_id, rpt_yr, rpt_tp) max_pass_3_dt
-FROM staging.operations_log
-)
-SELECT row_number() OVER () AS idx,
-f.cmte_id AS cand_cmte_id,
-f.rpt_yr + f.rpt_yr % 2::numeric AS fec_election_yr,
-max(f.cvg_end_dt)::text::date AS coverage_end_date
-FROM disclosure.v_sum_and_det_sum_report f
-WHERE f.orig_sub_id IN
+   SELECT sub_id, max(pass_3_entry_done_dt) over (partition by cand_cmte_id, rpt_yr, rpt_tp) max_pass_3_dt
+   FROM staging.operations_log
+),
+AGG_CVG_DT AS
 (
-SELECT SUB_ID FROM OPERATIONS_LOG 
-WHERE max_pass_3_dt IS NOT NULL
+SELECT 
+f.cmte_id AS committee_id,
+f.rpt_yr + f.rpt_yr % 2::numeric AS fec_election_yr,
+max(f.cvg_end_dt)::text::timestamp without time zone AS transaction_coverage_date
+FROM disclosure.v_sum_and_det_sum_report f
+WHERE 
+( f.orig_sub_id IN
+  (
+    SELECT SUB_ID FROM OPERATIONS_LOG 
+    WHERE max_pass_3_dt IS NOT NULL
+  ) 
 )
 AND f.form_tp_cd::text ~~ 'F3%'::text
 GROUP BY f.cmte_id, (f.rpt_yr + f.rpt_yr % 2::numeric)
+UNION
+SELECT 
+f.cmte_id AS committee_id,
+f.rpt_yr + f.rpt_yr % 2::numeric AS fec_election_yr,
+max(f.cvg_end_dt)::text::timestamp without time zone AS transaction_coverage_date
+FROM disclosure.v_sum_and_det_sum_report f
+WHERE f.cvg_end_dt < '20030101'
+AND f.form_tp_cd::text ~~ 'F3%'::text
+GROUP BY f.cmte_id, (f.rpt_yr + f.rpt_yr % 2::numeric)
+)
+SELECT row_number() OVER () AS idx,
+AGG_CVG_DT.COMMITTEE_ID,
+AGG_CVG_DT.FEC_ELECTION_YR,
+AGG_CVG_DT.transaction_coverage_date
+FROM AGG_CVG_DT
 WITH DATA;
 
  
@@ -31,7 +51,7 @@ GRANT SELECT ON TABLE public.ofec_agg_coverage_date_mv_tmp TO fec_read;
 CREATE INDEX IF NOT EXISTS idx_ofec_agg_cvg_dt_mv_cmte_id_fec_elec_yr_tmp 
 ON public.ofec_agg_coverage_date_mv_tmp
   USING btree
-  (cand_cmte_id COLLATE pg_catalog."default", fec_election_yr);
+  (committee_id COLLATE pg_catalog."default", fec_election_yr);
 
 CREATE UNIQUE INDEX IF NOT EXISTS idx_ofec_agg_cvg_dt_mv_idx_tmp 
 ON public.ofec_agg_coverage_date_mv_tmp
