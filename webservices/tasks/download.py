a80824b1afccf92dd177b2c9bb74ed98d33b641d
Vraj Mohan <radhakrishnan.vrajmohan@gsa.gov>
2018-08-21 10:16:38 -0700

    Remove stale code
    This call is no longer needed as we no longer store anything under the
    name cached-calls.

--- webservices/tasks/download.py
+++ webservices/tasks/download.py
@@ -145,7 +145,7 @@ def export_query(path, qs):
 
 @app.task
 def clear_bucket():
-    permanent_dir = ('legal', 'bulk-downloads', 'cached-calls')
+    permanent_dir = ('legal', 'bulk-downloads')
     for obj in task_utils.get_bucket().objects.all():
         if not obj.key.startswith(permanent_dir):
             obj.delete()
