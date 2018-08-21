79551541ef5472cfb508ada0596f16f998bb857a
Vraj Mohan <radhakrishnan.vrajmohan@gsa.gov>
2018-08-21 10:35:27 -0700

    Extract Swagger tests into separate module
    They are not integration tests and need not belong there.

---
+++ tests/test_swagger.py
@@ -0,0 +1,29 @@
+import unittest
+
+from apispec import utils, exceptions
+from webservices.spec import spec, format_docstring
+
+class TestSwagger(unittest.TestCase):
+
+    def test_swagger_valid(self):
+        try:
+            utils.validate_swagger(spec)
+        except exceptions.SwaggerError as error:
+            self.fail(str(error))
+
+    def test_format_docstring(self):
+        DOCSTRING = '''
+        a
+        b
+
+        c
+        '''
+
+        after_format = format_docstring(DOCSTRING)
+        expected_format = 'a b \n\n c'
+        self.assertEqual(after_format, expected_format)
+
+    def test_format_not_docstring(self):
+        after_format = format_docstring(None)
+        expected_format = ''
+        self.assertEqual(after_format, expected_format)
