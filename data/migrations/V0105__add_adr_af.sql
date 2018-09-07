c95e4b3f00645122667a5daf4e7324e7ec34ff5b
Laura Beaufort <31420082+lbeaufort@users.noreply.github.com>
2018-09-07 17:19:42 -0400

    Add `_vw` suffix to view

--- data/migrations/V0105__add_adr_af.sql
+++ data/migrations/V0105__add_adr_af.sql
@@ -3,11 +3,9 @@ Issue #3359
 Remove case_type = 'MUR' so we can include ADR and AF
 */
 
---TODO: Does schema fecmur still make sense here?
-
 SET search_path = fecmur, pg_catalog;
 
-CREATE OR REPLACE VIEW cases_with_parsed_case_serial_numbers AS
+CREATE OR REPLACE VIEW cases_with_parsed_case_serial_numbers_vw AS
     SELECT "case".case_id,
         "case".case_no,
         (regexp_replace(("case".case_no)::text, '(\d+).*'::text, '\1'::text))::integer AS case_serial,
@@ -17,7 +15,9 @@ CREATE OR REPLACE VIEW cases_with_parsed_case_serial_numbers AS
     FROM "case"
     WHERE case_type IN ('MUR', 'AF', 'ADR');
 
-ALTER TABLE cases_with_parsed_case_serial_numbers OWNER TO fec;
-GRANT ALL ON TABLE cases_with_parsed_case_serial_numbers TO fec;
-GRANT SELECT ON TABLE cases_with_parsed_case_serial_numbers TO fec_read;
-GRANT SELECT ON TABLE cases_with_parsed_case_serial_numbers TO openfec_read;
+ALTER TABLE cases_with_parsed_case_serial_numbers_vw OWNER TO fec;
+GRANT ALL ON TABLE cases_with_parsed_case_serial_numbers_vw TO fec;
+GRANT SELECT ON TABLE cases_with_parsed_case_serial_numbers_vw TO fec_read;
+GRANT SELECT ON TABLE cases_with_parsed_case_serial_numbers_vw TO openfec_read;
+
+DROP VIEW cases_with_parsed_case_serial_numbers;
