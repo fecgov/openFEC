9c768057da1b5062edd6ab0408d396b4a94860e1
Vraj Mohan <radhakrishnan.vrajmohan@gsa.gov>
2018-09-07 18:59:30 -0700

    Use MigratedDBTestCase in advisory opinion tests

---
+++ data/load_base_advisory_opinion_data.sql
@@ -0,0 +1,30 @@
+--
+-- Data for Name: aouser.entity_type; Type: TABLE DATA; Schema: aouser; Owner: fec
+--
+
+INSERT INTO aouser.entity_type VALUES (0, NULL, NULL, '2017-01-04 17:18:57.682');
+INSERT INTO aouser.entity_type VALUES (1, 'Federal candidate/candidate committee/officeholder', NULL, '2017-01-04 17:18:57.691');
+INSERT INTO aouser.entity_type VALUES (2, 'Publicly funded candidates/committees', NULL, '2017-01-04 17:18:57.698');
+INSERT INTO aouser.entity_type VALUES (3, 'Party committee, national', NULL, '2017-01-04 17:18:57.705');
+INSERT INTO aouser.entity_type VALUES (4, 'Party committee, state or local', NULL, '2017-01-04 17:18:57.712');
+INSERT INTO aouser.entity_type VALUES (5, 'Nonconnected political committee', NULL, '2017-01-04 17:18:57.72');
+INSERT INTO aouser.entity_type VALUES (6, 'Separate segregated fund', NULL, '2017-01-04 17:18:57.729');
+INSERT INTO aouser.entity_type VALUES (7, 'Labor Organization', NULL, '2017-01-04 17:18:57.736');
+INSERT INTO aouser.entity_type VALUES (8, 'Trade Association', NULL, '2017-01-04 17:18:57.742');
+INSERT INTO aouser.entity_type VALUES (9, 'Membership Organization, Cooperative, Corporation W/O Capital Stock', NULL, '2017-01-04 17:18:57.75');
+INSERT INTO aouser.entity_type VALUES (10, 'Corporation (including LLCs electing corporate status)', NULL, '2017-01-04 17:18:57.759');
+INSERT INTO aouser.entity_type VALUES (11, 'Partnership (including LLCs electing partnership status)', NULL, '2017-01-04 17:18:57.766');
+INSERT INTO aouser.entity_type VALUES (12, 'Governmental entity', NULL, '2017-01-04 17:18:57.773');
+INSERT INTO aouser.entity_type VALUES (13, 'Research/Public Interest/Educational Institution', NULL, '2017-01-04 17:18:57.78');
+INSERT INTO aouser.entity_type VALUES (14, 'Law Firm', NULL, '2017-01-04 17:18:57.786');
+INSERT INTO aouser.entity_type VALUES (15, 'Individual', NULL, '2017-01-04 17:18:57.793');
+INSERT INTO aouser.entity_type VALUES (16, 'Other', NULL, '2017-01-04 17:18:57.8');
+
+--
+-- Data for Name: aouser.role; Type: TABLE DATA; Schema: aouser; Owner: fec
+--
+
+INSERT INTO aouser.role VALUES (0, 'Requestor', '2017-03-14 09:52:50.646026');
+INSERT INTO aouser.role VALUES (1, 'Requestor', '2017-03-14 09:52:50.646026');
+INSERT INTO aouser.role VALUES (2, 'Commenter', '2017-03-14 09:52:50.646026');
+INSERT INTO aouser.role VALUES (3, 'Counsel/Representative', '2017-03-14 09:52:50.646026');
