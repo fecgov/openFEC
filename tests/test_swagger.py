bec820122c293fda6b3389725bb77aaf30115c08
Vraj Mohan <radhakrishnan.vrajmohan@gsa.gov>
2018-09-17 19:32:20 -0700

    Upgrade to apispec 1.0.0b2

--- tests/test_swagger.py
+++ tests/test_swagger.py
@@ -10,7 +10,7 @@ class TestSwagger(unittest.TestCase):
 
     def test_swagger_valid(self):
         try:
-            utils.validate_swagger(spec)
+            utils.validate_spec(spec)
         except exceptions.SwaggerError as error:
             self.fail(str(error))
 
