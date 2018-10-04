1b4719902cecd41695d775edb13b8fc2e7e3a178
Patcharee Phongsvirajati <pphongsvirajati@fec.gov>
2018-10-04 12:14:08 -0400

    Modified stage task to deploy test branch

--- tasks.py
+++ tasks.py
@@ -141,7 +141,8 @@ def _detect_space(repo, branch=None, yes=False):
 
 DEPLOY_RULES = (
     ('prod', _detect_prod),
-    ('stage', lambda _, branch: branch.startswith('release')),
+    ('stage', lambda _, branch: branch == 'test/test-develop-on-stage'),
+    #('stage', lambda _, branch: branch.startswith('release')),
     ('dev', lambda _, branch: branch == 'develop'),
 )
 
