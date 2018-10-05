2c7f44d31dc2cea2f0847315bf8ec16a37235c6f
Laura Beaufort <31420082+lbeaufort@users.noreply.github.com>
2018-10-05 12:12:28 -0400

    Set count estimate threshold to 500K for all endpoints

--- webservices/common/views.py
+++ webservices/common/views.py
@@ -73,7 +73,7 @@ class ItemizedResource(ApiResource):
             query, count = self.join_committee_queries(kwargs)
             return utils.fetch_seek_page(query, kwargs, self.index_column, count=count)
         query = self.build_query(**kwargs)
-        count = counts.count_estimate(query, models.db.session, threshold=5000)
+        count = counts.count_estimate(query, models.db.session, threshold=500000)
         return utils.fetch_seek_page(query, kwargs, self.index_column, count=count, cap=self.cap)
 
     def join_committee_queries(self, kwargs):
@@ -100,5 +100,5 @@ class ItemizedResource(ApiResource):
         sort, hide_null = kwargs['sort'], kwargs['sort_hide_null']
         query, _ = sorting.sort(query, sort, model=self.model, hide_null=hide_null)
         page_query = utils.fetch_seek_page(query, kwargs, self.index_column, count=-1, eager=False).results
-        count = counts.count_estimate(query, models.db.session, threshold=5000)
+        count = counts.count_estimate(query, models.db.session, threshold=500000)
         return page_query, count
