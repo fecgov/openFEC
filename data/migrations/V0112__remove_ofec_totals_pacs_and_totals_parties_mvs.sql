efe132f3143af155eb8432e880c7077495473431
Jean <jjeng@fec.gov>
2018-10-12 11:55:38 -0400

    modify migration file V0112 to add IF EXISTS clause with DROP

--- data/migrations/V0112__remove_ofec_totals_pacs_and_totals_parties_mvs.sql
+++ data/migrations/V0112__remove_ofec_totals_pacs_and_totals_parties_mvs.sql
@@ -9,12 +9,12 @@ SET search_path = public, pg_catalog;
 -- later research reveals that the two models using these two views are not necessary.  So both views and MVs can be deleted.
 
 -- --------------------------
-DROP VIEW public.ofec_totals_pacs_vw;
+DROP VIEW IF EXISTS public.ofec_totals_pacs_vw;
 
 DROP MATERIALIZED VIEW IF EXISTS public.ofec_totals_pacs_mv;
 
 -- ----------------------------
 -- ----------------------------
-DROP VIEW public.ofec_totals_parties_vw;
+DROP VIEW IF EXISTS public.ofec_totals_parties_vw;
 
 DROP MATERIALIZED VIEW IF EXISTS public.ofec_totals_parties_mv;
