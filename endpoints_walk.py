cec8e3556ed707831b5bd17c61d9975470accc11
qqss88 <qqss88@gmail.com>
2018-10-17 11:10:04 -0400

    endpoints_walk.py: adding api_key as another user input option.

--- endpoints_walk.py
+++ endpoints_walk.py
@@ -8,15 +8,14 @@ Note: on the production tier, api_key is required.
 
 quick server list for reference:
 
-prod: https://api.open.fec.gov/v1/
-stage: https://api-stage.open.fec.gov/v1/
-dev: https://fec-dev-api.app.cloud.gov/v1/
+prod: https://api.open.fec.gov
+stage: https://api-stage.open.fec.gov
+dev: https://fec-dev-api.app.cloud.gov
 """
 import os
 import click
 import requests
 
-API_KEY = os.environ['FEC_API_KEY']
 
 COMMITTEE_ID = 'C00496067'
 CANDIDATE_ID = 'P40002172'
@@ -91,11 +90,12 @@ endpoints = [
 ]
 
 @click.command()
-@click.option('--server', default="https://fec-dev-api.app.cloud.gov/v1", help='server to check with.')
-def check_endpoints(server):
+@click.option('--server', default="https://fec-dev-api.app.cloud.gov", help='server to check with.')
+@click.option('--api_key', default=lambda: os.environ.get('FEC_API_KEY', ''), help='an api_key is needed for testing.')
+def check_endpoints(server, api_key):
     """Simple script to check all endpoints status quickly."""
     for endp in endpoints:
-        r = requests.get((server + endp).format(api_key=API_KEY,
+        r = requests.get((server + '/v1' + endp).format(api_key=api_key,
                                                 committee_id=COMMITTEE_ID,
                                                 candidate_id=CANDIDATE_ID,
                                                 committee_type=COMMITTEE_TYPE,
