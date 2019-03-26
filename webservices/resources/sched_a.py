86526024c90e8a98e49e77c2ba3ed7f01586811a
qqss88 <qqss88@gmail.com>
2019-03-25 20:31:18 -0400

    minor modification

--- webservices/resources/sched_a.py
+++ webservices/resources/sched_a.py
@@ -13,6 +13,58 @@ from webservices.common.views import ItemizedResource
 from webservices import exceptions
 
 
+# 54
+
+# This works in python 2 and 3 and is a bit cleaner than before, but requires SA>=1.0.
+
+from sqlalchemy.engine.default import DefaultDialect
+from sqlalchemy.sql.sqltypes import String, DateTime, NullType
+
+# python2/3 compatible.
+PY3 = str is not bytes
+text = str if PY3 else unicode
+int_type = int if PY3 else (int, long)
+str_type = str if PY3 else (str, unicode)
+
+
+class StringLiteral(String):
+    """Teach SA how to literalize various things."""
+    def literal_processor(self, dialect):
+        super_processor = super(StringLiteral, self).literal_processor(dialect)
+
+        def process(value):
+            if isinstance(value, int_type):
+                return text(value)
+            if not isinstance(value, str_type):
+                value = text(value)
+            result = super_processor(value)
+            if isinstance(result, bytes):
+                result = result.decode(dialect.encoding)
+            return result
+        return process
+
+class LiteralDialect(DefaultDialect):
+    colspecs = {
+        # prevent various encoding explosions
+        String: StringLiteral,
+        # teach SA about how to literalize a datetime
+        DateTime: StringLiteral,
+        # don't format py2 long integers to NULL
+        NullType: StringLiteral,
+    }
+
+
+def literalquery(statement):
+    """NOTE: This is entirely insecure. DO NOT execute the resulting strings."""
+    import sqlalchemy.orm
+    if isinstance(statement, sqlalchemy.orm.Query):
+        statement = statement.statement
+    return statement.compile(
+        dialect=LiteralDialect(),
+        compile_kwargs={'literal_binds': True},
+    ).string
+
+
 @doc(
     tags=['receipts'],
     description=docs.SCHEDULE_A,
@@ -114,6 +166,7 @@ class ScheduleAView(ItemizedResource):
         # print(str(query.statement.compile(
         #     dialect=postgresql.dialect(),
         #     compile_kwargs={"literal_binds": True})))
+        # print(literalquery(query))    
         return query
 
 @doc(
