89931ddcd7740678de60a579139397a335592f3a
qqss88 <qqss88@gmail.com>
2019-05-02 16:00:30 -0400

    updating line number related files.

--- sched_b_baseline/sched_b_walk.py
+++ sched_b_baseline/sched_b_walk.py
@@ -1,32 +1,5 @@
 """
-this is a simple scipt to walk through a list sched_a endpopints made
-for baseline performance testing
-
-those endpoints currently not loading at all:
-
->>top committee type 'W' not loading with cycle141618:
-https://api-stage.open.fec.gov/v1/schedules/schedule_a?
-sort_hide_null=false&per_page=30&is_individual=true&two_year_transaction_period
-=2016&two_year_transaction_period=2018&two_year_transaction_period=2014&
-sort=-contribution_receipt_date&sort_nulls_last=false&api_key=DEMO_KEY&
-recipient_committee_type=W
-
->>top line_num not loading with 141618:
-https://api-stage.open.fec.gov/v1/schedules/schedule_a?
-sort_hide_null=false&per_page=30&is_individual=true&
-two_year_transaction_period=2016&two_year_transaction_period=2018
-&two_year_transaction_period=2014&sort=-contribution_receipt_date
-&sort_nulls_last=false&api_key=DEMO_KEY&line_number=11AI
-
->>self_employed not loading:
-https://api-stage.open.fec.gov/v1/schedules/schedule_a?
-sort_hide_null=false&per_page=30&is_individual=true&
-two_year_transaction_period=2018&two_year_transaction_period=2014&
-sort=-contribution_receipt_date&sort_nulls_last=false&api_key=DEMO_KEY&
-contributor_employer=not employed
-
-
-
+this is a simple scipt to walk through a list sched_b endpopints
 """
 import os
 import click
