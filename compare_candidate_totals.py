75aa2204e5b63f77c9414571289d7f9553a116d7
Laura Beaufort <31420082+lbeaufort@users.noreply.github.com>
2019-05-22 10:47:51 -0400

    Refactoring

--- compare_candidate_totals.py
+++ compare_candidate_totals.py
@@ -1,71 +1,56 @@
 import os
 import requests
-import logging
+import click
 
-candidate_datatable = "/candidates/totals/?candidate_id={0}&election_year={1}&election_full={2}&api_key={3}&sort=-receipts"
-candidate_profile = "/candidate/{0}/totals/?cycle={1}&api_key={2}"
-election_profile = "/elections/?candidate_id={0}&cycle={1}&election_full={2}&api_key={3}&office={4}&sort_nulls_last=true&sort=-total_receipts&per_page=100"
-#dev_url = 'https://fec-dev-api.app.cloud.gov'
-dev_url = 'http://localhost:5000'
-prod_url = 'https://api.open.fec.gov'
 api_key = os.environ.get("FEC_API_KEY")
 
-envs = {'dev': dev_url, 'prod': prod_url}
-
-
-def compare_candidate_totals():
-
-    top_candidates = []
-    candidate_url = "https://api.open.fec.gov/v1/candidates/totals/?api_key={0}&sort_hide_null=false&sort_nulls_last=true&is_active_candidate=True&election_full=true&sort=-receipts&page=1".format(api_key)
-
-    # Top 20 Presidential 2020
-    top_candidates.extend(requests.get(candidate_url + "&election_year=2020&office=P&per_page=20").json().get('results'))
-    # Top 30 Senate 2020
-    top_candidates.extend(requests.get(candidate_url + "&election_year=2020&office=S&per_page=30").json().get('results'))
-    # Top 30 Senate 2022
-    top_candidates.extend(requests.get(candidate_url + "&election_year=2022&office=S&per_page=30").json().get('results'))
-
-    # Top 30 Senate 2024 - check after Q1 2019
-    top_candidates.extend(requests.get(candidate_url + "&election_year=2024&office=S").json().get('results'))
-
-    # Top 100 House 2020
-    top_candidates.extend(requests.get(candidate_url + "&election_year=2020&office=H&per_page=100").json().get('results'))
+@click.command()
+@click.option('--office-types', default=['H', 'S', 'P'], help='Which offices to check. Format as H,S')
+@click.option('--year', default=2020, help='Start year')
+@click.option('--candidate-id', help='Check one candidate')
+#@click.option('--envs', default=['dev', 'prod'], help='Which envs to check')
+def compare_candidate_totals(office_types, year, candidate_id, envs):
 
     mismatch_list = set([])
-    comparison_results = {}
+
+    #local_url = 'http://localhost:5000'
+    dev_url = 'https://fec-dev-api.app.cloud.gov'
+    prod_url = 'https://api.open.fec.gov'
+
+    # TODO: Ask about local/dev/prod
+    envs_to_check = {'dev': dev_url, 'prod': prod_url}
+
+    candidate_datatable = "/candidates/totals/?candidate_id={0}&election_year={1}&election_full={2}&api_key={3}&sort=-receipts"
+    candidate_profile = "/candidate/{0}/totals/?cycle={1}&api_key={2}"
+    election_profile = "/elections/?candidate_id={0}&cycle={1}&election_full={2}&api_key={3}&office={4}&sort_nulls_last=true&sort=-total_receipts&per_page=100"
 
     endpoints = ['datatable', 'candidate', 'election']
     values_to_check = ['receipts', 'disbursements', 'cash_on_hand_end_period']
 
-    ##TODO: Do this with a class and state?
-    def set_results(result_type, env, value):
-        # Clean up inconsistencies in labellings
-        # 0.00 is Falsy
-        if value.get('total_receipts') is not None:
-            value['receipts'] = value['total_receipts']
-        if value.get('total_disbursements') is not None:
-            value['disbursements'] = value['total_disbursements']
-        if value.get('last_cash_on_hand_end_period') is not None:
-            value['cash_on_hand_end_period'] = value['last_cash_on_hand_end_period']
-        comparison_results[result_type + '_' + env] = value
+    for candidate in get_top_candidates(office_types, year, candidate_id):
 
-    def get_results(result_type, env):
-        return comparison_results.get(result_type + '_' + env)
-
-    def get_printable_url(url):
-        return url.replace(api_key, 'DEMO_KEY')
-
-    for candidate in top_candidates:
-
+        result_list = []
         candidate_id = candidate.get('candidate_id')
         candidate_name = candidate.get('name')
         candidate_election = candidate.get('election_year')
 
-        for env, url in envs.items():
+        for env, url in envs_to_check.items():
 
-            datatable_url = (url + "/v1" + candidate_datatable).format(candidate_id, candidate_election, 'true', api_key)
-            candidate_profile_url = (url + "/v1" + candidate_profile).format(candidate_id, candidate_election, api_key)
-            election_profile_url = (url + "/v1" + election_profile).format(candidate_id, candidate_election, 'true', api_key, candidate.get('office_full').lower())
+            results = Results(env)
+
+            datatable_url = (url + "/v1" + candidate_datatable).format(
+                candidate_id, candidate_election, 'true', api_key
+            )
+            candidate_profile_url = (url + "/v1" + candidate_profile).format(
+                candidate_id, candidate_election, api_key
+            )
+            election_profile_url = (url + "/v1" + election_profile).format(
+                candidate_id,
+                candidate_election,
+                'true',
+                api_key,
+                candidate.get('office_full').lower(),
+            )
 
             # show full election for S, P
             if candidate.get('office') in ("S", "P"):
@@ -75,21 +60,43 @@ def compare_candidate_totals():
 
             # Add state for House/Senate and District for House
             if candidate.get('office') != "P":
-                election_profile_url += "&state={}&election_full={}".format(candidate.get('state'), 'True')
+                election_profile_url += "&state={}&election_full={}".format(
+                    candidate.get('state'), 'True'
+                )
             if candidate.get('office') == "H":
-                election_profile_url += "&district={}&election_full={}".format(candidate.get('district'), 'False')
+                election_profile_url += "&district={}&election_full={}".format(
+                    candidate.get('district'), 'False'
+                )
 
-            print("\n*********Checking: {}, {} in {} ************\n".format(candidate_id, candidate_name, env))
+            print(
+                "\n****Checking: {}, {} in {}. Election year is {}****\n".format(
+                    candidate_id, candidate_name, env, candidate_election
+                )
+            )
 
-            print("\nCandidate totals datatable url: {}".format(get_printable_url(datatable_url)))
-            print("\nCandidate profile page totals url: {}".format(get_printable_url(candidate_profile_url)))
-            print("\nElection profile page totals url: {}".format(get_printable_url(election_profile_url)))
+            print(
+                "\nCandidate totals datatable url: {}".format(
+                    get_printable_url(datatable_url)
+                )
+            )
+            print(
+                "\nCandidate profile page totals url: {}".format(
+                    get_printable_url(candidate_profile_url)
+                )
+            )
+            print(
+                "\nElection profile page totals url: {}".format(
+                    get_printable_url(election_profile_url)
+                )
+            )
 
             datatable_results = requests.get(datatable_url).json().get('results')
-            candidate_results = requests.get(candidate_profile_url).json().get('results')
+            candidate_results = (requests.get(candidate_profile_url).json().get('results'))
 
             # This is a list of all candidates - we'll need to loop through them to match
-            all_election_results = requests.get(election_profile_url).json().get('results')
+            all_election_results = (
+                requests.get(election_profile_url).json().get('results')
+            )
 
             for election_result in all_election_results:
                 if election_result.get('candidate_id') == candidate_id:
@@ -98,41 +105,137 @@ def compare_candidate_totals():
 
             if not all([datatable_results, candidate_results, election_match]):
                 print("\nERROR: No results for one endpoint")
-                print("Candidate datatable has results? {}".format(datatable_results is not None))
-                print("Candidate profile page has results? {}".format(candidate_results is not None))
-                print("Election profile page has results? {}".format(election_match is not None))
+                print(
+                    "Candidate datatable has results? {}".format(
+                        datatable_results is not None
+                    )
+                )
+                print(
+                    "Candidate profile page has results? {}".format(
+                        candidate_results is not None
+                    )
+                )
+                print(
+                    "Election profile page has results? {}".format(
+                        election_match is not None
+                    )
+                )
                 mismatch_list.add((candidate_id, candidate_name))
             else:
                 # Take the top result
-                set_results('datatable', env, datatable_results[0])
-                set_results('candidate', env, candidate_results[0])
+                results.set('datatable', datatable_results[0])
+                results.set('candidate', candidate_results[0])
                 # Take the matched results
-                set_results('election', env, election_match)
+                results.set('election', election_match)
                 for value in values_to_check:
-                    baseline = get_results('datatable', env).get(value)
+                    baseline = results.get('datatable').get(value)
                     # for endpoint in endpoints:
-                    #     print("{}: {}".format(endpoint, get_results(endpoint, env).get(value)))
-                    if any(get_results(endpoint, env).get(value) != baseline for endpoint in endpoints):
+                    #     print("{}: {}".format(endpoint, results.get(endpoint).get(value)))
+                    if any(
+                        results.get(endpoint).get(value) != baseline
+                        for endpoint in endpoints
+                    ):
                         print("\n!!! ERROR - {} results don't match!!!\n".format(value))
                         print("| Data source | Total {} |\n|--|--|".format(value))
                         for endpoint in endpoints:
-                            print("| {} datatable or profile page \t\t|\t${:,.2f}|".format(endpoint, get_results(endpoint, env).get(value)))
+                            print(
+                                "| {} datatable or profile page \t\t|\t${:,.2f}|".format(
+                                    endpoint, results.get(endpoint).get(value)
+                                )
+                            )
                         mismatch_list.add((candidate_id, candidate_name))
+
+            result_list.append(results)
 
             print("\nMismatch list ({}): {}".format(env, mismatch_list))
 
         for endpoint in endpoints:
-            print("\n*********Checking across environments: {} ************\n".format(endpoint))
+            print(
+                "\n****Checking across environments: {} ****\n".format(
+                    endpoint
+                )
+            )
             for value in values_to_check:
                 # Grab the baseline value for the first environment
-                baseline = get_results(endpoint, list(envs.keys())[0]).get(value)
+                baseline = result_list[0].get(endpoint).get(value)
+
                 # If any values differ across environment
-                if any(get_results(endpoint, env).get(value) != baseline for env in envs.keys()):
+                if any(
+                    result.get(endpoint).get(value) != baseline
+                    for result in result_list
+                ):
                     print("\n!!! ERROR - environment results don't match!!!")
                     print("| Data source | Total {} |\n|--|--|".format(value))
-                    for env in envs.keys():
-                        print("| {} {} {}\t\t|\t${:,.2f}|".format(endpoint, value, env, get_results(endpoint, env).get(value)))
+                    for result in result_list:
+                        print(
+                            "| {} {} {}\t\t|\t${:,.2f}|".format(
+                                endpoint,
+                                value,
+                                result.env,
+                                result.get(endpoint).get(value),
+                            )
+                        )
                     mismatch_list.add((candidate_id, candidate_name))
+
+
+def get_top_candidates(office_types=['P', 'S', 'H'], start_year=2020, candidate_id=None):
+
+    top_candidates = []
+    candidate_url = "https://api.open.fec.gov/v1/candidates/totals/?api_key={0}&sort_hide_null=false&sort_nulls_last=true&is_active_candidate=True&election_full=true&sort=-receipts&page=1".format(
+        api_key
+    )
+    if candidate_id:
+        candidate_url += "&candidate_id={}".format(candidate_id)
+    print("Getting candidate info")
+    if 'P' in office_types:
+        # Top 20 presidential
+        top_candidates.extend(
+            requests.get(candidate_url + "&election_year={}&office=P&per_page=20".format(start_year))
+            .json()
+            .get('results')
+        )
+    if 'S' in office_types:
+        # Top 30 senate, 3 cycles
+        # Start year plus next two elections
+        for year in range(start_year, start_year + 5, 2):
+            top_candidates.extend(
+                requests.get(candidate_url + "&election_year={}&office=S&per_page=30".format(year))
+                .json()
+                .get('results')
+            )
+    if 'H' in office_types:
+        # Top 100 house
+        top_candidates.extend(
+            requests.get(candidate_url + "&election_year={}&office=H&per_page=100".format(start_year))
+            .json()
+            .get('results')
+        )
+    return top_candidates
+
+
+class Results(object):
+    """docstring for Results"""
+    def __init__(self, env):
+        self.env = env
+        self.result_type = None
+
+    def set(self, result_type, value):
+        # Clean up inconsistencies in labellings
+        # 0.00 is Falsy
+        if value.get('total_receipts') is not None:
+            value['receipts'] = value['total_receipts']
+        if value.get('total_disbursements') is not None:
+            value['disbursements'] = value['total_disbursements']
+        if value.get('last_cash_on_hand_end_period') is not None:
+            value['cash_on_hand_end_period'] = value['last_cash_on_hand_end_period']
+        self.result_type = value
+
+    def get(self, result_type):
+        return self.result_type
+
+
+def get_printable_url(url):
+    return url.replace(api_key, 'DEMO_KEY')
 
 
 if __name__ == "__main__":
