7506c4e1d6b0e85ce4181a175145af2ff76896a9
Jason Upchurch <jupchurch@fec.gov>
2019-06-12 11:25:40 -0400

    update trigger definition for fec_fitem_sched_f_insert()

--- data/migrations/V0136__redefine_fec_fitem_sched_b_insert.sql
+++ data/migrations/V0136__redefine_fec_fitem_sched_b_insert.sql
@@ -8,7 +8,7 @@ However, since existing API referencing column two_year_transaction_period a lot
 
 DO $$
 BEGIN
-    EXECUTE format('alter table disclosure.ju_fec_fitem_sched_b rename column election_cycle to two_year_transaction_period');
+    EXECUTE format('alter table disclosure.fec_fitem_sched_b rename column election_cycle to two_year_transaction_period');
     EXCEPTION 
              WHEN undefined_column THEN 
                 null;
@@ -21,7 +21,7 @@ The calculation of value for recipient_name_text in both public.ofec_sched_b and
 It should use clean_recipient_cmte_id instead of recipient_cmte_id.
 */
 
-CREATE OR REPLACE FUNCTION disclosure.ju_fec_fitem_sched_b_insert()
+CREATE OR REPLACE FUNCTION disclosure.fec_fitem_sched_b_insert()
   RETURNS trigger AS
 $BODY$
 begin
@@ -35,7 +35,51 @@ end
 $BODY$
   LANGUAGE plpgsql VOLATILE
   COST 100;
-ALTER FUNCTION disclosure.ju_fec_fitem_sched_b_insert()
+ALTER FUNCTION disclosure.fec_fitem_sched_b_insert()
   OWNER TO fec;
 
+DROP TRIGGER IF EXISTS tri_fec_fitem_sched_b ON disclosure.fec_fitem_sched_b;
 
+CREATE TRIGGER tri_fec_fitem_sched_b
+  BEFORE INSERT
+  ON disclosure.fec_fitem_sched_b
+  FOR EACH ROW
+  EXECUTE PROCEDURE disclosure.fec_fitem_sched_b_insert();
+
+
+-- ----------------------------
+-- ----------------------------
+-- disclosure.fec_fitem_sched_f
+-- ----------------------------
+-- ----------------------------
+DO $$
+BEGIN
+    EXECUTE format('ALTER TABLE disclosure.fec_fitem_sched_f ADD COLUMN payee_name_text tsvector');
+    EXCEPTION 
+             WHEN duplicate_column THEN 
+                null;
+             WHEN others THEN 
+                RAISE NOTICE 'some other error: %, %',  sqlstate, sqlerrm;  
+END$$;
+
+CREATE OR REPLACE FUNCTION disclosure.fec_fitem_sched_f_insert()
+  RETURNS trigger AS
+$BODY$
+begin
+	new.payee_name_text := to_tsvector(regexp_replace(new.pye_nm::text, '[^a-zA-Z0-9]', ' ', 'g'));
+
+    	return new;
+end
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+ALTER FUNCTION disclosure.fec_fitem_sched_f_insert()
+  OWNER TO fec;
+
+DROP TRIGGER IF EXISTS tri_fec_fitem_sched_f ON disclosure.fec_fitem_sched_f;
+
+CREATE TRIGGER tri_fec_fitem_sched_f
+  BEFORE INSERT
+  ON disclosure.fec_fitem_sched_f
+  FOR EACH ROW
+  EXECUTE PROCEDURE disclosure.fec_fitem_sched_f_insert();
