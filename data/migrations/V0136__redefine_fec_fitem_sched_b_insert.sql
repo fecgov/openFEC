005246da14bd5de0afb7617788a64eb39ef92b60
Jason Upchurch <jupchurch@fec.gov>
2019-06-12 16:16:02 -0400

    update to_tsvector for ofec_committee_fulltext_audit_mv

--- data/migrations/V0136__redefine_fec_fitem_sched_b_insert.sql
+++ data/migrations/V0136__redefine_fec_fitem_sched_b_insert.sql
@@ -147,3 +147,45 @@ CREATE VIEW ofec_rad_analyst_vw AS
 ALTER TABLE ofec_rad_analyst_vw OWNER TO fec;
 GRANT SELECT ON TABLE ofec_rad_analyst_vw TO fec_read;
 GRANT SELECT ON TABLE ofec_rad_analyst_vw TO openfec_read;
+
+
+/*
+update to_tsvector definition for ofec_commite_fulltext_audit_mv
+*/
+DROP MATERIALIZED VIEW ofec_committee_fulltext_audit_mv;
+CREATE MATERIALIZED VIEW ofec_committee_fulltext_audit_mv AS
+WITH
+cmte_info AS (
+    SELECT DISTINCT dc.cmte_id,
+        dc.cmte_nm,
+        dc.fec_election_yr,
+        dc.cmte_dsgn,
+        dc.cmte_tp,
+        b.FILED_CMTE_TP_DESC::text AS cmte_desc
+    FROM auditsearch.audit_case aa, disclosure.cmte_valid_fec_yr dc, staging.ref_filed_cmte_tp b
+    WHERE btrim(aa.cmte_id) = dc.cmte_id 
+      AND aa.election_cycle = dc.fec_election_yr
+      AND dc.cmte_tp IN ('H', 'S', 'P', 'X', 'Y', 'Z', 'N', 'Q', 'I', 'O', 'U', 'V', 'W')
+      AND dc.cmte_tp = b.filed_cmte_tp_cd
+)
+SELECT DISTINCT ON (cmte_id, cmte_nm)
+    row_number() over () AS idx,
+    cmte_id AS id,
+    cmte_nm AS name,
+CASE
+    WHEN cmte_nm IS NOT NULL THEN
+        setweight(to_tsvector(regexp_replace(cmte_nm, '[^a-zA-Z0-9]', ' ', 'g')), 'A') ||
+        setweight(to_tsvector(regexp_replace(cmte_id, '[^a-zA-Z0-9]', ' ', 'g')), 'B')
+    ELSE NULL::tsvector
+END AS fulltxt
+FROM cmte_info 
+ORDER BY cmte_id
+WITH DATA;
+
+ALTER TABLE ofec_committee_fulltext_audit_mv OWNER TO fec;
+
+CREATE UNIQUE INDEX ON ofec_committee_fulltext_audit_mv(idx);
+CREATE INDEX ON ofec_committee_fulltext_audit_mv using gin(fulltxt);
+
+GRANT ALL ON TABLE public.ofec_committee_fulltext_audit_mv TO fec;
+GRANT SELECT ON TABLE public.ofec_committee_fulltext_audit_mv TO fec_read;
