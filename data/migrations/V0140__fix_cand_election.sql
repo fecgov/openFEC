1143ab1a3fe7bf14bf2a945ceb6c705472fe4353
Laura Beaufort <31420082+lbeaufort@users.noreply.github.com>
2019-06-14 11:17:16 -0400

    Drop unused election_duration function

--- data/migrations/V0140__fix_cand_election.sql
+++ data/migrations/V0140__fix_cand_election.sql
@@ -1,12 +1,17 @@
 /*
 This migration file is to solve part of https://github.com/fecgov/openFEC/issues/3709
 
-update `ofec_candidate_election_mv` to use `candidate_election_duration` function
+`ofec_totals_candidate_committees_mv` depends on `ofec_candidate_election_vw`
+
+1) Update `ofec_candidate_election_mv` and, by extension, `ofec_candidate_election_vw`,
+ to use new `candidate_election_duration` function
+2) Drop function `election_duration` because it's no longer being used
+
 
 */
--- ---------------
--- ofec_candidate_election_mv
--- ---------------
+
+--1) Update `ofec_candidate_election_mv` and `ofec_candidate_election_vw`
+
 DROP MATERIALIZED VIEW IF EXISTS public.ofec_candidate_election_mv_tmp;
 
 CREATE MATERIALIZED VIEW public.ofec_candidate_election_mv_tmp AS
@@ -23,11 +28,14 @@ CREATE MATERIALIZED VIEW public.ofec_candidate_election_mv_tmp AS
   ORDER BY years.candidate_id, years.cand_election_year, prev.cand_election_year DESC
 WITH DATA;
 
+--Permissions
+
 ALTER TABLE public.ofec_candidate_election_mv_tmp
   OWNER TO fec;
 GRANT ALL ON TABLE public.ofec_candidate_election_mv_tmp TO fec;
 GRANT SELECT ON TABLE public.ofec_candidate_election_mv_tmp TO fec_read;
 
+--Indices
 
 CREATE UNIQUE INDEX idx_ofec_candidate_election_mv_tmp_cand_id_cand_election_yr
   ON public.ofec_candidate_election_mv_tmp
@@ -48,14 +56,18 @@ CREATE INDEX idx_ofec_candidate_election_mv_tmp_prev_election_yr
   ON public.ofec_candidate_election_mv_tmp
   USING btree
   (prev_election_year);
+
 -- ---------------
 CREATE OR REPLACE VIEW public.ofec_candidate_election_vw AS
 SELECT * FROM public.ofec_candidate_election_mv_tmp;
 -- ---------------
+
 DROP MATERIALIZED VIEW IF EXISTS public.ofec_candidate_election_mv;
 
 ALTER MATERIALIZED VIEW IF EXISTS public.ofec_candidate_election_mv_tmp RENAME TO ofec_candidate_election_mv;
+
 -- ---------------
+
 ALTER INDEX public.idx_ofec_candidate_election_mv_tmp_cand_id_cand_election_yr RENAME TO idx_ofec_candidate_election_mv_cand_id_cand_election_yr;
 
 ALTER INDEX public.idx_ofec_candidate_election_mv_tmp_cand_election_yr RENAME TO idx_ofec_candidate_election_mv_cand_election_yr;
@@ -63,3 +75,7 @@ ALTER INDEX public.idx_ofec_candidate_election_mv_tmp_cand_election_yr RENAME TO
 ALTER INDEX public.idx_ofec_candidate_election_mv_tmp_cand_id RENAME TO idx_ofec_candidate_election_mv_cand_id;
 
 ALTER INDEX public.idx_ofec_candidate_election_mv_tmp_prev_election_yr RENAME TO idx_ofec_candidate_election_mv_prev_election_yr;
+
+-- 2) Drop function `election_duration` because it's no longer being used
+
+DROP FUNCTION IF EXISTS public.election_duration();
