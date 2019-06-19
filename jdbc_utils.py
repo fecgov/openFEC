f010bd04a8b235f3a3936c8d9fb88e979bb6651f
Laura Beaufort <31420082+lbeaufort@users.noreply.github.com>
2019-06-19 12:20:15 -0400

    Remove credentials from migration url

--- jdbc_utils.py
+++ jdbc_utils.py
@@ -1,11 +1,8 @@
 import re
 def to_jdbc_url(dbi_url):
-    DB_URL_REGEX = re.compile(r'postgresql://(?P<username>[^:]*):?(?P<password>\S*)@(?P<host_port>\S*)$')
+    DB_URL_REGEX = re.compile(r'(?P<host_port>\S*)$')
     match = DB_URL_REGEX.match(dbi_url)
     if match:
-        jdbc_url = 'jdbc:postgresql://{}?user={}'.format(
-            match.group('host_port'), match.group('username'))
-        if match.group('password'):
-            jdbc_url += '&password={}'.format(match.group('password'))
+        jdbc_url = 'jdbc:postgresql://{}/fec'.format(match.group('host_port'))
         return jdbc_url
     return None
