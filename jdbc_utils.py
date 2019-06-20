ba8b8b39e057426cba787a9edbd5d34f49c05d34
Laura Beaufort <31420082+lbeaufort@users.noreply.github.com>
2019-06-20 14:00:34 -0400

    Add better error message handling

--- jdbc_utils.py
+++ jdbc_utils.py
@@ -1,5 +1,7 @@
 import re
+
 def get_jdbc_credentials(dbi_url):
+    """Extract username and password from connection string"""
     DB_URL_REGEX = re.compile(r'postgresql://(?P<username>[^:]*):?(?P<password>\S*)@(?P<host_port>\S*)$')
     match = DB_URL_REGEX.match(dbi_url)
     if match:
@@ -7,10 +9,11 @@ def get_jdbc_credentials(dbi_url):
             match.group('host_port'))
         username = match.group('username')
         password = match.group('password')
-        return (jdbc_url, username, password)
-    return (None, None, None)
+        return jdbc_url, username, password
+    return None, None, None
 
 def to_jdbc_url(dbi_url):
+    """Reformat PostgreSQL uri to JDBC uri"""
     jdbc_url, username, password = get_jdbc_credentials(dbi_url)
     if jdbc_url:
         jdbc_url = '{}?user={}'.format(jdbc_url, username)
@@ -19,8 +22,16 @@ def to_jdbc_url(dbi_url):
         return jdbc_url
     return None
 
+
 def remove_credentials(error):
-    message = "ERROR:"
-    if '(' in error:
-        message = error.split('(')[0]
+    """Remove credentials from PostgreSQL erorrs, preserve the error codes."""
+    code_lookup = {
+        '08001': 'Connection attempt failed',
+        '28P01': 'Password authentication failed',
+    }
+    message = "PostgreSQL error"
+    match = re.search(r'.*(SQL State  : )+(?P<error>[a-zA-Z0-9]{0,5})', error)
+    if match:
+        error_code = match.group('error')
+        message += ' {}: {}'.format(error_code, code_lookup.get(error_code, "Unrecognized code. See PostgreSQL docs."))
     return message
