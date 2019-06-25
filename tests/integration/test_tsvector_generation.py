9ee73cf0e5ce04e10291ee83a79b8727aa7c736c
Jason Upchurch <jupchurch@fec.gov>
2019-06-25 10:36:44 -0400

    add automated test for schedule_f payee_name_text

--- tests/integration/test_tsvector_generation.py
+++ tests/integration/test_tsvector_generation.py
@@ -166,3 +166,39 @@ class TriggerTestCase(common.BaseTestCase):
             #the only result not returned is the "bad" last element
             self.assertEquals(set(names[key]) - set(contbr_occupation_list), {names[key][-1]})
         connection.close()
+
+    def test_schedule_f_payee_name_text(self):
+        '''
+        Test to see that pye_nm is parsed correctly and retrieved as
+        expected from ts_vector column payee_name_text
+        '''
+        connection = db.engine.connect()
+        name = "O'Reilly"
+        names_good = {'O Reilly', "O'Reilly", 'O.Reilly', 'O-Reilly'}
+        names_bad = {'O', "O'Hare", "Reilly"}
+        i = 0
+        for name in names_good.union(names_bad):
+            i += 1
+            data = {
+                'pye_nm': name,
+                'sub_id': 9999999999999999970 + i,
+                'filing_form': 'F3'
+            }
+            insert = "INSERT INTO disclosure.fec_fitem_sched_f " + \
+                "(pye_nm, sub_id, filing_form) " + \
+                   " VALUES (%(pye_nm)s, %(sub_id)s, %(filing_form)s)"
+            connection.execute(insert, data)
+        manage.refresh_materialized(concurrent=False)
+        select = "SELECT * from disclosure.fec_fitem_sched_f " + \
+            "WHERE payee_name_text @@ to_tsquery('" + parse_fulltext(name) + "');"
+        print('select')
+        print(select)
+        results = connection.execute(select).fetchall()
+        print('results')
+        print(results)
+        pye_nm_list = {n[14] for n in results}
+        #assert all good names in result set
+        assert(names_good.issubset(pye_nm_list))
+        #assert no bad names in result set
+        assert(names_bad.isdisjoint(pye_nm_list))
+        connection.close()
