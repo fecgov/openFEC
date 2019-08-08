11f56b7dc475fe63a2d0d79905f0cf1a7fde586d
Priyanka Kasireddy <pkasireddy@fec.gov>
2019-08-08 11:36:52 -0400

    Rename the env varible NEW_RELIC_APP_NAME to APP_NAME

--- webservices/tasks/utils.py
+++ webservices/tasks/utils.py
@@ -40,3 +40,6 @@ def get_json_data(response):
     # convert the response bytes data to a string
     python_str = json.dumps(response.data.decode('utf-8'))
     return python_str
+
+def get_app_name():
+    return env.get_credential('APP_NAME')
