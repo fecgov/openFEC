458d21067c92c415bfcb5fad2a5cf4591c0cb30c
Pat Phongsvirajati <pphongsvirajati@fec.gov>
2019-08-14 13:28:57 -0400

    test deploy headers branch to stage

--- tasks.py
+++ tasks.py
@@ -142,7 +142,8 @@ def _detect_space(repo, branch=None, yes=False):
 
 DEPLOY_RULES = (
     ('prod', _detect_prod),
-    ('stage', lambda _, branch: branch.startswith('release')),
+    #('stage', lambda _, branch: branch.startswith('release')),
+    ('stage', lambda _, branch: branch.startswith('feature/test-API-response-headers')),
     ('dev', lambda _, branch: branch == 'develop'),
 )
 
