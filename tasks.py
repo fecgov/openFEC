4412dc19f1eda8658ee7508c967a043560ac44c8
Priyanka Kasireddy <pkasireddy@fec.gov>
2019-12-19 14:52:06 -0500

    Deploy feature to test the role for a api key that was set on the api umbrella

--- tasks.py
+++ tasks.py
@@ -142,7 +142,8 @@ def _detect_space(repo, branch=None, yes=False):
 
 DEPLOY_RULES = (
     ('prod', _detect_prod),
-    ('stage', lambda _, branch: branch.startswith('release')),
+    # ('stage', lambda _, branch: branch.startswith('release')),
+    ('stage', lambda _, branch: branch == 'feature/3979-whitelist-server-side-keys'),
     ('dev', lambda _, branch: branch == 'develop'),
 )
 
