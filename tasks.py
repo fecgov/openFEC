07e901e5d3a89902843a6e063420cbc57a081a36
Priyanka Kasireddy <pkasireddy@fec.gov>
2020-01-06 11:57:08 -0500

    Update dev deploy rule to point to develop branch

--- tasks.py
+++ tasks.py
@@ -143,8 +143,7 @@ def _detect_space(repo, branch=None, yes=False):
 DEPLOY_RULES = (
     ('prod', _detect_prod),
     ('stage', lambda _, branch: branch.startswith('release')),
-    ('dev', lambda _, branch: branch == 'feature/4076-upgrade-smart-open-related-pkgs'),
-    # ('dev', lambda _, branch: branch == 'develop'),
+    ('dev', lambda _, branch: branch == 'develop'),
 )
 
 
