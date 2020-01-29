a898b4d7e337184907235edd73538e7116648baf
Pat Phongsvirajati <pphongsvirajati@fec.gov>
2020-01-29 12:23:06 -0500

    temporary deploy of this test branch for h4 testing to dev.

--- tasks.py
+++ tasks.py
@@ -143,7 +143,8 @@ def _detect_space(repo, branch=None, yes=False):
 DEPLOY_RULES = (
     ('prod', _detect_prod),
     ('stage', lambda _, branch: branch.startswith('release')),
-    ('dev', lambda _, branch: branch == 'develop'),
+    # temp deploy of this branch to dev
+    ('dev', lambda _, branch: branch == 'test/4112-schedule-h-endpoint'),
 )
 
 
