2558d69fedb969d0d555c58257c8a53bcc25093d
Laura Beaufort <lbeaufort@fec.gov>
2020-03-25 16:46:03 -0400

    First pass at adding flake8 to pytests

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,7 @@ from webservices import rest
 
 import pytest
 import subprocess
+import logging
 
 
 @pytest.fixture(scope="session")
@@ -32,3 +33,30 @@ def reset_schema():
     ]:
         rest.db.engine.execute('drop schema if exists %s cascade;' % schema)
     rest.db.engine.execute('create schema public;')
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--skip-linting",
+        action="store_true",
+        default=False,
+        help="Skip linting checks",
+    )
+    parser.addoption(
+        "--only-linting",
+        action="store_true",
+        default=False,
+        help="Only run linting checks",
+    )
+
+
+def pytest_collection_modifyitems(session, config, items):
+    if config.getoption("--skip-linting"):
+        items[:] = [item for item in items if not item.get_closest_marker('flake8')]
+    if config.getoption("--only-linting"):
+        items[:] = [item for item in items if item.get_closest_marker('flake8')]
+
+
+def pytest_configure(config):
+    """Flake8 is very verbose by default. Silence it."""
+    logging.getLogger("flake8").setLevel(logging.WARNING)
