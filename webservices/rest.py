84f9930bd482ff4ba9c251de8d59e47179b5fea8
Jason Upchurch <jupchurch@fec.gov>
2020-05-22 14:39:37 -0400

    add feature flag

--- webservices/rest.py
+++ webservices/rest.py
@@ -564,6 +564,7 @@ def api_ui():
         specs_url=url_for('docs.api_spec'),
         PRODUCTION=env.get_credential('PRODUCTION'),
         api_key_signup_key=env.get_credential('API_UMBRELLA_SIGNUP_KEY'),
+        api_key_signup_feature_flag=bool(env.get_credential('API_UMBRELLA_SIGNUP_KEY_FEATURE_FLAG', '')),
     )
 
 
