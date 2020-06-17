5b60754b0937f72f52da932f8d7182c0b92eb689
Laura Beaufort <lbeaufort@fec.gov>
2020-06-17 17:33:28 -0400

    Locust test changes

--- locustfile.py
+++ locustfile.py
@@ -50,46 +50,85 @@ TERMS = ["embezzle", "email", "department", "contribution", "commission"]
 # TODO: Add more small
 small_records_sched_a = [
     {"contributor_name": "Teachout Zephyr"},
-    {"contributor_state": "GU"},
+    # {"contributor_state": "GU"},
+    {"contributor_name": "Farnsworth"},
+    {"contributor_name": "Beaufort"},
+    {"contributor_name": "Karny"},
 ]
 
 medium_records_sched_a = [
     # this one gave us issues before because it was mid-sized about 21,000
-    {"committee_id": "C00496067"},
+    # {"committee_id": "C00496067"},
     # these seemed mid sized on a given two year period
-    {"contributor_city": "Fresno"},
-    {"contirbutor_city": "Sedona"},
+    # {"contributor_city": "Fresno"},
+    # {"contributor_city": "Sedona"},
     {"contributor_occupation": "government"},
+
 ]
 
 large_records_sched_a = [
-    {"committee_id": "C00401224"},
+    # {"committee_id": "C00401224"},
+    # {
+    #     "committee_id": [
+    #         "C00401224",
+    #         "C00003418",
+    #         "C00010603",
+    #         "C00027466",
+    #         "C00005561",
+    #         "C00484642",
+    #     ]
+    # },
+    {"contributor_occupation": "retired"},
+    {"contributor_employer": "retired"},
     {
-        "committee_id": [
+        "contributor_name": [
             "C00401224",
             "C00003418",
             "C00010603",
             "C00027466",
             "C00005561",
             "C00484642",
-        ]
+        ],
+        "sort": "-contribution_receipt_date"
+    },
+    {
+        "contributor_occupation": [
+            "retired",
+            "teacher",
+            "lawyer",
+            "consultant",
+            "self",
+            "self-employed",
+        ],
+        "sort": "-contribution_receipt_date"
+    },
+    {
+        "contributor_employer": [
+            "self",
+            "retired",
+            "amazon",
+            "facebook",
+            "google",
+        ],
+        "sort": "-contribution_receipt_date"
     },
     {"contributor_state": "NY"},
     {"contributor_state": "TX"},
     # some common last names in the US
     {"contributor_name": "Smith"},
     {"contributor_name": "Johnson"},
+    {"contributor_name": "Li"},
     # seeing this problem in production
-    {
-        "data_type": "processed",
-        "sort_hide_null": "true",
-        "committee_id": "C00401224",
-        "two_year_transaction_period": 2016,
-        "min_date": "07/01/2016",
-        "max_date": "07/31/2016",
-        "sort": "-contribution_receipt_date",
-        "per_page": 30,
-    },
+    # {
+    #     "data_type": "processed",
+    #     "sort_hide_null": "true",
+    #     "committee_id": "C00401224",
+    #     "two_year_transaction_period": 2016,
+    #     "min_date": "07/01/2016",
+    #     "max_date": "07/31/2016",
+    #     "sort": "-contribution_receipt_date",
+    #     "per_page": 30,
+    # },
 ]
 
 
@@ -99,8 +138,8 @@ poor_performance_a = [
         "sort_nulls_large": True,
         "contributor_name": "paul+johnson",
         "two_year_transaction_period": 2014,
-        "min_date": "01%2F01%2F2013",
-        "max_date": "12%2F31%2F2014",
+        # "min_date": "1/1/2013",
+        # "max_date": "12/31/2014",
         "contributor_state": "IN",
         "sort": "-contribution_receipt_date",
         "per_page": 30,
@@ -110,8 +149,8 @@ poor_performance_a = [
         "sort_nulls_large": True,
         "contributor_name": "Robert+F+Pence",
         "two_year_transaction_period": 2014,
-        "min_date": "01%2F01%2F2013",
-        "max_date": "12%2F31%2F2014",
+        # "min_date": "1/1/2013",
+        # "max_date": "12/31/2014",
         "sort": "-contribution_receipt_date",
         "per_page": 100,
         "is_individual": True,
@@ -120,8 +159,8 @@ poor_performance_a = [
         "sort_nulls_large": True,
         "contributor_name": "tom+lewis",
         "two_year_transaction_period": 2016,
-        "min_date": "01%2F01%2F2015",
-        "max_date": "12%2F31%2F2016",
+        # "min_date": "1/1/2015",
+        # "max_date": "12/31/2016",
         "sort": "-contribution_receipt_amount",
         "per_page": 30,
         "is_individual": True,
@@ -130,8 +169,8 @@ poor_performance_a = [
         "sort_nulls_large": True,
         "contributor_name": "Becher%2C+S",
         "two_year_transaction_period": 2016,
-        "min_date": "01%2F01%2F2015",
-        "max_date": "12%2F31%2F2016&",
+        # "min_date": "1/1/2015",
+        # "max_date": "12/31/2016",
         "contributor_state": "FL",
         "sort": "-contribution_receipt_date",
         "per_page": 30,
@@ -141,8 +180,8 @@ poor_performance_a = [
         "sort_nulls_large": True,
         "contributor_name": "Becher",
         "two_year_transaction_period": 2016,
-        "min_date": "01%2F01%2F2015",
-        "max_date": "12%2F31%2F2016",
+        # "min_date": "1/1/2015",
+        # "max_date": "12/31/2016",
         "contributor_state": "FL",
         "sort": "-contribution_receipt_date",
         "per_page": 30,
@@ -210,6 +249,14 @@ def get_random_date():
     return "{y}-{m}-{d}".format(y=str(year), m=str(month), d=str(date))
 
 
+def get_random_cycle():
+    rand_date = get_random_date()
+    _year = int(rand_date.split("-")[0])
+    _cycle = (_year + _year % 2)
+    return _cycle
+
+
+
 class Tasks(locust.TaskSet):
     def on_start(self):
         self.candidates = self.fetch_ids("candidates", "candidate_id")
@@ -221,10 +268,11 @@ class Tasks(locust.TaskSet):
         print("*********fetch_ids response:{}".format(resp))
         return [result[key] for result in resp.json()["results"]]
 
-    @locust.task
-    def load_home(self):
-        params = {"api_key": API_KEY}
-        self.client.get("", name="home", params=params)
+
+    # @locust.task
+    # def load_home(self):
+    #     params = {"api_key": API_KEY}
+    #     self.client.get("", name="home", params=params)
 
     @locust.task
     def test_download(self):
@@ -234,14 +282,11 @@ class Tasks(locust.TaskSet):
         the filename payload is optional(cms ajax call do pass in a filename each time.)
         """
         rand_date = get_random_date()
-        _year = int(rand_date.split("-")[0])
-        _cycle = (_year + 1) if _year % 2 == 1 else _year
-
         params = {
             "api_key": API_KEY,
             "sort_hide_null": False,
             "sort_nulls_last": False,
-            "two_year_transaction_period": _cycle,
+            "two_year_transaction_period": get_random_cycle(),
             "min_date": rand_date,
             "max_date": rand_date,
             "sort": "-contribution_receipt_date",
@@ -255,113 +300,115 @@ class Tasks(locust.TaskSet):
             json=payload,
         )
 
-    @locust.task
-    def load_candidates_search(self, term=None):
-        term = term or random.choice(CANDIDATES)
-        params = {"api_key": API_KEY, "sort": "-receipts", "q": term}
-        self.client.get("candidates/search", name="candidate_search", params=params)
+    # @locust.task
+    # def load_candidates_search(self, term=None):
+    #     term = term or random.choice(CANDIDATES)
+    #     params = {"api_key": API_KEY, "sort": "-receipts", "q": term}
+    #     self.client.get("candidates/search", name="candidate_search", params=params)
 
-    @locust.task
-    def load_committees_search(self, term=None):
-        term = term or random.choice(CANDIDATES)
-        params = {"api_key": API_KEY, "sort": "-receipts", "q": term}
-        self.client.get("committees", name="committee_search", params=params)
+    # @locust.task
+    # def load_committees_search(self, term=None):
+    #     term = term or random.choice(CANDIDATES)
+    #     params = {"api_key": API_KEY, "sort": "-receipts", "q": term}
+    #     self.client.get("committees", name="committee_search", params=params)
 
-    @locust.task
-    def load_candidates_table(self):
-        params = {
-            "cycle": [random.choice(CYCLES) for _ in range(3)],
-            "api_key": API_KEY,
-        }
-        self.client.get("candidates", name="candidates_table", params=params)
+    # @locust.task
+    # def load_candidates_table(self):
+    #     params = {
+    #         "cycle": [random.choice(CYCLES) for _ in range(3)],
+    #         "api_key": API_KEY,
+    #     }
+    #     self.client.get("candidates", name="candidates_table", params=params)
 
-    @locust.task
-    def load_committees_table(self):
-        params = {
-            "cycle": [random.choice(CYCLES) for _ in range(3)],
-            "api_key": API_KEY,
-        }
-        self.client.get("committees", name="committees_table", params=params)
+    # @locust.task
+    # def load_committees_table(self):
+    #     params = {
+    #         "cycle": [random.choice(CYCLES) for _ in range(3)],
+    #         "api_key": API_KEY,
+    #     }
+    #     self.client.get("committees", name="committees_table", params=params)
 
-    @locust.task
-    def load_candidate_detail(self, candidate_id=None):
-        params = {"api_key": API_KEY}
-        candidate_id = candidate_id or random.choice(self.candidates)
-        self.client.get(
-            os.path.join("candidate", candidate_id),
-            name="candidate_detail",
-            params=params,
-        )
+    # @locust.task
+    # def load_candidate_detail(self, candidate_id=None):
+    #     params = {"api_key": API_KEY}
+    #     candidate_id = candidate_id or random.choice(self.candidates)
+    #     self.client.get(
+    #         os.path.join("candidate", candidate_id),
+    #         name="candidate_detail",
+    #         params=params,
+    #     )
 
-    @locust.task
-    def load_committee_detail(self, committee_id=None):
-        params = {"api_key": API_KEY}
-        committee_id = committee_id or random.choice(self.committees)
-        self.client.get(
-            os.path.join("committee", committee_id),
-            name="committee_detail",
-            params=params,
-        )
+    # @locust.task
+    # def load_committee_detail(self, committee_id=None):
+    #     params = {"api_key": API_KEY}
+    #     committee_id = committee_id or random.choice(self.committees)
+    #     self.client.get(
+    #         os.path.join("committee", committee_id),
+    #         name="committee_detail",
+    #         params=params,
+    #     )
 
-    @locust.task
-    def load_candidate_totals(self, candidate_id=None):
-        params = {"api_key": API_KEY}
-        candidate_id = candidate_id or random.choice(self.candidates)
-        self.client.get(
-            os.path.join("candidate", candidate_id, "totals"),
-            name="candidate_totals",
-            params=params,
-        )
+    # @locust.task
+    # def load_candidate_totals(self, candidate_id=None):
+    #     params = {"api_key": API_KEY}
+    #     candidate_id = candidate_id or random.choice(self.candidates)
+    #     self.client.get(
+    #         os.path.join("candidate", candidate_id, "totals"),
+    #         name="candidate_totals",
+    #         params=params,
+    #     )
 
-    @locust.task
-    def load_committee_totals(self, committee_id=None):
-        params = {"api_key": API_KEY}
-        committee_id = committee_id or random.choice(self.committees)
-        self.client.get(
-            os.path.join("committee", committee_id, "totals"),
-            name="committee_totals",
-            params=params,
-        )
+    # @locust.task
+    # def load_committee_totals(self, committee_id=None):
+    #     params = {"api_key": API_KEY}
+    #     committee_id = committee_id or random.choice(self.committees)
+    #     self.client.get(
+    #         os.path.join("committee", committee_id, "totals"),
+    #         name="committee_totals",
+    #         params=params,
+    #     )
 
-    @locust.task
-    def load_legal_documents_search(self, term=None):
-        term = term or random.choice(CANDIDATES)
-        params = {"q": term, "api_key": API_KEY}
-        self.client.get("legal/search", name="legal_search", params=params)
+    # @locust.task
+    # def load_legal_documents_search(self, term=None):
+    #     term = term or random.choice(CANDIDATES)
+    #     params = {"q": term, "api_key": API_KEY}
+    #     self.client.get("legal/search", name="legal_search", params=params)
 
-    @locust.task
-    def get_mur(self):
-        params = {"api_key": API_KEY}
-        self.client.get("legal/docs/murs/7074", name="legal_get_mur", params=params)
+    # @locust.task
+    # def get_mur(self):
+    #     params = {"api_key": API_KEY}
+    #     self.client.get("legal/docs/murs/7074", name="legal_get_mur", params=params)
 
-    @locust.task
-    def get_adr(self):
-        params = {"api_key": API_KEY}
-        self.client.get("legal/docs/adrs/668", name="legal_get_adr", params=params)
+    # @locust.task
+    # def get_adr(self):
+    #     params = {"api_key": API_KEY}
+    #     self.client.get("legal/docs/adrs/668", name="legal_get_adr", params=params)
 
-    @locust.task
-    def get_admin_fine(self):
-        params = {"api_key": API_KEY}
-        self.client.get(
-            "legal/docs/admin_fines/2274", name="legal_get_admin_fine", params=params
-        )
+    # @locust.task
+    # def get_admin_fine(self):
+    #     params = {"api_key": API_KEY}
+    #     self.client.get(
+    #         "legal/docs/admin_fines/2274", name="legal_get_admin_fine", params=params
+    #     )
 
-    @locust.task
-    def get_ao(self):
-        params = {"api_key": API_KEY}
-        self.client.get(
-            "legal/docs/advisory_opinions/2018-07", name="legal_get_ao", params=params
-        )
+    # @locust.task
+    # def get_ao(self):
+    #     params = {"api_key": API_KEY}
+    #     self.client.get(
+    #         "legal/docs/advisory_opinions/2018-07", name="legal_get_ao", params=params
+    #     )
 
     @locust.task
     def load_schedule_a_small(self):
         params = random.choice(small_records_sched_a)
+        params["two_year_transaction_period"] = get_random_cycle()
         params["api_key"] = API_KEY
         self.client.get("schedules/schedule_a/", name="schedule_a_small", params=params)
 
     @locust.task
     def load_schedule_a_medium(self):
         params = random.choice(medium_records_sched_a)
+        params["two_year_transaction_period"] = get_random_cycle()
         params["api_key"] = API_KEY
         self.client.get(
             "schedules/schedule_a/", name="schedule_a_medium", params=params
@@ -370,6 +417,7 @@ class Tasks(locust.TaskSet):
     @locust.task
     def load_schedule_a_large(self):
         params = random.choice(large_records_sched_a)
+        params["two_year_transaction_period"] = get_random_cycle()
         params["api_key"] = API_KEY
         self.client.get(
             "schedules/schedule_a/", name="load_schedule_a_large", params=params
@@ -378,38 +426,39 @@ class Tasks(locust.TaskSet):
     @locust.task
     def load_schedule_a_problematic(self):
         params = random.choice(poor_performance_a)
+        params["two_year_transaction_period"] = get_random_cycle()
         params["api_key"] = API_KEY
         self.client.get(
             "schedules/schedule_a/", name="load_schedule_a_problematic", params=params
         )
 
-    @locust.task
-    def load_schedule_b_problematic(self):
-        params = random.choice(poor_performance_b)
-        params["api_key"] = API_KEY
-        self.client.get(
-            "schedules/schedule_b/", name="load_schedule_b_problematic", params=params
-        )
+    # @locust.task
+    # def load_schedule_b_problematic(self):
+    #     params = random.choice(poor_performance_b)
+    #     params["api_key"] = API_KEY
+    #     self.client.get(
+    #         "schedules/schedule_b/", name="load_schedule_b_problematic", params=params
+    #     )
 
-    @locust.task
-    def load_audit_category(self):
-        params = {"api_key": API_KEY}
-        self.client.get("audit-category/", name="load_audit_category", params=params)
+    # @locust.task
+    # def load_audit_category(self):
+    #     params = {"api_key": API_KEY}
+    #     self.client.get("audit-category/", name="load_audit_category", params=params)
 
-    @locust.task
-    def load_filings(self):
-        params = {"api_key": API_KEY}
-        self.client.get("filings/", name="load_filings", params=params)
+    # @locust.task
+    # def load_filings(self):
+    #     params = {"api_key": API_KEY}
+    #     self.client.get("filings/", name="load_filings", params=params)
 
-    @locust.task
-    def load_totals(self):
-        params = {"api_key": API_KEY}
-        self.client.get("totals/P/", name="load_totals", params=params)
+    # @locust.task
+    # def load_totals(self):
+    #     params = {"api_key": API_KEY}
+    #     self.client.get("totals/P/", name="load_totals", params=params)
 
-    @locust.task
-    def load_reports(self):
-        params = {"api_key": API_KEY}
-        self.client.get("reports/P/", name="load_reports", params=params)
+    # @locust.task
+    # def load_reports(self):
+    #     params = {"api_key": API_KEY}
+    #     self.client.get("reports/P/", name="load_reports", params=params)
 
 
 class Swarm(locust.HttpLocust):
