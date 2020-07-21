3a1093df100c085713d6e31be52c215d1e450d28
Laura Beaufort <lbeaufort@fec.gov>
2020-07-21 11:06:38 -0400

    Update number of gevent workers to 9 (2*CPU+1)

--- bin/run.sh
+++ bin/run.sh
@@ -1,2 +1,2 @@
 python manage.py cf_startup
-gunicorn --access-logfile - --error-logfile - --log-level info --timeout 300 -k gevent -w 4 webservices.rest:app
+gunicorn --access-logfile - --error-logfile - --log-level info --timeout 300 -k gevent -w 9 webservices.rest:app
