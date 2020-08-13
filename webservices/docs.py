a4417413bb074c3d032a43a23843149a7344f9ae
Jason Upchurch <jupchurch@fec.gov>
2020-08-13 09:20:26 -0400

    Fix math error in swagger ui

--- webservices/docs.py
+++ webservices/docs.py
@@ -8,9 +8,9 @@ This application programming interface (API) allows you to explore the way
 candidates and committees fund their campaigns.
 
 The FEC API is a RESTful web service supporting full-text and field-specific searches on
-FEC data. [Bulk downloads](https://www.fec.gov/data/advanced/?tab=bulk-data) are available on the current
-site. Information is tied to the underlying forms by file ID and image ID. Data is updated
-nightly.
+FEC data. [Bulk downloads](https://www.fec.gov/data/advanced/?tab=bulk-data) are available \
+on the current site. Information is tied to the underlying forms by file ID and image ID. \
+Data is updated nightly.
 
 There are a lot of data, and a good place to start is to use search to find
 interesting candidates and committees. Then, you can use their IDs to find report or line
@@ -18,10 +18,18 @@ item details with the other endpoints. If you are interested in individual donor
 out contributor information in schedule_a.
 
 <b class="body" id="getting_started_head">Getting started with the openFEC API</b><br>
-If you would like to use the FEC's API programmatically, you can sign up for your own API key using our form. Alternatively, you can still try out our API without an API key by using the web interface and using DEMO_KEY. Note that when you use the openFEC API you are subject to the [Terms of Service](https://github.com/fecgov/FEC/blob/master/TERMS-OF-SERVICE.md) and [Acceptable Use policy](https://github.com/fecgov/FEC/blob/master/ACCEPTABLE-USE-POLICY.md).
+If you would like to use the FEC's API programmatically, you can sign up for your own API \
+key using our form. Alternatively, you can still try out our API without an API key by using \
+the web interface and using DEMO_KEY. Note that when you use the openFEC API you are \
+subject to the [Terms of Service](https://github.com/fecgov/FEC/blob/master/TERMS-OF-SERVICE.md) \
+and [Acceptable Use policy](https://github.com/fecgov/FEC/blob/master/ACCEPTABLE-USE-POLICY.md).
 
 
-Signing up for an API key will enable you to place up to 1,000 calls an hour. Each call is limited to 100 results per page. You can email questions, comments or a request to get a key for 7,600 calls per hour (120 calls per minute) to <a href="mailto:APIinfo@fec.gov">APIinfo@fec.gov</a>. You can also ask questions and discuss the data in a community led [group](https://groups.google.com/forum/#!forum/fec-data).
+Signing up for an API key will enable you to place up to 1,000 calls an hour. Each call \
+is limited to 100 results per page. You can email questions, comments or a request to get \
+a key for 120 calls per minute to <a href="mailto:APIinfo@fec.gov">APIinfo@fec.gov</a>. You \
+can also ask questions and discuss the data in a community led \
+[group](https://groups.google.com/forum/#!forum/fec-data).
 
 The model definitions and schema are available at [/swagger](/swagger/). This is useful for
 making wrappers and exploring the data.
