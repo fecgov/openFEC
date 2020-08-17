e39b06fd2a7351fa770c1ec1a3c755676ae34d28
Jason Upchurch <jupchurch@fec.gov>
2020-08-17 16:47:29 -0400

    Add temp offset model

--- webservices/common/models/costs.py
+++ webservices/common/models/costs.py
@@ -47,7 +47,51 @@ class CommunicationCost(db.Model):
     file_number = db.Column('file_num', db.Integer)
     image_number = db.Column('image_num', db.String, index=True)
 
+class CommunicationCostOffset(db.Model):
+    __tablename__ = 'ofec_communication_cost_mv'
+    __table_args__ = {'extend_existing': True}
 
+    sub_id = db.Column(db.Integer, primary_key=True)
+    original_sub_id = db.Column('orig_sub_id', db.Integer, index=True)
+    candidate_id = db.Column('cand_id', db.String, index=True)
+    committee_id = db.Column('cmte_id', db.String, index=True)
+    committee_name = db.Column(db.String)
+    pdf_url = db.Column(db.String)
+    candidate_name = db.Column('s_o_cand_nm', db.String)
+    candidate_last_name = db.Column('s_o_cand_l_nm', db.String)
+    candidate_middle_name = db.Column('s_o_cand_m_nm', db.String)
+    candidate_first_name = db.Column('s_o_cand_f_nm', db.String)
+    candidate_office_state = db.Column('s_o_cand_office_st', db.String, index=True)
+    state_full = db.Column('s_o_cand_office_st_desc', db.String)
+    candidate_office_district = db.Column('s_o_cand_office_district', db.String, index=True)
+    candidate_office = db.Column('s_o_cand_office', db.String, index=True)
+    candidate_office_full = db.Column('s_o_cand_office_desc', db.String)
+    transaction_date = db.Column('communication_dt', db.Date, index=True)
+    transaction_amount = db.Column('communication_cost', db.Numeric(30, 2), index=True)
+    transaction_type = db.Column('transaction_tp', db.String)
+    communication_type = db.Column('communication_tp', db.String, index=True)
+    communication_type_full = db.Column('communication_tp_desc', db.String)
+    communication_class = db.Column('communication_class', db.String, index=True)
+    purpose = db.Column('communication_class_desc', db.String, index=True)
+    support_oppose_indicator = db.Column('s_o_ind', db.String, index=True)
+
+    # new columns added from ware house transition
+    action_code = db.Column('action_cd', db.String)
+    action_code_full = db.Column('action_cd_desc', db.String)
+    primary_general_indicator = db.Column('s_o_rpt_pgi', db.String)
+    primary_general_indicator_description = db.Column('s_o_rpt_pgi_desc', db.String)
+    report_type = db.Column('rpt_tp', db.String)
+    report_year = db.Column('rpt_yr', db.Integer)
+    cycle = db.Column('election_cycle', db.Integer, index=True)
+
+    form_type_code = db.Column('filing_form', db.String, index=True)
+    schedule_type = db.Column(db.String, index=True)
+    schedule_type_full = db.Column('schedule_type_desc', db.String)
+    tran_id = db.Column(db.String)
+    file_number = db.Column('file_num', db.Integer)
+    image_number = db.Column('image_num', db.String, index=True)
+
+    
 class Electioneering(db.Model):
     __tablename__ = 'ofec_electioneering_mv'
 
