19d76637425d5cbde2be151b7e7cc6b48b3ebb5c
Jun Li <jli@fec.gov>
2020-09-12 13:17:10 -0400

    Modify service name to fix ES7 connection.

--- webservices/legal_docs/test_es7.py
+++ webservices/legal_docs/test_es7.py
@@ -12,18 +12,16 @@ def test_es_conn():
         es_service_client = utils.create_es_client()
 
         client = es_service_client["client"]
-        broker = es_service_client["broker"]
-        logger.info("client = " + client)
-        logger.info("broker = " + broker)
+        # broker = es_service_client["broker"]
         document = {
             "title": "Moneyball",
             "director": "Bennett Miller",
             "year": "2011"
         }
 
-        es_service_client.index(index="movies", id="5", body=document)
+        client.index(index="movies", id="5", body=document)
 
-        logger.info(es_service_client.get(index="movies", id="5"))
+        logger.info(client.get(index="movies", id="5"))
 
     except Exception as err:
         logger.error('An error occurred while running the load command.{0}'.format(err))
