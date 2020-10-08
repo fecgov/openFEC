43b26cb368cf2a3e780fe0894f570dac3651cde4
Jean <jjeng@fec.gov>
2020-10-08 02:22:43 -0400

    modify MVs and endpoints for automating candidate committees to PAC conversion tracking

--- data/migrations/V0218__ofec_committee_history_mv_add_pcc_to_pac_info.sql
+++ data/migrations/V0218__ofec_committee_history_mv_add_pcc_to_pac_info.sql
@@ -10,6 +10,149 @@ pcc.candidate_election_year as former_cand_election_year
 */
 
 -- ----------
+-- ofec_pcc_to_pac_mv
+-- same as in V0215 except using base table of ofec_totals_combined_mv to reduce MV inter dependency
+-- ----------
+DROP VIEW IF EXISTS public.ofec_pcc_to_pac_vw;
+DROP MATERIALIZED VIEW IF EXISTS public.ofec_pcc_to_pac_mv;
+
+CREATE MATERIALIZED VIEW public.ofec_pcc_to_pac_mv
+AS
+with BASE_INFO as
+(
+    SELECT CMTE_PK, CMTE_ID, CMTE_NM, FILED_CMTE_TP, FILED_CMTE_DSGN, CREATE_DATE, 
+    TO_CHAR(CREATE_DATE, 'YYYY')::numeric +MOD(TO_CHAR(CREATE_DATE, 'YYYY')::numeric , 2)::numeric (4, 0) as FEC_ELECTION_YR
+    ,SUBSTRING(CMTE_PK::varchar,10, 6) CREATE_TIME, MST_RCT_REC_FLG
+    FROM DISCLOSURE.DIM_CMTE_IE_INF
+    WHERE CREATE_DATE IS NOT NULL
+    ORDER BY CMTE_ID, CREATE_DATE, CREATE_TIME
+)
+, first_record_per_cycle as 
+(
+    SELECT CMTE_PK, CMTE_ID, CMTE_NM, FILED_CMTE_TP, FILED_CMTE_DSGN, CREATE_DATE, CREATE_TIME, fec_election_yr, MST_RCT_REC_FLG
+    ,rank() over (partition by cmte_id, fec_election_yr order by create_date, CREATE_TIME) the_rank
+    FROM BASE_INFO
+)
+, info_begin_cycle as
+(
+    select CMTE_PK, CMTE_ID
+    ,coalesce(lag(CMTE_NM) over (partition by cmte_id order by fec_election_yr),CMTE_NM)  as CMTE_NM
+    ,coalesce(lag(FILED_CMTE_TP) over (partition by cmte_id order by fec_election_yr),FILED_CMTE_TP)  as FILED_CMTE_TP
+    ,coalesce(lag(FILED_CMTE_DSGN) over (partition by cmte_id order by fec_election_yr),FILED_CMTE_DSGN)  as FILED_CMTE_DSGN
+    ,concat('1/1/',(fec_election_yr-1)::varchar)::timestamp as create_date
+    ,create_time
+    ,fec_election_yr, MST_RCT_REC_FLG
+    from first_record_per_cycle
+    where the_rank = 1
+) 
+, last_record_per_cycle as 
+(
+    SELECT CMTE_PK, CMTE_ID, CMTE_NM, FILED_CMTE_TP, FILED_CMTE_DSGN, CREATE_DATE, CREATE_TIME, fec_election_yr, MST_RCT_REC_FLG
+    ,rank() over (partition by cmte_id, fec_election_yr order by MST_RCT_REC_FLG desc, create_date desc, CREATE_TIME desc) the_rank
+    FROM BASE_INFO
+)
+, info_end_cycle AS 
+(
+    SELECT CMTE_PK, CMTE_ID, CMTE_NM, FILED_CMTE_TP, FILED_CMTE_DSGN, CREATE_DATE, CREATE_TIME
+    , fec_election_yr, MST_RCT_REC_FLG
+    FROM last_record_per_cycle 
+    where the_rank = 1
+)
+, CHANGE_CAPTURE AS 
+(
+    SELECT info_end_cycle.CMTE_ID 
+    , info_begin_cycle.CMTE_NM AS FIRST_CMTE_NM
+    , info_end_cycle.CMTE_NM AS LATEST_CMTE_NM
+    , info_begin_cycle.FILED_CMTE_TP AS FIRST_CMTE_TP
+    , info_end_cycle.FILED_CMTE_TP AS LATEST_CMTE_TP
+    , info_begin_cycle.FILED_CMTE_DSGN AS FIRST_CMTE_DSGN
+    , info_end_cycle.FILED_CMTE_DSGN AS LATEST_CMTE_DSGN
+    , info_end_cycle.FEC_ELECTION_YR
+    FROM info_end_cycle JOIN info_begin_cycle
+    ON info_end_cycle.CMTE_ID = info_begin_cycle.CMTE_ID AND info_end_cycle.FEC_ELECTION_YR = info_begin_cycle.FEC_ELECTION_YR
+    ORDER BY info_end_cycle.CMTE_ID, info_end_cycle.FEC_ELECTION_YR
+)
+/*
+Some cand/cmte has special election that happened in the odd year,
+followed by regular election in even year.  
+For financial cycle purpose here, only care about the fec_election_yr.  
+So only take one row per CAND_ID/CMTE_ID/FEC_ELECTION_YR
+*/
+, CAND_CMTE_LINKAGE AS
+(
+    SELECT CAND_ID, CMTE_ID, FEC_ELECTION_YR
+    FROM ofec_cand_cmte_linkage_vw
+    group by CAND_ID, CMTE_ID, FEC_ELECTION_YR
+)
+/*
+some cmte filed different type of forms, either due to change of cmte_tp, or by mistakes
+ofec_totals_combined_vw include form_type in ('F3', 'F3P', 'F3X').  
+Here we need to know if these committees file financial information, no matter which form they reported money from.  
+Therefore get a cmte total per cycle.
+** In issue #4458, ofec_committee_history_mv need to refers to ofec_pcc_to_pac_mv,
+**  the complicated dependencies cause refresh sequences of MVs error out.
+since we only need to if these committees file financial information or not, 
+no need use whole ofec_totals_combined_vw.  Take the information from the base table of ofec_totals_combined_vw
+using the same criteria.  Result had been compared to make sure it is the same.
+*/
+, TOTALS AS
+(
+    SELECT vsd.cmte_id AS committee_id,
+    get_cycle(vsd.rpt_yr) AS cycle,
+    sum(COALESCE(vsd.ttl_receipts, 0)) AS receipts,
+    sum(COALESCE(vsd.ttl_disb, 0)) AS disbursements
+    FROM disclosure.v_sum_and_det_sum_report vsd
+    WHERE get_cycle(vsd.rpt_yr) >= 1979 AND (vsd.form_tp_cd::text <> 'F5'::text OR vsd.form_tp_cd::text = 'F5'::text AND (vsd.rpt_tp::text <> ALL (ARRAY['24'::character varying::text, '48'::character varying::text]))) AND (vsd.form_tp_cd::text <> ALL (ARRAY['F6'::text, 'SL'::text]))
+    GROUP BY vsd.cmte_id, (get_cycle(vsd.rpt_yr))
+)
+SELECT CHANGE_CAPTURE.CMTE_ID
+, CCL.CAND_ID
+, cand_yr.name as cand_name
+, cand_yr.candidate_election_year
+, CHANGE_CAPTURE.FEC_ELECTION_YR
+, CHANGE_CAPTURE.FIRST_CMTE_NM
+, CHANGE_CAPTURE.LATEST_CMTE_NM
+, CHANGE_CAPTURE.FIRST_CMTE_TP
+, CHANGE_CAPTURE.FIRST_CMTE_DSGN
+, CHANGE_CAPTURE.LATEST_CMTE_TP
+, CHANGE_CAPTURE.LATEST_CMTE_DSGN
+,totals.receipts, totals.disbursements
+FROM CHANGE_CAPTURE
+join CAND_CMTE_LINKAGE ccl 
+on ccl.cmte_id = CHANGE_CAPTURE.cmte_id and ccl.fec_election_yr = CHANGE_CAPTURE.fec_election_yr
+JOIN ofec_candidate_history_vw cand_yr
+ON cand_yr.candidate_id  = ccl.cand_id and cand_yr.two_year_period = CHANGE_CAPTURE.fec_election_yr
+LEFT JOIN totals ON totals.committee_id = CHANGE_CAPTURE.cmte_id and totals.cycle=CHANGE_CAPTURE.FEC_ELECTION_YR
+WHERE
+(
+    ((CHANGE_CAPTURE.FIRST_CMTE_TP IN ('H','P','S') and CHANGE_CAPTURE.FIRST_CMTE_DSGN IN ('P', 'A')) AND CHANGE_CAPTURE.LATEST_CMTE_TP NOT IN ('H','P','S'))
+    OR (CHANGE_CAPTURE.FIRST_CMTE_DSGN IN ('P','A') AND CHANGE_CAPTURE.LATEST_CMTE_DSGN NOT IN ('P','A'))
+)
+and cand_yr.candidate_election_year is not null
+and (COALESCE(receipts, 0) > 0 or COALESCE(disbursements, 0) > 0)
+and (FIRST_CMTE_DSGN not IN ('J') AND LATEST_CMTE_DSGN NOT IN ('J'))
+and LATEST_CMTE_TP NOT IN ('X','Y')
+and (change_capture.cmte_id, change_capture.fec_election_yr) not in (select cmte_id, fec_election_yr from public.ofec_pcc_conversion_exclude)
+ORDER BY CHANGE_CAPTURE.CMTE_ID, CHANGE_CAPTURE.FEC_ELECTION_YR;
+
+alter table public.ofec_pcc_to_pac_mv owner to fec;
+grant all on public.ofec_pcc_to_pac_mv to fec;
+grant select on public.ofec_pcc_to_pac_mv to fec_read;
+
+CREATE UNIQUE INDEX idx_ofec_pcc_to_pac_mv_cmte_id_fec_election_yr 
+    ON public.ofec_pcc_to_pac_mv USING btree (cmte_id,fec_election_yr);
+
+-- ------------------
+CREATE OR REPLACE VIEW public.ofec_pcc_to_pac_vw 
+AS select * from public.ofec_pcc_to_pac_mv;
+
+alter table public.ofec_pcc_to_pac_vw owner to fec;
+grant all on public.ofec_pcc_to_pac_vw to fec;
+grant select on public.ofec_pcc_to_pac_vw to fec_read;
+
+
+
+-- ----------
 -- ofec_committee_history_mv
 -- ----------
 DROP MATERIALIZED VIEW IF EXISTS public.ofec_committee_history_mv_tmp;
--- webservices/flow.py
+++ webservices/flow.py
@@ -118,12 +118,11 @@ def get_graph():
     graph.add_edge('committee_detail', 'sched_a_by_state_recipient_totals')
 
     graph.add_edges_from([
-        ('totals_combined', 'ofec_pcc_to_pac'),
         ('cand_cmte_linkage', 'ofec_pcc_to_pac'),
         ('candidate_history', 'ofec_pcc_to_pac'),
     ])
 
-    # graph.add_edge('ofec_pcc_to_pac', 'committee_history'),
+    graph.add_edge('ofec_pcc_to_pac', 'committee_history'),
     
 
     return graph
