4a904cc72d6c2853ddf39cc1b5300b5863cf48d0
Jun Li <jli@fec.gov>
2021-01-21 19:01:02 -0500

    Add leadership pac rows and pytest Clean code and change string single quote to double

--- webservices/resources/committees.py
+++ webservices/resources/committees.py
@@ -15,18 +15,20 @@ def filter_year(model, query, years):
         sa.or_(*[
             sa.and_(
                 sa.or_(
-                    sa.extract('year', model.last_file_date) >= year,
+                    sa.extract("year", model.last_file_date) >= year,
                     model.last_file_date == None,
                 ),
-                sa.extract('year', model.first_file_date) <= year,
+                sa.extract("year", model.first_file_date) <= year,
             )
             for year in years
         ])
     )  # noqa
 
 
+# use for this endpoint:(to get committee list)
+#  '/committees/'
 @doc(
-    tags=['committee'],
+    tags=["committee"],
     description=docs.COMMITTEE_LIST,
 )
 class CommitteeList(ApiResource):
@@ -34,28 +36,28 @@ class CommitteeList(ApiResource):
     model = models.Committee
     schema = schemas.CommitteeSchema
     page_schema = schemas.CommitteePageSchema
-    aliases = {'receipts': models.CommitteeSearch.receipts}
+    aliases = {"receipts": models.CommitteeSearch.receipts}
 
     filter_multi_fields = [
-        ('committee_id', models.Committee.committee_id),
-        ('filing_frequency', models.Committee.filing_frequency),
-        ('designation', models.Committee.designation),
-        ('organization_type', models.Committee.organization_type),
-        ('state', models.Committee.state),
-        ('party', models.Committee.party),
-        ('committee_type', models.Committee.committee_type),
+        ("committee_id", models.Committee.committee_id),
+        ("filing_frequency", models.Committee.filing_frequency),
+        ("designation", models.Committee.designation),
+        ("organization_type", models.Committee.organization_type),
+        ("state", models.Committee.state),
+        ("party", models.Committee.party),
+        ("committee_type", models.Committee.committee_type),
     ]
     filter_range_fields = [
-        (('min_first_file_date', 'max_first_file_date'), models.Committee.first_file_date),
-        (('min_last_f1_date', 'max_last_f1_date'), models.Committee.last_f1_date),
+        (("min_first_file_date", "max_first_file_date"), models.Committee.first_file_date),
+        (("min_last_f1_date", "max_last_f1_date"), models.Committee.last_f1_date),
     ]
     filter_fulltext_fields = [
-        ('q', models.CommitteeSearch.fulltxt),
-        ('treasurer_name', models.Committee.treasurer_text),
+        ("q", models.CommitteeSearch.fulltxt),
+        ("treasurer_name", models.Committee.treasurer_text),
     ]
 
     filter_overlap_fields = [
-        ('sponsor_candidate_id', models.Committee.sponsor_candidate_ids),
+        ("sponsor_candidate_id", models.Committee.sponsor_candidate_ids),
     ]
 
     @property
@@ -65,7 +67,7 @@ class CommitteeList(ApiResource):
             args.committee,
             args.committee_list,
             args.make_sort_args(
-                default='name',
+                default="name",
                 validator=args.IndexValidator(
                     models.Committee,
                     extra=list(self.aliases.keys()),
@@ -76,38 +78,41 @@ class CommitteeList(ApiResource):
     def build_query(self, **kwargs):
         query = super().build_query(**kwargs)
 
-        if {'receipts', '-receipts'}.intersection(kwargs.get('sort', [])) and 'q' not in kwargs:
+        if {"receipts", "-receipts"}.intersection(kwargs.get("sort", [])) and "q" not in kwargs:
             raise exceptions.ApiError(
-                'Cannot sort on receipts when parameter "q" is not set',
+                "Cannot sort on receipts when parameter 'q' is not set",
                 status_code=422,
             )
 
-        if kwargs.get('candidate_id'):
+        if kwargs.get("candidate_id"):
             query = query.filter(
-                models.Committee.candidate_ids.overlap(kwargs['candidate_id'])
+                models.Committee.candidate_ids.overlap(kwargs["candidate_id"])
             )
 
-        if kwargs.get('q'):
+        if kwargs.get("q"):
             query = query.join(
                 models.CommitteeSearch,
                 models.Committee.committee_id == models.CommitteeSearch.id,
             ).distinct()
 
-        if kwargs.get('year'):
-            query = filter_year(models.Committee, query, kwargs['year'])
+        if kwargs.get("year"):
+            query = filter_year(models.Committee, query, kwargs["year"])
 
-        if kwargs.get('cycle'):
-            query = query.filter(models.Committee.cycles.overlap(kwargs['cycle']))
+        if kwargs.get("cycle"):
+            query = query.filter(models.Committee.cycles.overlap(kwargs["cycle"]))
 
         return query
 
 
+# use for these endpoints:(return committee detail information)
+#  '/committee/<string:committee_id>/',
+#  '/candidate/<string:candidate_id>/committees/',
 @doc(
-    tags=['committee'],
+    tags=["committee"],
     description=docs.COMMITTEE_DETAIL,
     params={
-        'candidate_id': {'description': docs.CANDIDATE_ID},
-        'committee_id': {'description': docs.COMMITTEE_ID},
+        "candidate_id": {"description": docs.CANDIDATE_ID},
+        "committee_id": {"description": docs.COMMITTEE_ID},
     },
 )
 class CommitteeView(ApiResource):
@@ -117,9 +122,9 @@ class CommitteeView(ApiResource):
     page_schema = schemas.CommitteeDetailPageSchema
 
     filter_multi_fields = [
-        ('designation', models.CommitteeDetail.designation),
-        ('organization_type', models.CommitteeDetail.organization_type),
-        ('committee_type', models.CommitteeDetail.committee_type),
+        ("designation", models.CommitteeDetail.designation),
+        ("organization_type", models.CommitteeDetail.organization_type),
+        ("committee_type", models.CommitteeDetail.committee_type),
     ]
 
     @property
@@ -128,7 +133,7 @@ class CommitteeView(ApiResource):
             args.paging,
             args.committee,
             args.make_sort_args(
-                default='name',
+                default="name",
                 validator=args.IndexValidator(self.model),
             ),
         )
@@ -146,21 +151,27 @@ class CommitteeView(ApiResource):
                 models.CandidateCommitteeLink.candidate_id == candidate_id.upper()
             ).distinct()
 
-        if kwargs.get('year'):
-            query = filter_year(models.CommitteeDetail, query, kwargs['year'])
+        if kwargs.get("year"):
+            query = filter_year(models.CommitteeDetail, query, kwargs["year"])
 
-        if kwargs.get('cycle'):
-            query = query.filter(models.CommitteeDetail.cycles.overlap(kwargs['cycle']))
+        if kwargs.get("cycle"):
+            query = query.filter(models.CommitteeDetail.cycles.overlap(kwargs["cycle"]))
 
         return query
 
+
+# use for these endpoints:(return committee history)
+#  '/committee/<string:committee_id>/history/',
+#  '/committee/<string:committee_id>/history/<int:cycle>/',
+#  '/candidate/<string:candidate_id>/committees/history/',
+#  '/candidate/<string:candidate_id>/committees/history/<int:cycle>/',
 @doc(
-    tags=['committee'],
+    tags=["committee"],
     description=docs.COMMITTEE_HISTORY,
     params={
-        'candidate_id': {'description': docs.CANDIDATE_ID},
-        'committee_id': {'description': docs.COMMITTEE_ID},
-        'cycle': {'description': docs.COMMITTEE_CYCLE},
+        "candidate_id": {"description": docs.CANDIDATE_ID},
+        "committee_id": {"description": docs.COMMITTEE_ID},
+        "cycle": {"description": docs.COMMITTEE_CYCLE},
     },
 )
 class CommitteeHistoryView(ApiResource):
@@ -170,7 +181,7 @@ class CommitteeHistoryView(ApiResource):
     page_schema = schemas.CommitteeHistoryPageSchema
 
     filter_multi_fields = [
-        ('designation', models.CommitteeHistory.designation),
+        ("designation", models.CommitteeHistory.designation),
     ]
 
     @property
@@ -179,7 +190,7 @@ class CommitteeHistoryView(ApiResource):
             args.paging,
             args.committee_history,
             args.make_sort_args(
-                default='-cycle',
+                default="-cycle",
                 validator=args.IndexValidator(self.model),
             ),
         )
@@ -193,13 +204,13 @@ class CommitteeHistoryView(ApiResource):
             # '/committee/<string:committee_id>/history/<int:cycle>/',
             query = query.filter(models.CommitteeHistory.committee_id == committee_id.upper())
 
-        if candidate_id:
+        elif candidate_id:
             # use for
             # '/candidate/<candidate_id>/committees/history/',
             # '/candidate/<candidate_id>/committees/history/<int:cycle>/',
-            query_converted = query.filter(models.CommitteeHistory.former_candidate_id == candidate_id.upper())
 
-            query = query.join(
+            # 1) query for regular committees
+            query_regular = query.join(
                 models.CandidateCommitteeLink,
                 sa.and_(
                     models.CandidateCommitteeLink.committee_id == models.CommitteeHistory.committee_id,
@@ -209,37 +220,71 @@ class CommitteeHistoryView(ApiResource):
                 models.CandidateCommitteeLink.candidate_id == candidate_id.upper(),
             )
 
+            # 2) query for PCC to PAC conversion
+            query_pcc_converted = query.filter(models.CommitteeHistory.former_candidate_id == candidate_id.upper())
+
+            # 3) query for Leadership PAC committees
+            query_leadership_pac = query.join(
+                models.CandidateCommitteeAlternateLink,
+                sa.and_(
+                    models.CandidateCommitteeAlternateLink.committee_id == models.CommitteeHistory.committee_id,
+                    models.CandidateCommitteeAlternateLink.fec_election_year == models.CommitteeHistory.cycle,
+                ),
+            ).filter(
+                models.CandidateCommitteeAlternateLink.candidate_id == candidate_id.upper(),
+            )
+
+            # query for election_full=false
+            query = query_regular
         if cycle:
             # use for
-            # '/committee/<string:committee_id>/history/<int:cycle>/',
             # '/candidate/<string:candidate_id>/committees/history/<int:cycle>/',
-            if kwargs.get('election_full') and candidate_id:
-                query_regular = query.filter(
+            if kwargs.get("election_full") and candidate_id:
+                # ====build query_regular
+                # When election_full = true, the parameter "cycle" means candidate election year.
+                # to get regular committee(s) we use the value in "election_yr_to_be_included"
+                query_regular = query_regular.filter(
                     models.CandidateCommitteeLink.election_yr_to_be_included == cycle,
                 ).order_by(
                     models.CommitteeHistory.committee_id,
                     sa.desc(models.CommitteeHistory.cycle),
                 ).distinct(
-                    # in same election_yr_to_be_included, remove duplicate committee
+                    # inside one candidate election year (election_yr_to_be_included),
+                    # remove duplicate committee(s), mostly for Presidental and Senate candidate.
                     models.CommitteeHistory.committee_id,
                 )
 
-                query_converted = query_converted.filter(
+                # ====build query_leadership_pac
+                query_leadership_pac = query_leadership_pac.filter(
+                    models.CandidateCommitteeAlternateLink.candidate_election_year == cycle,
+                ).order_by(
+                    models.CommitteeHistory.committee_id,
+                    sa.desc(models.CommitteeHistory.cycle),
+                ).distinct(
+                    # inside one candidate election year,
+                    # remove duplicate committee(s), mostly for Presidental and Senate candidate.
+                    models.CommitteeHistory.committee_id,
+                )
+
+                # ====build query_pcc_converted
+                query_pcc_converted = query_pcc_converted.filter(
                     models.CommitteeHistory.former_candidate_election_year == cycle,
                 ).order_by(
                     models.CommitteeHistory.committee_id,
                     sa.desc(models.CommitteeHistory.cycle),
                 ).distinct(
-                    # in same election_yr_to_be_included, remove duplicate committee
+                    # inside one candidate election year,
+                    # remove duplicate committee(s), mostly for Presidental and Senate candidate.
                     models.CommitteeHistory.committee_id,
                 )
 
-                query = query_regular.union(query_converted).order_by(
+                # union three queries: query_regular + query_leadership_pac + query_pcc_converted
+                query = query_regular.union(query_leadership_pac, query_pcc_converted).order_by(
                     models.CommitteeHistory.committee_id,
                     sa.desc(models.CommitteeHistory.cycle),
                 )
-
             else:
+                # for election_full=false
                 query = query.filter(models.CommitteeHistory.cycle == cycle)
 
         return query
