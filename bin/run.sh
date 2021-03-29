aeeb4168e424e47308a416cdd28752b8c8498081
Laura Beaufort <lbeaufort@fec.gov>
2021-03-29 16:36:01 -0400

    Update gunicorn, switch to worker `threads`

--- bin/run.sh
+++ bin/run.sh
@@ -1,2 +1,2 @@
 python manage.py cf_startup
-gunicorn --access-logfile - --error-logfile - --log-level info --timeout 300 -k gevent -w 9 webservices.rest:app
+gunicorn --access-logfile - --error-logfile - --log-level info --timeout 300 --workers=4 --threads=5 webservices.rest:app
