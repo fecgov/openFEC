3dcf913f2959b5e45626b41246808a473680998c
Jun Li <jli@fec.gov>
2021-10-07 22:24:10 -0400

    Send AO detail information to Slack when found modified AO(s) update some Slack messages

--- webservices/tasks/__init__.py
+++ webservices/tasks/__init__.py
@@ -10,36 +10,48 @@ import ssl
 schedule = {}
 if env.app.get("space_name", "unknown-space").lower() != "feature":
     schedule = {
-        # Reload DAILY_MODIFIED_STARTING_AO at 9pm(EST) except Sunday.
-        "reload_all_aos_daily_except_sunday": {
-            "task": "webservices.tasks.legal_docs.reload_all_aos_when_change",
-            "schedule": crontab(minute=0, hour=1, day_of_week="mon,tue,wed,thu,fri,sat"),
-        },
-        # Reload All AOs every Sunday at 9pm(EST).
-        "reload_all_aos_every_sunday": {
-            "task": "webservices.tasks.legal_docs.reload_all_aos",
-            "schedule": crontab(minute=0, hour=1, day_of_week="sun"),
-        },
-        # Reload RECENTLY_MODIFIED_CASES and RECENTLY_MODIFIED_STARTING_AO
-        # every 5 minutes during 6am-7pm(EST).
+        # Task 1: This task is launched every 5 minutes during 6am-7pm(EST).
+        # When found any modified AO within 8 hours, reload all AOs.
+        # When found modified case(s)(MUR/AF/ADR) within 8 hours, reload/delete the modified case(s).
         "refresh_legal_docs": {
             "task": "webservices.tasks.legal_docs.refresh_most_recent_legal_doc",
             "schedule": crontab(minute="*/5", hour="10-23"),
         },
-        # Send modified legal case(during 6am-7pm EST) alerts to Slack every day at 7pm(EST)
+        # Task 2: This task is launched at 9pm(EST) everyday except Sunday.
+        # When found any modified AO in past 24 hours, reload all AOs.
+        "reload_all_aos_daily_except_sunday": {
+            "task": "webservices.tasks.legal_docs.daily_reload_all_aos_when_change",
+            "schedule": crontab(minute=0, hour=1, day_of_week="mon,tue,wed,thu,fri,sat"),
+        },
+        # Task 3: This task is launched at 9pm(EST) only on Sunday.
+        # Reload all AOs.
+        "reload_all_aos_every_sunday": {
+            "task": "webservices.tasks.legal_docs.weekly_reload_all_aos",
+            "schedule": crontab(minute=0, hour=1, day_of_week="sun"),
+        },
+        # Task 4: This task is launched at 6pm(EST) everyday.
+        # When found modified AO(s) in past 24 hours, send AO detail information to Slack.
+        "send_alert_ao": {
+            "task": "webservices.tasks.legal_docs.send_alert_daily_modified_ao",
+            "schedule": crontab(minute=0, hour=22),
+        },
+        # Task 5: This task is launched at 7pm(EST) everyday.
+        # When found modified case(s)(MUR/AF/ADR) during 6am-7pm(EST), send case detail information to Slack.
         "send_alert_legal_case": {
-            "task": "webservices.tasks.legal_docs.send_alert_most_recent_legal_case",
+            "task": "webservices.tasks.legal_docs.send_alert_daily_modified_legal_case",
             "schedule": crontab(minute=0, hour=23),
         },
-        # Refresh materialized views everyday at 5am(EST).
-        "refresh_materialized_views": {
-            "task": "webservices.tasks.refresh.refresh_materialized_views",
-            "schedule": crontab(minute=0, hour=9),
-        },
-        # Snapshot Elasticsearch at 12am(EST) in Sunday.
+        # Task 6: This task is launched at 12am(EST) only on Sunday.
+        # Take Elasticsearch 'docs' index snapshot.
         "backup_elasticsearch_every_sunday": {
             "task": "webservices.tasks.legal_docs.create_es_backup",
             "schedule": crontab(minute=0, hour=4, day_of_week="sun"),
+        },
+        # Task 7: This task is launched at 5am(EST) everyday.
+        # Refresh public materialized views.
+        "refresh_materialized_views": {
+            "task": "webservices.tasks.refresh_db.refresh_materialized_views",
+            "schedule": crontab(minute=0, hour=9),
         },
     }
 
@@ -70,7 +82,7 @@ app.conf.update(
         "ssl_cert_reqs": ssl.CERT_NONE,
     },
     imports=(
-        "webservices.tasks.refresh",
+        "webservices.tasks.refresh_db",
         "webservices.tasks.download",
         "webservices.tasks.legal_docs",
     ),
