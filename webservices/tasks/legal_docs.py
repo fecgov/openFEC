4838d4822ac8baa22bfb5373b44c23bc94647f63
Jun Li <jli@fec.gov>
2022-01-24 18:36:22 -0500

    Reload AOs to handle multiple modified AOs

--- webservices/tasks/legal_docs.py
+++ webservices/tasks/legal_docs.py
@@ -15,41 +15,34 @@ from webservices.tasks.utils import get_app_name
 
 logger = logging.getLogger(__name__)
 
-DAILY_MODIFIED_AO = """
-    SELECT ao_no, pg_date
-    FROM aouser.aos_with_parsed_numbers
-    WHERE pg_date >= NOW() - '24 hour'::INTERVAL
-    ORDER BY ao_year, ao_serial
-    LIMIT 1;
-"""
-
-RECENTLY_MODIFIED_STARTING_AO = """
+RECENTLY_MODIFIED_AOS = """
     SELECT ao_no, pg_date
     FROM aouser.aos_with_parsed_numbers
     WHERE pg_date >= NOW() - '8 hour'::INTERVAL
-    ORDER BY ao_year, ao_serial
-    LIMIT 1;
+    ORDER BY ao_year, ao_serial;
 """
 
 RECENTLY_MODIFIED_CASES = """
     SELECT case_no, case_type, pg_date, published_flg
     FROM fecmur.cases_with_parsed_case_serial_numbers_vw
     WHERE pg_date >= NOW() - '8 hour'::INTERVAL
-    ORDER BY case_serial
+    ORDER BY case_serial;
 """
 
-DAILY_MODIFIED_AOS_SEND_ALERT = """
+# For daily_reload_all_aos_when_change(): in past 24 hours(9pm-9pm EST)
+DAILY_MODIFIED_AOS = """
     SELECT ao_no, pg_date
     FROM aouser.aos_with_parsed_numbers
     WHERE pg_date >= NOW() - '24 hour'::INTERVAL
-    ORDER BY ao_year, ao_serial
+    ORDER BY ao_year, ao_serial;
 """
 
+# for send_alert_daily_modified_legal_case(): during 6am-7pm(EST) (13 hours)
 DAILY_MODIFIED_CASES_SEND_ALERT = """
     SELECT case_no, case_type, pg_date, published_flg
     FROM fecmur.cases_with_parsed_case_serial_numbers_vw
     WHERE pg_date >= NOW() - '13 hour'::INTERVAL
-    ORDER BY case_serial
+    ORDER BY case_serial;
 """
 
 SLACK_BOTS = "#bots"
@@ -58,8 +51,16 @@ SLACK_BOTS = "#bots"
 @app.task(once={"graceful": True}, base=QueueOnce)
 def refresh_most_recent_legal_doc():
     """
-    When found any modified AO within 8 hours, reload all AOs.
-    When found modified case(s)(MUR/AF/ADR) within 8 hours, reload/delete the modified case(s).
+        # Task 1: This task is launched every 5 minutes during 6am-7pmEST(13 hours).
+        # Task 1A: refresh_most_recent_aos(conn):
+        # 1) Identify the most recently modified AO(s) within 8 hours
+        # 2) For each modified AO, find the earliest AO referenced by the modified AO
+        # 3) Reload all AO(s) starting from the referenced AO to the latest AO.
+
+        # Task 1B: refresh_most_recent_cases(conn):
+        # When found modified case(s)(MUR/AF/ADR) within 8 hours,
+        #   if published_flg = true, reload the case(s) on elasticsearch service.
+        #   if published_flg = false, delete the case(s) on elasticsearch service.
     """
     with db.engine.connect() as conn:
         refresh_most_recent_aos(conn)
@@ -68,22 +69,29 @@ def refresh_most_recent_legal_doc():
 
 def refresh_most_recent_aos(conn):
     """
-    When found any modified AO within 8 hours, relead all AOs starting the earliest AO.
+        # 1) Identify the most recently modified AO(s) within 8 hours
+        # 2) For each modified AO, find the earliest AO referenced by the modified AO
+        # 3) Reload all AO(s) starting from the referenced AO to the latest AO.
     """
-    logger.info(" Checking for recently modified AO...")
-    row = conn.execute(RECENTLY_MODIFIED_STARTING_AO).first()
-    if row:
-        logger.info(" Recently modified AO %s found at %s", row["ao_no"], row["pg_date"])
-        load_advisory_opinions(row["ao_no"])
-    else:
+    logger.info(" Checking for recently modified AOs...")
+    rs = conn.execute(RECENTLY_MODIFIED_AOS)
+    row_count = 0
+    for row in rs:
+        if row:
+            row_count += 1
+            logger.info(" Recently modified AO %s found at %s", row["ao_no"], row["pg_date"])
+            load_advisory_opinions(row["ao_no"])
+    if row_count <= 0:
         logger.info(" No recently modified AO(s) found.")
+    else:
+        logger.info(" Total of %d ao(s) loaded to elasticsearch successfully.", row_count)
 
 
 def refresh_most_recent_cases(conn):
     """
-    When found modified case(s)(MUR/AF/ADR) within 8 hours,
-        if published_flg = true reload the modified case(s).
-        if published_flg = false delete the case(s)
+        # When found modified case(s)(MUR/AF/ADR) within 8 hours,
+        #   if published_flg = true, reload the case(s) on elasticsearch service.
+        #   if published_flg = false, delete the case(s) on elasticsearch service.
     """
     logger.info(" Checking for recently modified cases(MUR/AF/ADR)...")
     rs = conn.execute(RECENTLY_MODIFIED_CASES)
@@ -96,7 +104,7 @@ def refresh_most_recent_cases(conn):
         load_cases(row["case_type"], row["case_no"])
         if row["published_flg"]:
             load_count += 1
-            logger.info(" Total of %d case(s) loaded to elasticsearch.", load_count)
+            logger.info(" Total of %d case(s) loaded to elasticsearch successfully.", load_count)
         else:
             deleted_case_count += 1
             logger.info(" Total of %d case(s) unpublished.", deleted_case_count)
@@ -108,18 +116,34 @@ def refresh_most_recent_cases(conn):
 @app.task(once={"graceful": True}, base=QueueOnce)
 def daily_reload_all_aos_when_change():
     """
-    When found any modified AO in past 24 hours, reload all AOs, because AO may reference backward and forward AO.
+        # 1) Identify the daily modified AO(s) in past 24 hours(9pm-9pm EST)
+        # 2) For each modified AO, find the earliest AO referenced by the modified AO
+        # 3) Reload all AO(s) starting from the referenced AO to the latest AO
+        # 4) Send AO detail information to Slack.
     """
+    slack_message = ""
+    logger.info(" Checking for daily modified AOs...")
     with db.engine.connect() as conn:
-        row = conn.execute(DAILY_MODIFIED_AO).first()
-        if row:
-            logger.info(" Daily AO found %s modified at %s", row["ao_no"], row["pg_date"])
-            logger.info(" Daily (%s) reload of all AOs ", datetime.date.today().strftime("%A"))
-            load_advisory_opinions(row["ao_no"])
-            logger.info(" Daily (%s) reload of all AOs completed", datetime.date.today().strftime("%A"))
-
-        else:
+        rs = conn.execute(DAILY_MODIFIED_AOS)
+        row_count = 0
+        for row in rs:
+            if row:
+                row_count += 1
+                logger.info(" Daily modified AO %s found at %s", row["ao_no"], row["pg_date"])
+                logger.info(" Daily (%s) reload of all AOs ", datetime.date.today().strftime("%A"))
+                load_advisory_opinions(row["ao_no"])
+                slack_message = slack_message + "AO_" + str(row["ao_no"]) + " found modified at " + str(row["pg_date"])
+                slack_message = slack_message + "\n"
+        if row_count <= 0:
             logger.info(" No daily (%s) modified AOs found. Skip reload", datetime.date.today().strftime("%A"))
+            slack_message = "No daily modified AO found."
+        else:
+            logger.info(
+                " Daily (%s) total of %d ao(s) reload to elasticsearch successfully.", datetime.date.today().strftime("%A"), row_count)
+
+    if slack_message:
+        slack_message = slack_message + " in " + get_app_name()
+        utils.post_to_slack(slack_message, SLACK_BOTS)
 
 
 @app.task(once={"graceful": True}, base=QueueOnce)
@@ -129,28 +153,9 @@ def weekly_reload_all_aos():
     """
     logger.info(" Weekly (%s) reload of all AOs ", datetime.date.today().strftime("%A"))
     load_advisory_opinions()
-    logger.info(" Weekly (%s) reload of all AOs completed", datetime.date.today().strftime("%A"))
-    slack_message = "Weekly reload of all AOs completed in {0} space".format(get_app_name())
+    logger.info(" Weekly (%s) reload of all AOs completed successfully", datetime.date.today().strftime("%A"))
+    slack_message = "Weekly reload of all AOs completed successfully in {0} space".format(get_app_name())
     utils.post_to_slack(slack_message, SLACK_BOTS)
-
-
-@app.task(once={"graceful": True}, base=QueueOnce)
-def send_alert_daily_modified_ao():
-    # When found modified AO(s) in past 24 hours, send AO detail information to Slack.
-    slack_message = ""
-    with db.engine.connect() as conn:
-        rs = conn.execute(DAILY_MODIFIED_AOS_SEND_ALERT)
-        row_count = 0
-        for row in rs:
-            row_count += 1
-            slack_message = slack_message + "AO_" + str(row["ao_no"]) + " found modified at " + str(row["pg_date"])
-            slack_message = slack_message + "\n"
-    if row_count <= 0:
-        slack_message = "No daily modified AO found"
-
-    if slack_message:
-        slack_message = slack_message + " in " + get_app_name()
-        utils.post_to_slack(slack_message, SLACK_BOTS)
 
 
 @app.task(once={"graceful": True}, base=QueueOnce)
