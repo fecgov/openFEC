01a1e7b9c977fffdf0b3b45136b961de9e62e823
Hong Cao <hcao@fec.gov>
2023-02-23 12:23:03 -0500

    Add interpretation for candidate_id

--- webservices/docs.py
+++ webservices/docs.py
@@ -71,6 +71,12 @@ The candidate endpoints primarily use data from FEC registration
 CANDIDATE_ID = '''
 A unique identifier assigned to each candidate registered with the FEC.
 If a person runs for several offices, that person will have separate candidate IDs for each office.
+First character indicates office - [P]residential, [H]ouse, [S]enate].
+Second character is the last digit of the two-year period the ID was created.
+Third and fourth is the candidate state. Presidential IDs don't have state.
+Fifth and sixth is the district when the candidate first ran. This does not change if the
+candidate/member's district changes during re-districting. Presidential IDs don't have districts.
+The rest is sequence.
 '''
 
 CANDIDATE_INACTIVE = '''
