780301c0864612bea7d94d974f9ebb9d4c4f0b50
jacques metevier <jmetevier@gmail.com>
2023-05-19 13:53:18 -0700

    update webpack and dev works

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,88 +1,90 @@
-const path = require("path");
-const webpack = require('webpack')
+const path = require('path');
+const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   entry: {
-    "swagger-layout": [
-      "./src/index.js"
-    ]
+    'swagger-layout': ['./src/index.js'],
   },
 
-  output:  {
-    path: path.join(__dirname, "static/swagger-ui/dist"),
-    library: "SwaggerLayout",
-    libraryTarget: "umd",
-    filename: "[name].js",
-    chunkFilename: "js/[name].js",
+  output: {
+    path: path.join(__dirname, 'static/swagger-ui/dist'),
+    library: 'SwaggerLayout',
+    libraryTarget: 'umd',
+    filename: '[name].js',
+    chunkFilename: 'js/[name].js',
   },
 
   mode: 'production',
 
   module: {
     rules: [
-      {
-        enforce: 'pre',
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'eslint-loader',
-      },
+      // {
+      //   enforce: 'pre',
+      //   test: /\.js$/,
+      //   exclude: /node_modules/,
+      //   loader: 'eslint-loader',
+      // },
       {
         test: /\.(js(x)?)(\?.*)?$/,
-        use: [{
-          loader: "babel-loader",
-          options: {
-            retainLines: true
-          }
-        }],
-        include: [ path.join(__dirname, "src") ]
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              retainLines: true,
+            },
+          },
+        ],
+        include: [path.join(__dirname, 'src')],
       },
       {
         test: /\.css$/,
         use: ExtractTextPlugin.extract({
-          fallback: "style-loader",
-          use: "css-loader"
-        })
+          fallback: 'style-loader',
+          use: 'css-loader',
+        }),
       },
       {
         test: /\.(png|jpg|gif|svg)$/,
-        use: [{
-          loader: 'file-loader',
-          options: {
-            emitFile: true,
-            name: '[name].[ext]',
-            publicPath: '/docs/static/dist'
-          }
-        }]
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              emitFile: true,
+              name: '[name].[ext]',
+              publicPath: '/docs/static/dist',
+            },
+          },
+        ],
       },
       {
         test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
         use: {
-          loader: "url-loader",
+          loader: 'url-loader',
           options: {
-            limit: 50000
-          }
-        }
-      }
-    ]
+            limit: 50000,
+          },
+        },
+      },
+    ],
   },
 
-    optimization: {
-	minimize: true,
-	minimizer: [new TerserPlugin({
-	    terserOptions: {
-		output: false,
-	    },
-	})],
-    },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          output: false,
+        },
+      }),
+    ],
+  },
 
-    
   plugins: [
     new ExtractTextPlugin({
-      filename: "main.css",
-      allChunks: true
-    })
-  ]
-
+      filename: 'main.css',
+      allChunks: true,
+    }),
+  ],
 };
