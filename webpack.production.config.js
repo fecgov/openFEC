85590680a682022e9cb6242a4642ce329744ea0d
jacques metevier <jmetevier@gmail.com>
2023-05-21 11:32:08 -0700

    upgrade webpack to max 4x

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,25 +1,22 @@
-const path = require("path");
-const webpack = require('webpack')
+const path = require('path');
+const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
+  // const ESLintPlugin = require('eslint-webpack-plugin');
+  // mode: 'development',
+  // plugins: [new ESLintPlugin()],
   entry: {
-    "swagger-layout": [
-      "./src/index.js"
-    ]
+    'swagger-layout': ['./src/index.js'],
   },
-
-  output:  {
-    path: path.join(__dirname, "static/swagger-ui/dist"),
-    library: "SwaggerLayout",
-    libraryTarget: "umd",
-    filename: "[name].js",
-    chunkFilename: "js/[name].js",
+  output: {
+    path: path.join(__dirname, 'static/swagger-ui/dist'),
+    library: 'SwaggerLayout',
+    libraryTarget: 'umd',
+    filename: '[name].js',
+    chunkFilename: 'js/[name].js',
   },
-
-  mode: 'production',
-
   module: {
     rules: [
       {
@@ -29,60 +26,42 @@ module.exports = {
         loader: 'eslint-loader',
       },
       {
-        test: /\.(js(x)?)(\?.*)?$/,
-        use: [{
-          loader: "babel-loader",
-          options: {
-            retainLines: true
-          }
-        }],
-        include: [ path.join(__dirname, "src") ]
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
       },
       {
         test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: "style-loader",
-          use: "css-loader"
-        })
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(png|jpg|gif|svg)$/,
-        use: [{
-          loader: 'file-loader',
-          options: {
-            emitFile: true,
-            name: '[name].[ext]',
-            publicPath: '/docs/static/dist'
-          }
-        }]
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              emitFile: true,
+              name: '[name].[ext]',
+              publicPath: '/docs/static/dist',
+            },
+          },
+        ],
       },
       {
         test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
         use: {
-          loader: "url-loader",
+          loader: 'url-loader',
           options: {
-            limit: 50000
-          }
-        }
-      }
-    ]
+            limit: 50000,
+          },
+        },
+      },
+    ],
   },
-
-    optimization: {
-	minimize: true,
-	minimizer: [new TerserPlugin({
-	    terserOptions: {
-		output: false,
-	    },
-	})],
-    },
-
-    
-  plugins: [
-    new ExtractTextPlugin({
-      filename: "main.css",
-      allChunks: true
-    })
-  ]
-
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+    // fallback: {
+    //   path: false,
+    // },
+  },
 };
