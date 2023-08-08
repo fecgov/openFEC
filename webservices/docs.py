9175e18cd574a40a633b650fd9324b1d40fb8771
Corinne Lucas <66386084+cnlucas@users.noreply.github.com>
2023-08-08 16:58:08 -0400

    add test language

--- webservices/docs.py
+++ webservices/docs.py
@@ -5,7 +5,7 @@ API_DESCRIPTION = '''
 This application programming interface (API) allows you to explore the way \
 candidates and committees fund their campaigns.
 
-The Federal Election Commission (FEC) API is a RESTful web service supporting full-text and field-specific searches on
+The Federal Election Commission (FEC) API is a TEST ONE web service supporting full-text and field-specific searches on
 FEC data. [Bulk downloads](https://www.fec.gov/data/advanced/?tab=bulk-data) are available on the current
 site. Information is tied to the underlying forms by file ID and image ID. Data are updated
 nightly.
