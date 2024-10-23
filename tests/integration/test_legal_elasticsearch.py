77e6dac5e83ba0425f8190a45f28c273abd1312c
tmpayton <121631201+tmpayton@users.noreply.github.com>
2024-10-23 09:27:30 -0400

    add class decorators

--- tests/integration/test_legal_elasticsearch.py
+++ tests/integration/test_legal_elasticsearch.py
@@ -13,6 +13,7 @@ class TestLegalSearch():
     es_client = create_es_client()
     base_search_url = "/v1/legal/search/?"
 
+    @classmethod
     def setup_class(self):
         self.app = rest.app.test_client()
         for idx in ALL_INDICES:
@@ -23,6 +24,7 @@ class TestLegalSearch():
         for index in ALL_INDICES:
             self.es_client.indices.delete(index)
 
+    @classmethod
     def teardown_class(self):
         for index in ALL_INDICES:
             self.es_client.indices.delete(index)
