fff5d8ab52235b4f96541679396f0cc8f57bfe3d
Corinne Lucas <clucas@fec.gov>
2024-12-09 12:28:19 -0500

    increase workers

--- bin/run.sh
+++ bin/run.sh
@@ -1,2 +1,2 @@
 python cli.py cf_startup
-gunicorn --access-logfile - --error-logfile - --log-level info --timeout 300 -k gthread --threads 4 -w 4 webservices.rest:app
+gunicorn --access-logfile - --error-logfile - --log-level info --timeout 300 -k gthread --threads 9 -w 9 webservices.rest:app
