da52641f038672b8e3df9f3d60d6692124bd927f
Corinne Lucas <clucas@fec.gov>
2024-12-09 10:06:25 -0500

    deploy gthreads to dev

--- bin/run.sh
+++ bin/run.sh
@@ -1,2 +1,2 @@
 python cli.py cf_startup
-gunicorn --access-logfile - --error-logfile - --log-level info --timeout 300 -k gevent -w 9 webservices.rest:app
+gunicorn --access-logfile - --error-logfile - --log-level info --timeout 300 -k gthread --threads 9 -w 1 webservices.rest:app
