7f5657fa371e9747304e7f13e5eb470919995c0c
Corinne Lucas <clucas@fec.gov>
2024-12-09 13:09:53 -0500

    sync worker

--- bin/run.sh
+++ bin/run.sh
@@ -1,2 +1,2 @@
 python cli.py cf_startup
-gunicorn --access-logfile - --error-logfile - --log-level info --timeout 300 -k gthread --threads 9 -w 9 webservices.rest:app
+gunicorn --access-logfile - --error-logfile - --log-level info --timeout 300 -w 9 webservices.rest:app
