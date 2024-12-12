8f7dbee4f4e1231f8e5eb919af4fa6688482a97e
Corinne Lucas <clucas@fec.gov>
2024-12-12 09:56:35 -0500

    test gthreads stat

--- bin/run.sh
+++ bin/run.sh
@@ -1,2 +1,2 @@
 python cli.py cf_startup
-gunicorn --access-logfile - --error-logfile - --log-level info --timeout 300 -k gevent -w 9 webservices.rest:app
+gunicorn --access-logfile - --error-logfile - --log-level info --timeout 300 -k gthreads --threads 4 -w 9 webservices.rest:app
