1afdbd9fe5b977bbbed1236763ba8d719289a55c
Corinne Lucas <clucas@fec.gov>
2025-05-06 09:54:21 -0400

    add test

--- webservices/docs.py
+++ webservices/docs.py
@@ -2102,15 +2102,15 @@ Number of results to return (max 10)
 '''
 
 AO_NUMBER = '''
-Force advisory opinion number
+Advisory opinion number
 '''
 
 AO_YEAR = '''
-Force advisory opinion year
+Advisory opinion year
 '''
 
 AO_NAME = '''
-Force advisory opinion name
+Advisory opinion name
 '''
 
 AO_MIN_ISSUE_DATE = '''
