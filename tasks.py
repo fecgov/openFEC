8e3eb8df683cf1beb59093cdb626cfd9a49d34c2
tmpayton <121631201+tmpayton@users.noreply.github.com>
2025-12-09 12:21:09 -0500

    upgrade smart_open v7

--- tasks.py
+++ tasks.py
@@ -142,7 +142,7 @@ def _detect_space(repo, branch=None, yes=False):
 
 DEPLOY_RULES = (
     ('prod', _detect_prod),
-    ('stage', lambda _, branch: branch.startswith('release')),
+    ('stage', lambda _, branch: branch == 'test-smart-open-stage'),
     ('dev', lambda _, branch: branch == 'develop'),
 )
 
