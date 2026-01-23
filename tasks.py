6b90b3fa1462065977b83a30c5d3fe6dea9625b1
Jun Li <jli@fec.gov>
2026-01-23 10:59:44 -0500

    test rulmaking celery work on stage

--- tasks.py
+++ tasks.py
@@ -142,8 +142,9 @@ def _detect_space(repo, branch=None, yes=False):
 
 DEPLOY_RULES = (
     ('prod', _detect_prod),
-    ('stage', lambda _, branch: branch.startswith('release')),
-    ('dev', lambda _, branch: branch == 'test-rulemakings-celery-task'),
+    # ('stage', lambda _, branch: branch.startswith('release')),
+    ('stage', lambda _, branch: branch == 'test-rulemakings-celery-task'),
+    ('dev', lambda _, branch: branch == 'develop'),
 )
 
 
