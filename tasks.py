7aa7fef9b35b50d0eba925d4486f26d401e41d12
Pat Phongsvirajati <pphongsvirajati@fec.gov>
2026-03-06 15:05:18 -0500

    deploy to stage

--- tasks.py
+++ tasks.py
@@ -142,7 +142,8 @@ def _detect_space(repo, branch=None, yes=False):
 
 DEPLOY_RULES = (
     ('prod', _detect_prod),
-    ('stage', lambda _, branch: branch.startswith('release')),
+    # ('stage', lambda _, branch: branch.startswith('release')),
+    ('stage', lambda _, branch: branch == 'feature/sort_documents_by_doc_date'),
     ('dev', lambda _, branch: branch == 'develop'),
 )
 
