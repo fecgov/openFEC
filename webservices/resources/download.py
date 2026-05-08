9ee0a276bed55135c0e565535df336fe655abeca
tmpayton <121631201+tmpayton@users.noreply.github.com>
2026-05-08 11:30:46 -0400

    add soft and hard time limits to downloads, add task_id

--- webservices/resources/download.py
+++ webservices/resources/download.py
@@ -11,9 +11,13 @@ from flask import request
 from webservices import utils
 from webservices import exceptions
 from webservices.tasks import download
+from webservices.tasks.utils import delete_redis_value
 from webservices.tasks import utils as task_utils
 from webservices.utils import use_kwargs_original
 from flask import current_app
+import logging
+
+logger = logging.getLogger(__name__)
 
 client = boto3.client('s3')
 
@@ -23,28 +27,46 @@ URL_EXPIRY = 7 * 24 * 60 * 60
 
 class DownloadView(utils.Resource):
 
-    @use_kwargs_original({'filename': fields.Str(load_default=None)})
-    def post(self, path, filename=None, **kwargs):
+    @use_kwargs_original({'filename': fields.Str(load_default=None),
+                          'task_id': fields.Str(load_default=None)}, location='json')
+    def post(self, path, filename=None, task_id=None, **kwargs):
         parts = request.path.split('/')
         parts.remove('download')
         path = '/'.join(parts)
+
         cached_file = get_cached_file(path, request.query_string, filename=filename)
         if cached_file:
+            if task_id and task_utils.get_redis_value('download-queued:{}'.format(task_id)):
+                delete_redis_value('download-queued:{}'.format(task_id))
+
             return {
                 'status': 'complete',
                 'url': cached_file,
             }
+
+        if task_id:
+            if task_utils.get_redis_value('download-failed:{}'.format(task_id)):
+                delete_redis_value('download-failed:{}'.format(task_id))
+                logging.exception(f"Download {task_id} failed")
+                raise exceptions.ApiError('Download failed', status_code=http.client.INTERNAL_SERVER_ERROR)
+            if task_utils.get_redis_value('download-queued:{}'.format(task_id)):
+                return {'status': 'queued', 'task_id': task_id}
+
         resource = download.call_resource(current_app, path, request.query_string.decode('UTF-8'))
+
         if resource['count'] > MAX_RECORDS:
             raise exceptions.ApiError(
                 'Cannot request downloads with more than {} records'.format(MAX_RECORDS),
                 status_code=http.client.FORBIDDEN,
             )
-        download.export_query.delay(
+
+        task_id = download.export_query.delay(
             path,
             base64.b64encode(request.query_string).decode('UTF-8')
-        )
-        return {'status': 'queued'}
+        ).id
+        task_utils.set_redis_value('download-queued:{}'.format(task_id), True, age=7200)
+
+        return {'status': 'queued', 'task_id': task_id}
 
 
 def get_cached_file(path, qs, filename=None):
