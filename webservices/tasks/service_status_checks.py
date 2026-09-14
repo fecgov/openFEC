7f5ceaf8a43a8f1ad1af3670f1cd32e40517b64c
tmpayton <121631201+tmpayton@users.noreply.github.com>
2026-09-14 09:30:18 -0400

    add timeout

--- webservices/tasks/service_status_checks.py
+++ webservices/tasks/service_status_checks.py
@@ -6,7 +6,7 @@ from webservices.tasks.celery import FlaskQueueOnce
 SYSTEM_STATUS_CACHE_AGE = env.get_credential("SYSTEM_STATUS_CACHE_AGE") or 40
 
 
-@shared_task(once={"graceful": True}, base=FlaskQueueOnce, ignore_result=False)
+@shared_task(once={"graceful": True, "timeout": 25}, base=FlaskQueueOnce, ignore_result=False)
 def heartbeat():
     # if this task is running, that means that redis, celery-beat, and celery-worker are up
     set_redis_value("CELERY_STATUS", {"celery-is-running": True}, age=SYSTEM_STATUS_CACHE_AGE)
