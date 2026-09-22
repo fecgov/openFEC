4700671d58951efad76c4b3f9389b44ec83a5346
Jun Li <jli@fec.gov>
2026-09-22 10:51:43 -0400

    fix rm upload miss doc bug

--- webservices/legal/rulemaking_docs/rulemaking.py
+++ webservices/legal/rulemaking_docs/rulemaking.py
@@ -541,6 +541,7 @@ def get_documents(rm_no, rm_id, bucket):
                 }
 
                 if not row["contents"]:
+                    documents.append(document)
                     logger.error(
                         "PDF contents not found for document ID {0} and rulemaking no {1}: cannot upload to S3".format(
                             row["doc_id"], rm_no
@@ -568,7 +569,7 @@ def get_documents(rm_no, rm_id, bucket):
                             )
                     except Exception:
                         pass
-
+                
             else:
                 # If no level_1 document record exists in documents_vw for the specified level_1 value and level_2 = 0,
                 # only the level_1 label will be displayed. Ex:1997-03,rm_id=4391,level_1=1
